4b8e50113c2742728a457631df9c3f2cce889bbf
IvanRamyk <ivan.r@mate.academy>
2022-01-30 13:24:44 +0200

    Add task

--- README.md
+++ README.md
@@ -1,4 +1,161 @@
-# Python boilerplate for GitHub tasks
+# Lockdown cafe
 
 - Read [the guideline](https://github.com/mate-academy/py-task-guideline/blob/main/README.md) before start
-- Implement the task described [here](app/main.py)
+
+Implement a `Cafe` class, whose instances have one attribute `name`.
+Write a `visit_cafe` method with one parameter, `visitor` of type `dict`.
+Due to the pandemic, not everyone can visit the cafe.
+
+If the visitor does not have a `vaccine` key, it means that he is not vaccinated. 
+In this case, the method must raise a `NotVaccinatedError` exception.
+```python
+kfc = Cafe("KFC")
+visitor = {
+    "name": "Paul",
+    "age": 23,
+}
+kfc.visit_cafe(visitor)     # NotVaccinatedError
+```
+
+
+The vaccine must not be expired, otherwise the method should raise an `OutdatedVaccineError` exception.
+You can get an `expiration_date` from `visitor["vaccine"]` dictionary.
+```python
+import datetime
+
+kfc = Cafe("KFC")
+visitor = {
+    "name": "Paul",
+    "age": 23,
+    "vaccine": {
+        "expiration_date": datetime.datetime(year=2019, month=2, day=23)
+    }
+}
+kfc.visit_cafe(visitor)     # OutdatedVaccineError
+```
+
+And the last rule: all visitors must wear masks. Otherwise `visit_cafe` 
+should raise an `NotWearingMaskError` exception.
+```python
+kfc = Cafe("KFC")
+visitor = {
+    "name": "Paul",
+    "age": 23,
+    "vaccine": {
+        "expiration_date": datetime.datetime.today()
+    },
+    "wearing_a_mask": False
+}
+kfc.visit_cafe(visitor)     # NotWearingMaskError
+```
+
+If all the rules are met, then the person can visit the cafe and 
+the method should return the string `"Welcome to {cafe.name}"`
+
+```python
+kfc = Cafe("KFC")
+visitor = {
+    "name": "Paul",
+    "age": 23,
+    "vaccine": {
+        "expiration_date": datetime.datetime.today()
+    },
+    "wearing_a_mask": True
+}
+kfc.visit_cafe(visitor) == "Welcome to KFC"
+```
+
+Write a function `go_to_cafe` which takes a `friends` list and a `cafe`.
+It should return a string `"Friends can go to {cafe.name}"` if they are all allowed to visit it.
+
+If at least one of the friend have problems with vaccines the function should return a message `"All friends should be vaccinated".
+
+If everyone is vaccinated but somebody isn't wearing a mask
+the function should return a message `"Friends should buy {masks_to_buy} masks"` where `masks_to_buy` is the number of 
+friends who don't have a mask.
+
+```python
+friends = [
+    {
+        "name": "Alisa",
+        "vaccine": {
+            "expiration_date": datetime.datetime.today()
+        },
+        "wearing_a_mask": True
+    },
+    {
+        "name": "Bob",
+        "vaccine": {
+            "expiration_date": datetime.datetime.today()
+        },
+        "wearing_a_mask": True
+    },
+]
+go_to_cafe(friends, Cafe("KFC")) == "Friends can go to KFC"
+```
+
+```python
+friends = [
+    {
+        "name": "Alisa",
+        "vaccine": {
+            "expiration_date": datetime.datetime.today()
+        },
+        "wearing_a_mask": False
+    },
+    {
+        "name": "Bob",
+        "vaccine": {
+            "expiration_date": datetime.datetime.today()
+        },
+        "wearing_a_mask": False
+    },
+]
+go_to_cafe(friends, Cafe("KFC")) == "Friends should buy 2 masks"
+```
+
+```python
+friends = [
+    {
+        "name": "Alisa",
+        "wearing_a_mask": True
+    },
+    {
+        "name": "Bob",
+        "vaccine": {
+            "expiration_date": datetime.datetime.today()
+        },
+        "wearing_a_mask": True
+    },
+]
+go_to_cafe(friends, Cafe("KFC")) == "All friends should be vaccinated"
+```
+
+Use the following project structure:
+
+```app/
+    main.py
+    cafe.py
+    errors.py
+```
+
+All errors should be defined in the `errors.py` module.
+The `Cafe` class should be defined in `cafe.py` module.
+Function should be implemented in `main.py` module.
+
+
+Notes:
+* Use `try/except` to handle errors in `go_to_cafe`
+* Be sure to pass descriptive messages when you raise the exception in the `visit_cafe` method
+* Create a `VaccineError` parent class for `NotVaccinatedError` and `OutdatedVaccineError` errors.
+Use it to catch both types of errors in the same `except` clause.
+* You can work with dates using `datetime` module.
+* You can compare two dates using `<` operator:
+```python
+import datetime
+date1 = datetime.datetime(2020, 3, 4).date()
+date2 = datetime.datetime(2022, 1, 30).date()
+
+assert  date1 < date2
+```
+* To get the current date use `datetime.datetime.today().date()`
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,260 @@
-# TODO: add tests
-from app.main import hello_world
+import datetime
+
+import pytest
+
+from app.cafe import Cafe
+from app.errors import NotVaccinatedError, VaccineError, OutdatedVaccineError, NotWearingMaskError
+from app.main import go_to_cafe
 
 
-def test_hello_world():
-    assert hello_world() == "Hello, world!"
+def test_errors_hierarchy():
+    assert NotVaccinatedError.__bases__ == (VaccineError, ), (
+        "NotVaccinatedError should inherit only VaccineError class"
+    )
+    assert OutdatedVaccineError.__bases__ == (VaccineError, ), (
+        "OutdatedVaccineError should inherit only VaccineError class"
+    )
+
+
+@pytest.mark.parametrize(
+    "visitor",
+    [
+        {
+            "name": "John",
+            "age": 21,
+            "wearing_a_mask": True,
+        },
+        {
+            "name": "Alisa",
+            "age": 19,
+            "wearing_a_mask": True,
+        },
+    ]
+)
+def test_cafe_visit_should_raise_not_vaccinated_error_when_person_does_not_have_vaccine_key(visitor):
+    cafe = Cafe("")
+    with pytest.raises(NotVaccinatedError):
+        cafe.visit_cafe(visitor)
+
+
+@pytest.mark.parametrize(
+    "visitor",
+    [
+        {
+            "name": "John",
+            "age": 21,
+            "vaccine": {
+                "name": "Pfizer",
+                "expiration_date": (datetime.datetime.now() - datetime.timedelta(days=2)).date(),
+            },
+            "wearing_a_mask": True,
+        },
+        {
+            "name": "Alisa",
+            "age": 19,
+            "vaccine": {
+                "name": "Moderna",
+                "expiration_date": (datetime.datetime.now() - datetime.timedelta(days=4)).date(),
+            },
+            "wearing_a_mask": True,
+        },
+    ]
+)
+def test_cafe_visit_should_raise_outdated_vaccine_error_when_vaccine_has_expired(visitor):
+    cafe = Cafe("")
+    with pytest.raises(OutdatedVaccineError):
+        cafe.visit_cafe(visitor)
+
+
+@pytest.mark.parametrize(
+    "visitor",
+    [
+        {
+            "name": "John",
+            "age": 21,
+            "vaccine": {
+                "name": "Pfizer",
+                "expiration_date": (datetime.datetime.now() + datetime.timedelta(days=2)).date(),
+            },
+            "wearing_a_mask": False,
+        },
+        {
+            "name": "Alisa",
+            "age": 19,
+            "vaccine": {
+                "name": "Moderna",
+                "expiration_date": (datetime.datetime.now() + datetime.timedelta(days=4)).date(),
+            },
+            "wearing_a_mask": False,
+        },
+    ]
+)
+def test_cafe_visit_should_raise_not_wearing_mask_error_when_wearing_a_mask_is_false(visitor):
+    cafe = Cafe("")
+    with pytest.raises(NotWearingMaskError):
+        cafe.visit_cafe(visitor)
+
+
+@pytest.mark.parametrize(
+    "visitor,cafe_name",
+    [
+        (
+            {
+                "name": "John",
+                "age": 21,
+                "vaccine": {
+                    "name": "Pfizer",
+                    "expiration_date": datetime.datetime.today().date(),
+                },
+                "wearing_a_mask": True,
+            },
+            "KFC"
+        ),
+        (
+            {
+                "name": "Alisa",
+                "age": 19,
+                "vaccine": {
+                    "name": "Moderna",
+                    "expiration_date": (datetime.datetime.now() + datetime.timedelta(days=4)).date(),
+                },
+                "wearing_a_mask": True,
+            },
+            "McDonald's",
+        )
+    ]
+)
+def test_cafe_visit_should_return_welcome_when_visitor_is_wearing_a_mask_and_vaccinated(visitor, cafe_name):
+    cafe = Cafe(cafe_name)
+    assert cafe.visit_cafe(visitor) == f"Welcome to {cafe_name}"
+
+
+@pytest.mark.parametrize(
+    "friends,cafe,expected_message",
+    [
+        (
+            [
+                {
+                    "name": "Ivan",
+                    "vaccine": {
+                        "name": "Moderna",
+                        "expiration_date": (datetime.datetime.now() + datetime.timedelta(days=4)).date(),
+                    },
+                    "wearing_a_mask": True,
+                },
+                {
+                    "name": "Oleksii",
+                    "vaccine": {
+                        "name": "Pfizer",
+                        "expiration_date": datetime.datetime.now().date(),
+                    },
+                    "wearing_a_mask": True,
+                },
+            ],
+            Cafe("KFC"),
+            "Friends can go to KFC"
+        ),
+        (
+            [
+                {
+                    "name": "Alisa",
+                    "vaccine": {
+                        "name": "Pfizer",
+                        "expiration_date": (datetime.datetime.now() + datetime.timedelta(days=30)).date(),
+                    },
+                    "wearing_a_mask": False,
+                },
+                {
+                    "name": "Bob",
+                    "vaccine": {
+                        "name": "Pfizer",
+                        "expiration_date": (datetime.datetime.now() + datetime.timedelta(days=20)).date(),
+                    },
+                    "wearing_a_mask": False,
+                },
+                {
+                    "name": "Harry",
+                    "vaccine": {
+                        "name": "Moderna",
+                        "expiration_date": (datetime.datetime.now() + datetime.timedelta(days=45)).date(),
+                    },
+                    "wearing_a_mask": True,
+                },
+            ],
+            Cafe("KFC"),
+            "Friends should buy 2 masks"
+        ),
+        (
+            [
+                {
+                    "name": "Alisa",
+                    "vaccine": {
+                        "name": "Pfizer",
+                        "expiration_date": (datetime.datetime.now() + datetime.timedelta(days=30)).date(),
+                    },
+                    "wearing_a_mask": False,
+                },
+                {
+                    "name": "Bob",
+                    "vaccine": {
+                        "name": "Pfizer",
+                        "expiration_date": (datetime.datetime.now() + datetime.timedelta(days=20)).date(),
+                    },
+                    "wearing_a_mask": False,
+                },
+                {
+                    "name": "Harry",
+                    "vaccine": {
+                        "name": "Moderna",
+                        "expiration_date": (datetime.datetime.now() + datetime.timedelta(days=45)).date(),
+                    },
+                    "wearing_a_mask": False,
+                },
+            ],
+            Cafe("KFC"),
+            "Friends should buy 3 masks"
+        ),
+        (
+            [
+                {
+                    "name": "Alisa",
+                    "vaccine": {
+                        "name": "Pfizer",
+                        "expiration_date": (datetime.datetime.now() - datetime.timedelta(days=1)).date(),
+                    },
+                    "wearing_a_mask": True,
+                },
+                {
+                    "name": "Bob",
+                    "vaccine": {
+                        "name": "Pfizer",
+                        "expiration_date": (datetime.datetime.now() + datetime.timedelta(days=20)).date(),
+                    },
+                    "wearing_a_mask": True,
+                },
+            ],
+            Cafe("KFC"),
+            "All friends should be vaccinated"
+        ),
+        (
+            [
+                {
+                    "name": "Alisa",
+                    "vaccine": {
+                        "name": "Pfizer",
+                        "expiration_date": datetime.datetime.now().date(),
+                    },
+                    "wearing_a_mask": True,
+                },
+                {
+                    "name": "Bob",
+                    "wearing_a_mask": True,
+                },
+            ],
+            Cafe("KFC"),
+            "All friends should be vaccinated"
+        ),
+    ]
+)
+def test_go_to_the_cafe(friends, cafe, expected_message):
+    assert go_to_cafe(friends=friends, cafe=cafe) == expected_message
