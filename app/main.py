6b1698e7ad53b5f920daa11b49ace18e2608e6b6
Alex Zhidkov <sashazudkov.@gmail.com>
2022-01-31 18:05:03 +0200

    fix mistake

--- app/main.py
+++ app/main.py
@@ -1,18 +1,25 @@
+import datetime
+
 from app.cafe import Cafe
 from app.errors import VaccineError, NotWearingMaskError
 
 
 def go_to_cafe(friends: list, cafe: Cafe):
-    masks_to_buy = sum([
+    masks_to_buy = sum(
         1 for friend in friends
         if not friend["wearing_a_mask"] or "wearing_a_mask" not in friend
-    ])
-    try:
-        for friend in friends:
+    )
+    is_vaccine = True
+    for friend in friends:
+        try:
             cafe.visit_cafe(friend)
-    except VaccineError:
-        return "All friends should be vaccinated"
-    except NotWearingMaskError:
+        except VaccineError:
+            is_vaccine = False
+        except NotWearingMaskError:
+            pass
+    if not is_vaccine:
+        return 'All friends should be vaccinated'
+    if masks_to_buy:
         return f"Friends should buy {masks_to_buy} masks"
 
     return f"Friends can go to {cafe.name}"
