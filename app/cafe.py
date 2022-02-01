327ece812484dd5a28a764ed8d6f7739f227a379
Artur Shavula <arthuromgnh@gmail.com>
2022-02-01 14:49:50 +0200

    Fixed go_to_cafe function. Refactored Cafe.visit_cafe function

--- app/cafe.py
+++ app/cafe.py
@@ -8,19 +8,16 @@ class Cafe:
         self.name = name
 
     def visit_cafe(self, visitor: dict):
-        if 'vaccine' in visitor:
-            if visitor['vaccine']['expiration_date'] >= datetime.date.today():
-                if 'wearing_a_mask' in visitor and visitor['wearing_a_mask']:
-                    return f"Welcome to {self.name}"
-                else:
-                    raise errors.NotWearingMaskError(
-                        f"Visitor {visitor['name']} has no mask"
-                    )
-            else:
-                raise errors.OutdatedVaccineError(
-                    f"Visitor {visitor['name']} has outdated vaccine"
-                )
-        else:
+        if 'vaccine' not in visitor:
             raise errors.NotVaccinatedError(
                 f"Visitor {visitor['name']} has no vaccine"
             )
+        if not visitor['vaccine']['expiration_date'] >= datetime.date.today():
+            raise errors.OutdatedVaccineError(
+                f"Visitor {visitor['name']} has outdated vaccine"
+            )
+        if not ('wearing_a_mask' in visitor and visitor['wearing_a_mask']):
+            raise errors.NotWearingMaskError(
+                f"Visitor {visitor['name']} has no mask"
+            )
+        return f"Welcome to {self.name}"
