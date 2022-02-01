8393a5722e6ef2e1dda6a599052cdbaa80e62c21
Artur Shavula <arthuromgnh@gmail.com>
2022-02-01 16:08:41 +0200

    Refactored Cafe.visit_cafe

--- app/cafe.py
+++ app/cafe.py
@@ -12,7 +12,7 @@ class Cafe:
             raise errors.NotVaccinatedError(
                 f"Visitor {visitor['name']} has no vaccine"
             )
-        if not visitor['vaccine']['expiration_date'] >= datetime.date.today():
+        if visitor['vaccine']['expiration_date'] < datetime.date.today():
             raise errors.OutdatedVaccineError(
                 f"Visitor {visitor['name']} has outdated vaccine"
             )
