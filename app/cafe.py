371305ad751ec50ce4ab46a1a886c3ddb328a4c3
Rybak Dmytro <codingclaud@gmail.com>
2022-02-12 17:34:58 +0200

    'Fixed_tests'

--- app/cafe.py
+++ app/cafe.py
@@ -11,13 +11,17 @@ class Cafe:
 
     def visit_cafe(self, visitor: dict):
         if "vaccine" not in visitor.keys():
-            raise NotVaccinatedError("NotVaccinatedError")
+            raise NotVaccinatedError(
+                f"Visitor {visitor['name']} has no vaccine"
+            )
 
         if visitor["vaccine"]["expiration_date"] < self.today:
             raise OutdatedVaccineError(
                 f"Visitor {visitor['name']} has outdated vaccine"
             )
-        if not visitor["wearing_a_mask"]:
-            raise NotWearingMaskError("NotWearingMaskError")
+        if not ('wearing_a_mask' in visitor and visitor['wearing_a_mask']):
+            raise NotWearingMaskError(
+                f"Visitor {visitor['name']} has no mask"
+            )
 
         return f"Welcome to {self.name}"
