1d3419d0f650c4ded1cd9ff903f077bcf3bf2ff3
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-04-02 21:15:39 +0300

    Solution . Katiukha

--- app/cafe.py
+++ app/cafe.py
@@ -1,4 +1,6 @@
-from app.errors import *
+from app.errors import NotVaccinatedError, \
+    NotWearingMaskError, \
+    OutdatedVaccineError
 import datetime
 
 
@@ -9,23 +11,9 @@ class Cafe():
     def visit_cafe(self, visitor: dict):
         if "vaccine" not in visitor.keys():
             raise NotVaccinatedError("немає вакцини")
-        elif "wearing_a_mask" in visitor.keys() and not visitor["wearing_a_mask"]:
+        if "wearing_a_mask" in visitor.keys() and \
+                not visitor["wearing_a_mask"]:
             raise NotWearingMaskError("Без маски")
-        else:
-            dif_days = (datetime.date.today() - visitor["vaccine"]["expiration_date"]).days
-            if dif_days > 180:
-                raise OutdatedVaccineError(f"ВАКЦИНА ПРОСТРОЧЕНА, на {dif_days - 180} дні")
-            print(f"Welcome to {self.name}")
-
-
-if __name__ == "__main__":
-    kfc = Cafe("KFC")
-    visitor = {
-        "name": "Paul",
-        "age": 23,
-        "vaccine": {
-            "expiration_date": datetime.date.today()
-        },
-        "wearing_a_mask": True
-    }
-    kfc.visit_cafe(visitor)
+        if datetime.date.today() > visitor["vaccine"]["expiration_date"]:
+            raise OutdatedVaccineError("ВАКЦИНА ПРОСТРОЧЕНА")
+        return f"Welcome to {self.name}"
--- app/main.py
+++ app/main.py
@@ -1,24 +1,18 @@
-import datetime
-
 from app.cafe import Cafe
-from app.errors import *
+from app.errors import VaccineError, \
+    NotWearingMaskError
 
 
 def go_to_cafe(friends: list, cafe: Cafe):
     no_mask = 0
-    vaccine = True
     for visitor in friends:
         try:
             cafe.visit_cafe(visitor)
-        except NotVaccinatedError as e:
-            vaccine = False
-        except NotWearingMaskError as e:
+        except VaccineError:
+            return "All friends should be vaccinated"
+        except NotWearingMaskError:
             no_mask += 1
-        except OutdatedVaccineError as e:
-            vaccine = False
-    if not vaccine:
-        print("All friends should be vaccinated")
-    elif no_mask > 0:
-        print(f"Friends should buy 2 masks")
-    else:
-        print(f"Friends can go to {cafe.name}")
+
+    if no_mask:
+        return f"Friends should buy {no_mask} masks"
+    return f"Friends can go to {cafe.name}"
