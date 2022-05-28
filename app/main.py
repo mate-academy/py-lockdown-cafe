af2af0b82e9ba6a042490088954946aeaf118845
Serhiy Zhyhadlo <sergeyz.dev1996@gmail.com>
2022-05-28 11:38:56 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,26 @@
-# write your code here
+import datetime
+
+from app.errors import NotVaccinatedError
+from app.errors import NotWearingMaskError
+
+
+def go_to_cafe(friends, cafe):
+    mask_count = 0
+    for friend in friends:
+        if friend['wearing_a_mask'] is False:
+            mask_count += 1
+    for friend in friends:
+        try:
+            if "vaccine" not in friend or \
+                    friend['vaccine']['expiration_date']\
+                    < datetime.date.today():
+                raise NotVaccinatedError("All friends should be vaccinated")
+        except NotVaccinatedError:
+            return "All friends should be vaccinated"
+    for friend in friends:
+        try:
+            if friend['wearing_a_mask'] is False:
+                raise NotWearingMaskError
+        except NotWearingMaskError:
+            return f"Friends should buy {mask_count} masks"
+    return f"Friends can go to {cafe.name}"
