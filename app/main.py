70698684a22692d311db8a0b81dde02e6229efe8
ILIAS <nossudak@gmail.com>
2022-05-30 12:47:07 +0300

    Added error messages

--- app/main.py
+++ app/main.py
@@ -6,17 +6,14 @@ from app.cafe import Cafe
 
 
 def go_to_cafe(friends: list, cafe: Cafe):
-    everyone_has_vac = True
     persons_without_mask = 0
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
         except VaccineError:
-            everyone_has_vac = False
+            return "All friends should be vaccinated"
         except NotWearingMaskError:
             persons_without_mask += 1
-    if everyone_has_vac is False:
-        return "All friends should be vaccinated"
     if persons_without_mask > 0:
         return f"Friends should buy {persons_without_mask} masks"
     return f"Friends can go to {cafe.name}"
