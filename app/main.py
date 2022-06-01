54ae52af226d76299f451ee5a0d5584e62bb81f7
Vasyl Smutok <23.smutok@gmail.com>
2022-06-01 14:35:08 +0300

    fixed the complexity of the code

--- app/main.py
+++ app/main.py
@@ -15,9 +15,9 @@ def go_to_cafe(friends: list, cafe: Cafe):
         except NotWearingMaskError:
             masks_to_buy += 1
 
-        else:
-            if index == len(friends) - 1 and masks_to_buy == 0:
-                return f"Friends can go to {cafe.name}"
-            continue
+    if masks_to_buy:
+        return f"Friends should buy {masks_to_buy} masks"
 
-    return f"Friends should buy {masks_to_buy} masks"
+    return f"Friends can go to {cafe.name}"
+
+
