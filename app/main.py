75673562a3e4f32bdb0c87521b66ffe71f4e3909
Evgeniy Tretiak <103074600+Evgeniy994@users.noreply.github.com>
2022-06-02 19:42:08 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ def go_to_cafe(friends: list, cafe: Cafe):
         except NotWearingMaskError:
             masks_to_buy += 1
 
-    if not masks_to_buy:
+    if masks_to_buy > 0:
         return f"Friends should buy {masks_to_buy} masks"
     else:
         return f"Friends can go to {cafe.name}"
