0d09324d27bd8f3b9334e46c315658054930f6d4
Anton Lasko <anton.lasko1993@gmail.com>
2022-06-07 15:01:12 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -5,13 +5,12 @@ from app.errors import VaccineError, NotWearingMaskError
 def go_to_cafe(friends: list, cafe: Cafe):
     masks_to_buy = 0
     mask_errors_count = 0
-    guest = Cafe(cafe.name)
     for friend in friends:
         if friend['wearing_a_mask'] is False:
             masks_to_buy += 1
     for friend in friends:
         try:
-            guest.visit_cafe(friend)
+            cafe.visit_cafe(friend)
         except VaccineError:
             return "All friends should be vaccinated"
         except NotWearingMaskError:
