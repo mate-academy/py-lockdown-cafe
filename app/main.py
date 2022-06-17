774fa39162e20f7fed54c2b349ede86fe4dfa829
bohdan <bodyan1108@gmail.com>
2022-06-17 15:18:48 +0300

    Fixed annotation and added brackets to import errors

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
+from app.cafe import Cafe
 from app.errors import VaccineError, NotWearingMaskError
 
 
-def go_to_cafe(friends: list, cafe) -> str:
+def go_to_cafe(friends: list, cafe: Cafe) -> str:
     masks_to_buy = 0
 
     for friend in friends:
