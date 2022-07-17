833613bfc6e9b304bb8864cf91b18b93c3d768e6
Anastasiia Shevchenko <106030737+shevchenko-anastasiia@users.noreply.github.com>
2022-07-17 19:42:43 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from app.cafe import Cafe
-from app.errors import NotWearingMaskError, VaccineError
+from app.errors import VaccineError, NotWearingMaskError
 
 
 def go_to_cafe(friends: list, cafe: Cafe):
@@ -9,11 +9,8 @@ def go_to_cafe(friends: list, cafe: Cafe):
             cafe.visit_cafe(friend)
         except VaccineError:
             return "All friends should be vaccinated"
-
         except NotWearingMaskError:
             masks_to_buy += 1
-
-        if masks_to_buy > 0:
-            return f'Friends should buy {masks_to_buy} masks'
-
-        return f'Friends can go to {cafe.name}'
+    if masks_to_buy:
+        return f"Friends should buy {masks_to_buy} masks"
+    return f"Friends can go to {cafe.name}"
