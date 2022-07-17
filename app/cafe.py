e50e22682eb26b9e2fc3a667e615bf2a5634a469
Anastasiia Shevchenko <106030737+shevchenko-anastasiia@users.noreply.github.com>
2022-07-17 19:38:51 +0300

    Update cafe.py

--- app/cafe.py
+++ app/cafe.py
@@ -11,8 +11,8 @@ class Cafe:
     def visit_cafe(self, visitor: dict):
         if 'vaccine' not in visitor:
             raise NotVaccinatedError
-        if visitor["vaccine"]['expiration_date'] < datetime.date.today():
+        if visitor['vaccine']['expiration_date'] < datetime.date.today():
             raise OutdatedVaccineError
         if visitor['wearing_a_mask'] is False:
             raise NotWearingMaskError
-        return f"Welcome to {self.name}"
+        return f'Welcome to {self.name}'
