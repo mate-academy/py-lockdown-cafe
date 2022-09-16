0a64525f785a09eb4d336a6f3b056c1ac9cd01e4
Kyryll Katash <kyryllkatash@gmail.com>
2022-09-16 16:47:50 +0300

    Solution for py-lockdown-cafe

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+from app.errors import VaccineError
+from app.errors import NotWearingMaskError
+
+
+def go_to_cafe(friends: list, cafe: object):
+    mask = 0
+    for i in friends:
+        try:
+            cafe.visit_cafe(i)
+        except VaccineError:
+            return "All friends should be vaccinated"
+        except NotWearingMaskError:
+            mask += 1
+    if mask > 0:
+        return f"Friends should buy {mask} masks"
+    return f"Friends can go to {cafe.name}"
