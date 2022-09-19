8574a5a3e72ee375d16cc080d36823d502bb9cca
Kyryll Katash <kyryllkatash@gmail.com>
2022-09-19 17:33:26 +0300

    Changed loop variable name

--- app/main.py
+++ app/main.py
@@ -3,14 +3,14 @@ from app.errors import NotWearingMaskError
 
 
 def go_to_cafe(friends: list, cafe: object):
-    mask = 0
-    for i in friends:
+    mask_needed = 0
+    for friend in friends:
         try:
-            cafe.visit_cafe(i)
+            cafe.visit_cafe(friend)
         except VaccineError:
             return "All friends should be vaccinated"
         except NotWearingMaskError:
-            mask += 1
-    if mask > 0:
-        return f"Friends should buy {mask} masks"
+            mask_needed += 1
+    if mask_needed > 0:
+        return f"Friends should buy {mask_needed} masks"
     return f"Friends can go to {cafe.name}"
