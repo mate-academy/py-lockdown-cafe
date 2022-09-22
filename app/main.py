4a18134f0f7b46e8ee9c8341b39a6b24083861fe
mltdll <ilya.vyplavin@gmail.com>
2022-09-22 16:16:10 +0300

    Refactor go_to_cafe() function

--- app/main.py
+++ app/main.py
@@ -3,20 +3,15 @@ from app.errors import VaccineError, NotWearingMaskError
 
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
-    someone_is_not_vaccinated = False
     not_wearing_masks = 0
 
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
         except VaccineError:
-            someone_is_not_vaccinated = True
-            break
+            return "All friends should be vaccinated"
         except NotWearingMaskError:
             not_wearing_masks += 1
-
-    if someone_is_not_vaccinated:
-        return "All friends should be vaccinated"
 
     if not_wearing_masks > 0:
         return f"Friends should buy {not_wearing_masks} masks"
