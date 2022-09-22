a6b696e5e92a22066ebefb7aa2b44a78bece60bc
Serhii Kovalenko <27kovalenko@gmail.com>
2022-09-22 17:36:19 +0300

    Fixed error

--- app/cafe.py
+++ app/cafe.py
@@ -10,16 +10,13 @@ class Cafe:
         self.name = name
 
     def visit_cafe(self, visitor):
-        try:
-            if "vaccine" not in visitor:
-                raise NotVaccinatedError
+        if "vaccine" not in visitor:
+            raise NotVaccinatedError
 
-            if visitor["vaccine"]["expiration_date"] < datetime.date.today():
-                raise OutdatedVaccineError
+        if visitor["vaccine"]["expiration_date"] < datetime.date.today():
+            raise OutdatedVaccineError
 
-            if visitor["wearing_a_mask"] is False:
-                raise NotWearingMaskError
-        except Exception as error:
-            raise error
+        if visitor["wearing_a_mask"] is False:
+            raise NotWearingMaskError
         else:
             return f"Welcome to {self.name}"
