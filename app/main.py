9e913a7aa9249a3b6d42c5dc9c7267e5ccdbdbe8
Dmytro Kyrpan <d.kirpan@gmail.com>
2022-09-26 11:23:16 +0300

    solution v02 (return without else)

--- app/main.py
+++ app/main.py
@@ -13,5 +13,4 @@ def go_to_cafe(friends: list, cafe):
             masks_to_buy += 1
     if masks_to_buy > 0:
         return f"Friends should buy {masks_to_buy} masks"
-    else:
-        return f"Friends can go to {cafe.name}"
+    return f"Friends can go to {cafe.name}"
