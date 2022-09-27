88c144ec1d1494cf120b499ef4cc04572e66749c
Ivan Antonishak <jus1stored@gmail.com>
2022-09-27 14:23:54 +0300

    Fix import

--- app/cafe.py
+++ app/cafe.py
@@ -1,5 +1,8 @@
-from app.errors import NotVaccinatedError, \
-    OutdatedVaccineError, NotWearingMaskError
+from app.errors import (
+    NotVaccinatedError,
+    OutdatedVaccineError,
+    NotWearingMaskError
+)
 import datetime
 
 
