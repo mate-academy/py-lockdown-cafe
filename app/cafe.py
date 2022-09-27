f86b5a0fd85667415e2602db8d077f40e28a47f4
Taras Andruschenko <And999110@gmail.com>
2022-09-27 17:27:46 +0300

    Some import problems

---
+++ app/cafe.py
@@ -0,0 +1,19 @@
+import datetime
+from errors import (NotVaccinatedError,
+                    OutdatedVaccineError,
+                    NotWearingMaskError)
+
+
+class Cafe:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def visit_cafe(self, visitors: dict) -> str:
+        if "vaccine" not in visitors:
+            raise NotVaccinatedError
+        elif datetime.date.today() < visitors["vaccine"]["expiration_date"]:
+            raise OutdatedVaccineError
+        elif not visitors["wearing_a_mask"]:
+            raise NotWearingMaskError
+        else:
+            return f"Welcome to {self.name}"
