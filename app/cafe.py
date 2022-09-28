12852ec8fd4702822136d9a20eadd0fd0a40792e
Andriy Makarenko <andriy@mate.academy>
2022-09-28 20:45:43 +0300

    Solution. else replased by if

--- app/cafe.py
+++ app/cafe.py
@@ -11,9 +11,9 @@ class Cafe:
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
             raise NotVaccinatedError
-        elif visitor["vaccine"]["expiration_date"] < datetime.date.today():
+        if visitor["vaccine"]["expiration_date"] < datetime.date.today():
             raise OutdatedVaccineError
-        elif not visitor["wearing_a_mask"]:
+        if not visitor["wearing_a_mask"]:
             raise NotWearingMaskError
 
         return f"Welcome to {self.name}"
--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from app.cafe import Cafe
 from app.errors import VaccineError, NotWearingMaskError
 
 
-def go_to_cafe(friends: list, cafe: Cafe):
+def go_to_cafe(friends: list, cafe: Cafe) -> str:
     masks_to_buy = 0
 
     for friend in friends:
@@ -14,4 +14,5 @@ def go_to_cafe(friends: list, cafe: Cafe):
             masks_to_buy += 1
     if masks_to_buy:
         return f"Friends should buy {masks_to_buy} masks"
+
     return f"Friends can go to {cafe.name}"
