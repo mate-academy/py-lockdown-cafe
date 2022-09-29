dd4d158cd15bbb8279c8d8e703e63adeae313407
HStakhiv <gsstahiv@gmail.com>
2022-09-29 20:32:57 +0300

    More informative error message

--- app/cafe.py
+++ app/cafe.py
@@ -12,10 +12,12 @@ class Cafe:
 
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
-            raise NotVaccinatedError("NotVaccinatedError")
+            raise NotVaccinatedError("No entry without proof of vaccination")
         if visitor["vaccine"]["expiration_date"] < date.today():
-            raise OutdatedVaccineError("OutdatedVaccineError")
+            raise OutdatedVaccineError(
+                "You have no valid proof of vaccination"
+            )
         if not visitor["wearing_a_mask"]:
-            raise NotWearingMaskError("NotWearingMaskError")
+            raise NotWearingMaskError("No face mask. No entry!")
 
         return f"Welcome to {self.name}"
