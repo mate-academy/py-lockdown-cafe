5750d8557571895f355067da7598e4ebc70b3551
Yaroslav Stetsko <yar.stetsko@gmail.com>
2022-09-29 22:13:57 +0300

    Fixed imports

--- app/cafe.py
+++ app/cafe.py
@@ -1,7 +1,8 @@
 from datetime import date
 
-from app.errors import NotVaccinatedError, \
-    OutdatedVaccineError, NotWearingMaskError
+from app.errors import (NotVaccinatedError,
+                        OutdatedVaccineError,
+                        NotWearingMaskError)
 
 
 class Cafe:
