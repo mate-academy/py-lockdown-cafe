98ed1b1f322b0d400db3c466dc66f48994326b46
Sergey <servizah@gmail.com>
2022-10-01 18:20:55 +0300

    annotation added, else deleted

--- app/cafe.py
+++ app/cafe.py
@@ -1,18 +1,18 @@
 import datetime
-from app.errors \
-    import NotVaccinatedError, OutdatedVaccineError, NotWearingMaskError
+from app.errors import (
+    NotVaccinatedError, OutdatedVaccineError, NotWearingMaskError
+)
 
 
 class Cafe:
     def __init__(self, name: str) -> None:
         self.name = name
 
-    def visit_cafe(self, visitor: dict):
+    def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor.keys():
             raise NotVaccinatedError("All friends should be vaccinated")
         if visitor["vaccine"]["expiration_date"] < datetime.date.today():
             raise OutdatedVaccineError("All friends should be vaccinated")
         if not visitor["wearing_a_mask"]:
             raise NotWearingMaskError("You need to wear a mask!")
-        else:
-            return f"Welcome to {self.name}"
+        return f"Welcome to {self.name}"
