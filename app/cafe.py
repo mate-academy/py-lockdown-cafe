aa20b9ea59f27cea74b95a1ad900c16116ceb267
Edik Marushchak <e.moscow138@gmail.com>
2022-10-01 10:34:39 +0300

    'Solution'

--- app/cafe.py
+++ app/cafe.py
@@ -1,6 +1,9 @@
 from datetime import date
-from app.errors import NotVaccinatedError, \
-    OutdatedVaccineError, NotWearingMaskError
+from app.errors import (
+    NotVaccinatedError,
+    OutdatedVaccineError,
+    NotWearingMaskError
+)
 
 
 class Cafe:
