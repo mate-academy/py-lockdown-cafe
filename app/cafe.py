1ae0fe4583584382dbafe8c2b0f5eba30487e72e
LavrykN <nlavryk@kaaiot.io>
2022-10-11 15:10:47 +0300

    Refactoring

--- app/cafe.py
+++ app/cafe.py
@@ -11,9 +11,8 @@ class Cafe:
     def visit_cafe(self, visitor: dict) -> str | None:
         if "vaccine" not in visitor:
             raise NotVaccinatedError("Visitor is not vaccinated")
-        elif visitor["vaccine"]["expiration_date"] < datetime.date.today():
+        if visitor["vaccine"]["expiration_date"] < datetime.date.today():
             raise OutdatedVaccineError("Expired vaccine")
-        elif not visitor["wearing_a_mask"]:
+        if not visitor["wearing_a_mask"]:
             raise NotWearingMaskError("Visitor must wear a mask")
-        else:
-            return f"Welcome to {self.name}"
+        return f"Welcome to {self.name}"
