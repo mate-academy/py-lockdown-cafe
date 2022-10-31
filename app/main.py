58d28c5212de89b3af6b06331d1deaf2ac035db1
Denys Tereshchuk <terrya@ukr.net>
2022-10-31 11:47:04 +0400

    removed complication

--- app/main.py
+++ app/main.py
@@ -6,19 +6,14 @@ from app.errors import (
 
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
-    all_ok = 0
     masks_to_buy = 0
-    should_to_vaccinate = False
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
-            all_ok += 1
         except VaccineError:
-            should_to_vaccinate = True
+            return f"{VaccineError()}"
         except NotWearingMaskError:
             masks_to_buy += 1
-    if should_to_vaccinate:
-        return f"{VaccineError()}"
     if masks_to_buy:
         return f"Friends should buy {masks_to_buy} masks"
     return f"Friends can go to {cafe.name}"
