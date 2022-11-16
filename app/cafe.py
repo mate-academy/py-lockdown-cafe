6295e38df0609e53954478a3cb6eabc568717dd9
Admin <winofkill@gmail.com>
2022-11-16 21:54:03 +0400

    if not ---> get

--- app/cafe.py
+++ app/cafe.py
@@ -1,4 +1,5 @@
-from app.errors import NotVaccinatedError, \
+from app.errors import \
+    NotVaccinatedError, \
     OutdatedVaccineError, \
     NotWearingMaskError
 import datetime
@@ -9,16 +10,14 @@ class Cafe:
         self.name = name
 
     def visit_cafe(self, visitor: dict) -> str:
+        if not visitor.get("vaccine"):
+            raise NotVaccinatedError("All visitors "
+                                     "must be vaccinated! Please do it")
         try:
-            visitor["vaccine"]
-        except KeyError:
-            raise NotVaccinatedError("All friends should be vaccinated")
-        try:
-            date = visitor["vaccine"].get("expiration_date")
+            date = visitor.get("vaccine").get("expiration_date")
             assert date >= datetime.date.today()
-        except AssertionError:
-            raise OutdatedVaccineError("OutdatedVaccineError")
-        if visitor["wearing_a_mask"] is not True:
-            raise NotWearingMaskError("NotWearingMaskError")
-        else:
-            return f"Welcome to {self.name}"
+        except (AssertionError, AttributeError):
+            raise OutdatedVaccineError("Vaccine expired! Go home , dude ! ")
+        if visitor["wearing_a_mask"] is False:
+            raise NotWearingMaskError("Buy a mask, don't infect others !")
+        return f"Welcome to {self.name}"
--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from app.cafe import Cafe
-from app.errors import NotVaccinatedError, \
-    OutdatedVaccineError, \
+from app.errors import \
+    VaccineError, \
     NotWearingMaskError
 
 
@@ -9,14 +9,13 @@ def go_to_cafe(friends: list, cafe: Cafe) -> str:
     friends_without_vaccine = 0
     for person in friends:
         try:
-            Cafe.visit_cafe(cafe, person)
+            cafe.visit_cafe(person)
         except NotWearingMaskError:
             friends_without_mask += 1
-        except (NotVaccinatedError, OutdatedVaccineError):
+        except VaccineError:
             friends_without_vaccine += 1
             return "All friends should be vaccinated"
 
     if friends_without_mask == 0 and friends_without_vaccine == 0:
         return "Friends can go to KFC"
-    else:
-        return f"Friends should buy {friends_without_mask} masks"
+    return f"Friends should buy {friends_without_mask} masks"
