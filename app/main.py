f2aeed093c20f671b5281d51bf240bf66d3d6f39
Vladyslav <jp6@mail.ru>
2022-11-26 12:12:23 +0000

    fix

--- app/main.py
+++ app/main.py
@@ -8,14 +8,10 @@ def go_to_cafe(friends: list, cafe: Cafe) -> str:
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
-
         except VaccineError:
             return "All friends should be vaccinated"
-
         except NotWearingMaskError:
             masks += 1
-
     if masks > 0:
         return f"Friends should buy {masks} masks"
-
     return f"Friends can go to {cafe.name}"
