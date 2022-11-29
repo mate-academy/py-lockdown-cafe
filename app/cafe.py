b643517100e6e899321261bf7f1210c1667370e5
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-11-29 13:52:24 +0200

    created errors and cafe modul

---
+++ app/cafe.py
@@ -0,0 +1,22 @@
+from errors import NotVaccinatedError
+from errors import OutdatedVaccineError
+from errors import NotWearingMaskError
+import datetime
+
+
+class Cafe:
+    def __init__(self, name) -> None:
+        self.name = name
+
+
+    def visit_cafe(self, visitor: dict):
+        if not visitor["vaccine"]:
+            raise NotVaccinatedError
+        for key in visitor.keys():
+            if visitor["vaccine"] != datetime.date.today():
+                raise OutdatedVaccineError
+        if visitor["wearing_a_mask"] is False:
+            raise NotWearingMaskError
+
+        else:
+            return f"Welcome to {self.name}"
---
+++ app/errors.py
@@ -0,0 +1,10 @@
+class OutdatedVaccineError(Exception):
+    pass
+
+
+class NotWearingMaskError(Exception):
+    pass
+
+
+class NotVaccinatedError(Exception):
+    pass
