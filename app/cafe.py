cec85782314717f0d9e37fd036242946b61e4e5e
Vika <viktorykoroliova@gmail.com>
2022-12-04 13:06:56 +0200

    Fixed solution

--- app/cafe.py
+++ app/cafe.py
@@ -1,5 +1,9 @@
 import datetime
-from app import errors
+from app.errors import (
+    NotVaccinatedError,
+    OutdatedVaccineError,
+    NotWearingMaskError
+)
 
 
 class Cafe:
@@ -8,13 +12,16 @@ class Cafe:
 
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
-            raise errors.NotVaccinatedError(
-                "Everyone should be vaccinated")
+            raise NotVaccinatedError(
+                "Everyone should be vaccinated"
+            )
         current_date = datetime.date.today()
         if visitor["vaccine"]["expiration_date"] < current_date:
-            raise errors.OutdatedVaccineError(
-                "Everyone should have a valid vaccine")
+            raise OutdatedVaccineError(
+                "Everyone should have a valid vaccine"
+            )
         if not visitor["wearing_a_mask"]:
-            raise errors.NotWearingMaskError(
-                "Everyone should wear a face mask")
+            raise NotWearingMaskError(
+                "Everyone should wear a face mask"
+            )
         return f"Welcome to {self.name}"
--- app/main.py
+++ app/main.py
@@ -1,20 +1,19 @@
-from app import errors
 from app.cafe import Cafe
+from app.errors import (
+    VaccineError,
+    NotWearingMaskError
+)
 
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
-    count_vaccine = 0
     count_mask = 0
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
-        except errors.VaccineError:
-            count_vaccine += 1
-        except errors.NotWearingMaskError:
+        except VaccineError:
+            return "All friends should be vaccinated"
+        except NotWearingMaskError:
             count_mask += 1
-    if count_mask == 0 and count_vaccine == 0:
-        return f"Friends can go to {cafe.name}"
-    if count_vaccine > 0:
-        return "All friends should be vaccinated"
-    if count_vaccine == 0 and count_mask > 0:
+    if count_mask > 0:
         return f"Friends should buy {count_mask} masks"
+    return f"Friends can go to {cafe.name}"
