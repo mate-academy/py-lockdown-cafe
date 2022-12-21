f2f1f2f54c7fee9b093c38f168e7309e92115104
Haishuk Pavlo <xomas@ukr.net>
2022-12-21 02:57:25 +0200

    Done (with one cycle :))

--- app/main.py
+++ app/main.py
@@ -7,21 +7,14 @@ from app.errors import (
 
 
 def go_to_cafe(friends: list[dict], cafe: Cafe) -> str | VaccineError:
+    unmasked = 0
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
         except VaccineError:
             return "All friends should be vaccinated"
         except NotWearingMaskError:
-            continue
-
-    for friend in friends:
-        try:
-            cafe.visit_cafe(friend)
-        except NotWearingMaskError:
-            unmasked = 0
-            for some_friend in friends:
-                if some_friend["wearing_a_mask"] is False:
-                    unmasked += 1
-            return f"Friends should buy {unmasked} masks"
+            unmasked += 1
+    if unmasked >= 1:
+        return f"Friends should buy {unmasked} masks"
     return f"Friends can go to {cafe.name}"
