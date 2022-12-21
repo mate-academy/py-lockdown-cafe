bbd5781199d92e19cf11be03f46aa9265bf1e5f7
oleg-potichnyi <potichnyi.oleg@gmail.com>
2022-12-21 17:24:44 +0200

    Comments deleted

--- app/main.py
+++ app/main.py
@@ -5,8 +5,6 @@ from app.errors import NotWearingMaskError
 
 
 def go_to_cafe(friends: list, cafe: Cafe) -> Any:
-    # cafe_name = Cafe(cafe)
-    # print(cafe_name.name)
     masks_to_buy = 0
     for friend in friends:
         try:
@@ -15,7 +13,6 @@ def go_to_cafe(friends: list, cafe: Cafe) -> Any:
             return "All friends should be vaccinated"
         except NotWearingMaskError:
             masks_to_buy += 1
-
-    if masks_to_buy == 0:
-        return f"Friends can go to {cafe.name}"
-    return f"Friends should buy {masks_to_buy} masks"
+    if masks_to_buy != 0:
+        return f"Friends should buy {masks_to_buy} masks"
+    return f"Friends can go to {cafe.name}"
