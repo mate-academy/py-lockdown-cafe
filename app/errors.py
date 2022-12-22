c8c80dd52211a59d992c4231e106736e94494760
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2022-12-22 17:45:28 +0200

    Solution

---
+++ app/errors.py
@@ -0,0 +1,17 @@
+class VaccineError(Exception):
+
+    def __str__(self) -> str:
+        return "All friends should be vaccinated"
+
+
+class NotVaccinatedError(VaccineError):
+    pass
+
+
+class OutdatedVaccineError(VaccineError):
+    pass
+
+
+class NotWearingMaskError(Exception):
+    def __str__(self) -> str:
+        return "Friends should buy"
--- app/main.py
+++ app/main.py
@@ -1,71 +1,19 @@
-import datetime
+from app.cafe import Cafe
+from app.errors import VaccineError, NotWearingMaskError
 
 
-class VaccineError(Exception):
-
-    def __str__(self):
-        return "All friends should be vaccinated"
-
-
-class NotVaccinatedError(VaccineError):
-    pass
-
-
-class OutdatedVaccineError(VaccineError):
-    pass
-
-
-class NotWearingMaskError(Exception):
-    pass
-
-
-class Cafe:
-    def __init__(self, name: str) -> None:
-        self.name = name
-
-    def visit_cafe(self, visitor: dict) -> None:
-        masks_to_buy = 0
-        if "vaccine" not in visitor:
-            raise NotVaccinatedError
-        elif visitor["vaccine"]["expiration_date"] < datetime.date.today():
-            raise OutdatedVaccineError
-        elif not visitor["wearing_a_mask"]:
-            masks_to_buy += 1
-            raise NotWearingMaskError(f"Friends should buy {masks_to_buy} masks")
-        else:
-            print(f"Welcome to {self.name}")
-
-
-def go_to_cafe(friends: list, cafe: Cafe):
-    num = 0
+def go_to_cafe(friends: list, cafe: Cafe) -> str:
+    masks_to_buy = 0
+    num_of_errors = 0
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
-        except VaccineError as e:
-            num += 1
-            print(e)
-        except NotWearingMaskError as e:
-            num += 1
-            print(e)
-    if num == 0:
-        print(f"Friends can go to {cafe.name}")
-
-
-friends = [
-    {
-        "name": "Alisa",
-        "vaccine": {
-            "expiration_date": datetime.date.today()
-        },
-        "wearing_a_mask": False
-    },
-    {
-        "name": "Bob",
-        "vaccine": {
-            "expiration_date": datetime.date.today()
-        },
-        "wearing_a_mask": False
-    },
-]
-go_to_cafe(friends, Cafe("KFC"))
-
+        except VaccineError:
+            num_of_errors += 1
+            return "All friends should be vaccinated"
+        except NotWearingMaskError:
+            masks_to_buy += 1
+            num_of_errors += 1
+    if num_of_errors == 0:
+        return f"Friends can go to {cafe.name}"
+    return f"Friends should buy {masks_to_buy} masks"
