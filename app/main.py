603a927fc816267179b7617ddaca5cf6ec9b45e3
Anna Kabatova <kabatova.anna@ukr.net>
2022-12-30 17:46:07 +0200

    3rd try solution (lockdown cafe)

--- app/main.py
+++ app/main.py
@@ -3,15 +3,15 @@ from app.cafe import Cafe
 
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
-    m_error = 0
+    count_mask_error = 0
     for person in friends:
         try:
             cafe.visit_cafe(person)
         except VaccineError:
             return "All friends should be vaccinated"
         except NotWearingMaskError:
-            m_error += 1
-    if m_error != 0:
-        return f"Friends should buy {m_error} masks"
+            count_mask_error += 1
+    if count_mask_error != 0:
+        return f"Friends should buy {count_mask_error} masks"
     else:
         return f"Friends can go to {cafe.name}"
