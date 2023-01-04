9f88f9f86954c6bf38fb3576d9a416d40ee6dd95
drapail <mv.dibrov@gmail.com>
2023-01-04 21:41:06 +0100

    implemented solution

---
+++ app/cafe.py
@@ -0,0 +1,20 @@
+from datetime import date
+
+from app.errors import (NotVaccinatedError, OutdatedVaccineError,
+                        NotWearingMaskError)
+
+
+class Cafe:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def visit_cafe(self, visitor: dict) -> str:
+        if "vaccine" not in visitor.keys():
+            raise NotVaccinatedError(f"{visitor['name']} is not vaccinated")
+        if visitor["vaccine"]["expiration_date"] < date.today():
+            raise OutdatedVaccineError(f"{visitor['name']} "
+                                       f"has outdated vaccine")
+        if not visitor["wearing_a_mask"]:
+            raise NotWearingMaskError(f"{visitor['name']} "
+                                      f"is not wearing a mask")
+        return f"Welcome to {self.name}"
--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+from app.cafe import Cafe
+from app.errors import (NotWearingMaskError, VaccineError)
+
+
+def go_to_cafe(friends: list, cafe: Cafe) -> str:
+    not_wearing_mask = []
+    counter = 0
+    for friend in friends:
+        counter += 1
+        try:
+            cafe.visit_cafe(friend)
+        except VaccineError:
+            return "All friends should be vaccinated"
+        except NotWearingMaskError as e:
+            not_wearing_mask.append(e)
+        if counter == len(friends) and len(not_wearing_mask) >= 1:
+            return f"Friends should buy {len(not_wearing_mask)} masks"
+    return f"Friends can go to {cafe.name}"
