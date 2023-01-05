5600d04c87bc21bc140073fc87b8f49958690bc5
6aPa6aH <6apa6ah666@gmail.com>
2023-01-05 09:18:01 +0200

    fix_code2

--- app/cafe.py
+++ app/cafe.py
@@ -12,12 +12,11 @@ class Cafe:
 
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
-            raise NotVaccinatedError("You are not vaccinated."
-                                     " Please get vaccinated!")
-        if datetime.date.today() > \
-                visitor["vaccine"]["expiration_date"]:
-            raise OutdatedVaccineError("Your vaccination"
-                                       " period is overdue!")
+            raise NotVaccinatedError(
+                "You are not vaccinated. Please get vaccinated!"
+            )
+        if datetime.date.today() > visitor["vaccine"]["expiration_date"]:
+            raise OutdatedVaccineError("Your vaccination period is overdue!")
         if visitor["wearing_a_mask"] is False:
             raise NotWearingMaskError("Please wear or buy a mask!")
         return f"Welcome to {self.name}"
--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 from app.cafe import Cafe
 from app.errors import (
-    NotVaccinatedError,
-    OutdatedVaccineError,
+    VaccineError,
     NotWearingMaskError,
 )
 
@@ -11,9 +10,7 @@ def go_to_cafe(friends: list, cafe: Cafe) -> str:
     for friend in friends:
         try:
             Cafe.visit_cafe(cafe, friend)
-        except NotVaccinatedError:
-            return "All friends should be vaccinated"
-        except OutdatedVaccineError:
+        except VaccineError:
             return "All friends should be vaccinated"
         except NotWearingMaskError:
             masks_to_buy += 1
