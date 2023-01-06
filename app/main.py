8bf05b9a84ed9683ba461ea22ed63a4d67389414
6aPa6aH <6apa6ah666@gmail.com>
2023-01-06 09:33:53 +0200

    fix_code3

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def go_to_cafe(friends: list, cafe: Cafe) -> str:
     masks_to_buy = 0
     for friend in friends:
         try:
-            Cafe.visit_cafe(cafe, friend)
+            cafe.visit_cafe(friend)
         except VaccineError:
             return "All friends should be vaccinated"
         except NotWearingMaskError:
