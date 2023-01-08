3e5e874dc45fbc663cd2de2afe0970e575fa92cb
Andrii Skryl <a.skryl92@gmail.com>
2023-01-08 20:28:20 +0200

    Solution 2

--- app/cafe.py
+++ app/cafe.py
@@ -9,15 +9,10 @@ class Cafe:
         self.name = name
 
     def visit_cafe(self, visitor: dict) -> str:
-        rules_violated = 0
         if "vaccine" not in visitor:
-            rules_violated += 1
             raise NotVaccinatedError("The visitor is not vaccinated")
         if visitor["vaccine"]["expiration_date"] < datetime.date.today():
-            rules_violated += 1
             raise OutdatedVaccineError("The visitor's vaccine is expired")
         if not visitor["wearing_a_mask"]:
-            rules_violated += 1
             raise NotWearingMaskError("The visitor is not wearing a mask")
-        if rules_violated == 0:
-            return f"Welcome to {self.name}"
+        return f"Welcome to {self.name}"
--- app/main.py
+++ app/main.py
@@ -3,18 +3,14 @@ from app.errors import VaccineError, NotWearingMaskError
 
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
-    problems_with_vaccines = 0
     masks_to_buy = 0
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
         except VaccineError:
-            problems_with_vaccines += 1
+            return "All friends should be vaccinated"
         except NotWearingMaskError:
             masks_to_buy += 1
-    if problems_with_vaccines > 0:
-        return "All friends should be vaccinated"
     if masks_to_buy > 0:
         return f"Friends should buy {masks_to_buy} masks"
-    if problems_with_vaccines == 0 and masks_to_buy == 0:
-        return f"Friends can go to {cafe.name}"
+    return f"Friends can go to {cafe.name}"
