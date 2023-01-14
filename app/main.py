6bfac0b40f098eeada57453851d0fd2eb2d43c69
Alina Boichenko <ilub1108@gmail.com>
2023-01-14 11:12:39 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -7,10 +7,8 @@ def go_to_cafe(friends: list, cafe: Cafe) -> str:
     for visitor in friends:
         try:
             Cafe.visit_cafe(cafe, visitor)
-
         except VaccineError:
             return "All friends should be vaccinated"
-
         except NotWearingMaskError:
             masks_to_buy += 1
 
