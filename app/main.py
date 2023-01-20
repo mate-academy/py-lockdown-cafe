d7569e81dcd5954f04c0e76782fa804c7ccf0dae
yura.vataschuk <yura.vataschuk@gmail.com>
2023-01-20 10:14:32 +0200

    Solution 2

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from app.cafe import Cafe
-from app.errors import VaccineError, NotWearingMaskError
+from app.errors import VaccineError
+from app.errors import NotWearingMaskError
 
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
