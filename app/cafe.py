baa996b7a97fa4e016ed5314acefa2626570132c
Tymur Semelinskyi <t.semelinsky@gmail.com>
2023-01-21 21:12:31 +0100

    'Solution2'

--- app/cafe.py
+++ app/cafe.py
@@ -11,8 +11,12 @@ class Cafe:
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
             raise NotVaccinatedError
+        if "expiration_date" not in visitor["vaccine"]:
+            raise NotVaccinatedError
         if visitor["vaccine"]["expiration_date"] < datetime.date.today():
             raise OutdatedVaccineError
+        if "wearing_a_mask" not in visitor:
+            raise NotWearingMaskError
         if visitor["wearing_a_mask"] is False:
             raise NotWearingMaskError
         return f"Welcome to {self.name}"
