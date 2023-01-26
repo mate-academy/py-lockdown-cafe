76480cf43b861ccaf4f1b20cb58433e958b4297c
Taras Savchyn <savik1992@gmail.com>
2023-01-26 10:47:56 +0200

    change solution

--- app/main.py
+++ app/main.py
@@ -4,11 +4,18 @@ import datetime
 
 
 def sorted_friends(friend: dict) -> int:
-    return 0 if not friend.get("vaccine", False) or \
-        friend["vaccine"]["expiration_date"] < datetime.date.today() else 1
+    """I don't know how else to solve this task without using this function,
+    I tried for two days"""
+
+    return (
+        0 if not friend.get("vaccine")
+        or friend["vaccine"]["expiration_date"] < datetime.date.today()
+        else 1
+    )
 
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
+
     count = sum([1 for person in friends if not person["wearing_a_mask"]])
     try:
         [cafe.visit_cafe(friend)
