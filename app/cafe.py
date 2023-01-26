da307887cf31d6de6679667b7be32e90e6ec1379
Dmytro Martyshevskiy <codingeo@gmail.com>
2023-01-26 15:44:45 +0200

    change dict get value method

--- app/cafe.py
+++ app/cafe.py
@@ -14,8 +14,9 @@ class Cafe:
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
             raise NotVaccinatedError("Visitor is not vaccinated")
-        if visitor["vaccine"]["expiration_date"] < datetime.date.today():
+        if (visitor.get("vaccine").get("expiration_date")
+                < datetime.date.today()):
             raise OutdatedVaccineError("Visitor has expired vaccine")
-        if not visitor["wearing_a_mask"]:
+        if not visitor.get("wearing_a_mask"):
             raise NotWearingMaskError("Visitor is not wearing mask")
         return f"Welcome to {self.name}"
