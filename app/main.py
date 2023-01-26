95e7525ded55d4d5bf3f2dffa960bce9492811fe
Taras Savchyn <savik1992@gmail.com>
2023-01-26 18:30:28 +0200

    change code

--- app/main.py
+++ app/main.py
@@ -1,27 +1,19 @@
 from app.cafe import Cafe
 from app.errors import VaccineError, NotWearingMaskError
-import datetime
-
-
-def sorted_friends(friend: dict) -> int:
-    """I don't know how else to solve this task without using this function,
-    I tried for two days"""
-
-    return (
-        0 if not friend.get("vaccine")
-        or friend["vaccine"]["expiration_date"] < datetime.date.today()
-        else 1
-    )
 
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
-
-    count = sum([1 for person in friends if not person["wearing_a_mask"]])
-    try:
-        [cafe.visit_cafe(friend)
-         for friend in sorted(friends, key=sorted_friends)]
-    except VaccineError:
+    vac_errors = []
+    mask_errors = []
+    for friend in friends:
+        try:
+            cafe.visit_cafe(friend)
+        except VaccineError:
+            vac_errors.append(None)
+        except NotWearingMaskError:
+            mask_errors.append(None)
+    if vac_errors:
         return "All friends should be vaccinated"
-    except NotWearingMaskError:
-        return f"Friends should buy {count} masks"
+    elif mask_errors:
+        return f"Friends should buy {len(mask_errors)} masks"
     return f"Friends can go to {cafe.name}"
