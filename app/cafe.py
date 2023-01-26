99fc9f50e11c59ff30cbe76a8ef318b8bc0e3aff
Dmytro Martyshevskiy <codingeo@gmail.com>
2023-01-26 15:36:07 +0200

    update error messages

--- app/cafe.py
+++ app/cafe.py
@@ -13,9 +13,9 @@ class Cafe:
 
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
-            raise NotVaccinatedError("NotVaccinatedError")
+            raise NotVaccinatedError("Visitor is not vaccinated")
         if visitor["vaccine"]["expiration_date"] < datetime.date.today():
-            raise OutdatedVaccineError("OutdatedVaccineError")
+            raise OutdatedVaccineError("Visitor has expired vaccine")
         if not visitor["wearing_a_mask"]:
-            raise NotWearingMaskError("NotWearingMaskError")
+            raise NotWearingMaskError("Visitor is not wearing mask")
         return f"Welcome to {self.name}"
