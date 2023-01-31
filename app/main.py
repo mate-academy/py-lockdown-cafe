9509ab955b72128428e732213bf7b2f98739d89f
Taras Savchyn <savik1992@gmail.com>
2023-01-31 14:03:14 +0200

    reformat code

--- app/main.py
+++ app/main.py
@@ -3,17 +3,14 @@ from app.errors import VaccineError, NotWearingMaskError
 
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
-    vac_errors = []
-    mask_errors = []
+    count_masks = 0
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
         except VaccineError:
-            vac_errors.append(None)
+            return "All friends should be vaccinated"
         except NotWearingMaskError:
-            mask_errors.append(None)
-    if vac_errors:
-        return "All friends should be vaccinated"
-    elif mask_errors:
-        return f"Friends should buy {len(mask_errors)} masks"
+            count_masks += 1
+    if count_masks > 0:
+        return f"Friends should buy {count_masks} masks"
     return f"Friends can go to {cafe.name}"
