e3ab396774a20244b212f797e3bcdc788797d269
Vitalii <fahivchik1@gmail.com>
2023-02-01 13:21:35 +0200

    'Solution2'

--- app/cafe.py
+++ app/cafe.py
@@ -1,9 +1,10 @@
+import datetime
+
 from app.errors import (
     NotVaccinatedError,
     OutdatedVaccineError,
     NotWearingMaskError
 )
-import datetime
 
 
 class Cafe:
@@ -12,9 +13,9 @@ class Cafe:
 
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
-            raise NotVaccinatedError()
+            raise NotVaccinatedError("All friends should be vaccinated")
         if visitor["vaccine"]["expiration_date"] < datetime.date.today():
-            raise OutdatedVaccineError()
+            raise OutdatedVaccineError("All friends should be vaccinated")
         if visitor["wearing_a_mask"] is False:
-            raise NotWearingMaskError()
+            raise NotWearingMaskError(" ")
         return f"Welcome to {self.name}"
--- app/errors.py
+++ app/errors.py
@@ -1,21 +1,14 @@
 class VaccineError(Exception):
-    def __init__(self) -> None:
-        self.message = "All friends should be vaccinated"
-        super().__init__(self.message)
+    pass
 
 
 class NotVaccinatedError(VaccineError):
-    def __init__(self) -> None:
-        super().__init__()
+    pass
 
 
 class OutdatedVaccineError(VaccineError):
-    def __init__(self) -> None:
-        super().__init__()
+    pass
 
 
 class NotWearingMaskError(Exception):
-    def __init__(self, masks_to_buy: int = 1) -> None:
-        self.masks_to_buy = masks_to_buy
-        self.message = f"Friends should buy {self.masks_to_buy} masks"
-        super().__init__(self.message)
+    pass
