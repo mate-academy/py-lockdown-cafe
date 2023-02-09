a6c11e96c0dd64d1bb8908d5f44d9755308a6722
Oleksandr Lysanets <oleksandrlysanets@gmail.com>
2023-02-09 16:01:54 +0200

    Solution

--- app/cafe.py
+++ app/cafe.py
@@ -16,7 +16,8 @@ class Cafe:
         if "vaccine" not in visitor:
             raise NotVaccinatedError("Visitor must have the vaccine")
         expiration_date = visitor.get("vaccine", dict()).get("expiration_date")
-        if expiration_date != {} and expiration_date < datetime.date.today():
+        if (expiration_date is not None
+                and expiration_date < datetime.date.today()):
             raise OutdatedVaccineError("The vaccine must not be expired")
         if not visitor.get("wearing_a_mask"):
             raise NotWearingMaskError("Visitor must wear a mask")
