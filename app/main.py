2cfa2161db1eeb50ba7571cef2b4ac693501ddb1
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-02-13 18:20:58 +0200

    correct naming

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
+from __future__ import annotations
 from .cafe import Cafe
 from .errors import (VaccineError,
                      NotWearingMaskError)
 
 
-def go_to_cafe(friends: list, cafe: Cafe) -> (None, str):
-    masks_to_buy = sum(1 for j in friends if not j["wearing_a_mask"])
+def go_to_cafe(friends: list, cafe: Cafe) -> None | str:
+    masks_to_buy = sum(1 for friend in friends
+                       if not friend.get("wearing_a_mask"))
     is_mask = True
     for friend in friends:
         try:
