5d95ddd26fe9ebc91c0efcbceaf01cb29167de57
Max Bilokur <bilmaxim@gmail.com>
2023-02-16 21:28:55 +0200

    fixed imports and removed date variable

--- app/cafe.py
+++ app/cafe.py
@@ -1,7 +1,10 @@
-import datetime
+from datetime import date
 
-from app.errors import (NotVaccinatedError, OutdatedVaccineError,
-                        NotWearingMaskError)
+from app.errors import (
+    NotVaccinatedError,
+    OutdatedVaccineError,
+    NotWearingMaskError
+)
 
 
 class Cafe:
@@ -11,8 +14,7 @@ class Cafe:
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
             raise NotVaccinatedError
-        today = datetime.date.today()
-        if visitor["vaccine"]["expiration_date"] < today:
+        if visitor["vaccine"]["expiration_date"] < date.today():
             raise OutdatedVaccineError
         if not visitor["wearing_a_mask"]:
             raise NotWearingMaskError
