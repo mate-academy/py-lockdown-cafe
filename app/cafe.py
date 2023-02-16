e7e81ca507e01f1d27ca9acaeed747e8ff975681
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-02-16 21:31:22 +0200

    fixed some bugs

--- app/cafe.py
+++ app/cafe.py
@@ -12,13 +12,13 @@ class Cafe:
         today_date = datetime.date.today()
 
         if "vaccine" not in visitor or not visitor.get("vaccine"):
-            raise NotVaccinatedError("You must have a vaccine")
+            raise NotVaccinatedError(f"{visitor} you must have a vaccine")
 
         expiration_date = visitor["vaccine"]["expiration_date"]
         if expiration_date < today_date:
-            raise OutdatedVaccineError("Your vaccine is expired")
+            raise OutdatedVaccineError(f"{visitor} your vaccine is expired")
 
         if not visitor.get("wearing_a_mask"):
-            raise NotWearingMaskError("You must buy and wear a mask")
+            raise NotWearingMaskError(f"{visitor} you must buy a mask")
 
         return f"Welcome to {self.name}"
--- app/main.py
+++ app/main.py
@@ -4,10 +4,9 @@ from .errors import (VaccineError,
                      NotWearingMaskError)
 
 
-def go_to_cafe(friends: list, cafe: Cafe) -> None | str:
-    masks_to_buy = sum(1 for friend in friends
-                       if not friend.get("wearing_a_mask"))
-    is_mask = True
+def go_to_cafe(friends: list, cafe: Cafe) -> str:
+    masks_to_buy = 0
+
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
@@ -16,9 +15,9 @@ def go_to_cafe(friends: list, cafe: Cafe) -> None | str:
             return "All friends should be vaccinated"
 
         except NotWearingMaskError:
-            is_mask = False
+            masks_to_buy += 1
 
-    if not is_mask:
+    if masks_to_buy:
         return f"Friends should buy {masks_to_buy} masks"
 
     return f"Friends can go to {cafe.name}"
