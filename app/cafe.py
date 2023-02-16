0c9b7a11824a06b7bb40c0ba914b31cb09f2f8fc
sdobzhynetskyi <sergii.dobzhynetskyi@gmail.com>
2023-02-16 15:30:05 +0200

    solved task- flake8- corrections added

--- app/cafe.py
+++ app/cafe.py
@@ -1,7 +1,7 @@
-from app.errors import \
-    NotWearingMaskError,\
-    NotVaccinatedError,\
-    OutdatedVaccineError
+from app.errors import (NotWearingMaskError,
+                        NotVaccinatedError,
+                        OutdatedVaccineError
+                        )
 import datetime
 
 
@@ -12,10 +12,11 @@ class Cafe:
     def visit_cafe(self, person: dict) -> str:
         if "vaccine" not in person.keys():
             raise NotVaccinatedError("All friends should be vaccinated")
-        if person["vaccine"]["expiration_date"] < datetime.date.today():
+        if person.get("vaccine").get("expiration_date")\
+                < datetime.date.today():
             raise OutdatedVaccineError("All friends should be vaccinated")
         if "wearing_a_mask" not in person.keys() \
-                or person["wearing_a_mask"] is False:
+                or person.get("wearing_a_mask") is False:
             raise NotWearingMaskError("Friend should wear the mask")
         else:
             return f"Welcome to {self.name}"
--- app/main.py
+++ app/main.py
@@ -3,18 +3,15 @@ from app.cafe import Cafe
 
 
 def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
-    vaccinated = True
     mask_needed_counter = 0
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
         except VaccineError:
-            vaccinated = False
+            return "All friends should be vaccinated"
         except NotWearingMaskError:
             mask_needed_counter += 1
-    if vaccinated is False:
-        return "All friends should be vaccinated"
-    if mask_needed_counter == 0 and vaccinated is True:
+    if mask_needed_counter == 0:
         return f"Friends can go to {cafe.name}"
     if mask_needed_counter > 0:
         return f"Friends should buy {mask_needed_counter} masks"
