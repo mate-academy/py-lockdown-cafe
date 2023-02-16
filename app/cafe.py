e4aaae217223db640c84ccb8e3d42c69468293ae
sdobzhynetskyi <sergii.dobzhynetskyi@gmail.com>
2023-02-16 12:40:37 +0200

    solved task

---
+++ app/cafe.py
@@ -0,0 +1,21 @@
+from app.errors import \
+    NotWearingMaskError,\
+    NotVaccinatedError,\
+    OutdatedVaccineError
+import datetime
+
+
+class Cafe:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def visit_cafe(self, person: dict) -> str:
+        if "vaccine" not in person.keys():
+            raise NotVaccinatedError("All friends should be vaccinated")
+        if person["vaccine"]["expiration_date"] < datetime.date.today():
+            raise OutdatedVaccineError("All friends should be vaccinated")
+        if "wearing_a_mask" not in person.keys() \
+                or person["wearing_a_mask"] is False:
+            raise NotWearingMaskError("Friend should wear the mask")
+        else:
+            return f"Welcome to {self.name}"
--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+from app.errors import NotWearingMaskError, VaccineError
+from app.cafe import Cafe
+
+
+def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
+    vaccinated = True
+    mask_needed_counter = 0
+    for friend in friends:
+        try:
+            cafe.visit_cafe(friend)
+        except VaccineError:
+            vaccinated = False
+        except NotWearingMaskError:
+            mask_needed_counter += 1
+    if vaccinated is False:
+        return "All friends should be vaccinated"
+    if mask_needed_counter == 0 and vaccinated is True:
+        return f"Friends can go to {cafe.name}"  
+    if mask_needed_counter > 0:
+        return f"Friends should buy {mask_needed_counter} masks"
