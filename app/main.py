a144e17dd4a53a4271314d27e5a790e765c8bf41
Yevhen Kaskov <kaskov.e@gmail.com>
2023-02-17 12:40:32 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+from __future__ import annotations
+import datetime
+from app.cafe import Cafe
+
+
+def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
+    vaccinated_friends = (
+        [friend for friend in friends if
+         "vaccine" in friend and friend["vaccine"]["expiration_date"]
+         >= datetime.date.today()]
+    )
+    non_masked_friends = (
+        [friend for friend in vaccinated_friends
+         if not friend["wearing_a_mask"]]
+    )
+
+    if len(vaccinated_friends) != len(friends):
+        return "All friends should be vaccinated"
+    elif non_masked_friends:
+        return f"Friends should buy {len(non_masked_friends)} masks"
+    else:
+        return f"Friends can go to {cafe.name}"
