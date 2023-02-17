c3fa0fe1588e8814cc347df9efed28715356db74
Yulia <Mala>
2023-02-18 00:05:17 +0200

    -

--- app/cafe.py
+++ app/cafe.py
@@ -9,14 +9,11 @@ class Cafe:
         self.name = name
 
     def visit_cafe(self, visitor: dict) -> str:
-        try:
-            exp_date = visitor["vaccine"]["expiration_date"]
-        except KeyError:
-            raise NotVaccinatedError("visitor should be vaccinated")
-        try:
-            assert exp_date >= date.today()
-        except AssertionError:
-            raise OutdatedVaccineError("your vaccination is outdated ;(")
+        name = visitor['name']
+        if "vaccine" not in visitor:
+            raise NotVaccinatedError(f"{name} should be vaccinated")
+        if visitor["vaccine"]["expiration_date"] < date.today():
+            raise OutdatedVaccineError(f"{name}, your vaccination is outdated ;(")
         if not visitor["wearing_a_mask"]:
-            raise NotWearingMaskError("visitors are obliged to wear a mask")
+            raise NotWearingMaskError(f"{name}, you are obliged to wear a mask")
         return f"Welcome to {self.name}"
