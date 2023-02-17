a7375b07ef490a51986b2e3898a52a791256937e
Ada Kravchenko <ada.t.krav@gmail.com>
2023-02-17 15:01:14 +0200

    Solution

--- app/cafe.py
+++ app/cafe.py
@@ -11,7 +11,7 @@ class Cafe:
         self.name = name
 
     def visit_cafe(self, visitor: dict) -> str:
-        if "vaccine" not in visitor.keys():
+        if "vaccine" not in visitor:
             raise NotVaccinatedError(
                 f"Wow, {visitor['name']} is so late (cafe.py, line 16)."
             )
