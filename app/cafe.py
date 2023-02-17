fc898c76f7c9a51e0bd2478a30ee4898058b71af
Zenon <zenion@ukr.net>
2023-02-17 17:38:31 +0200

    Fix errors message

--- app/cafe.py
+++ app/cafe.py
@@ -12,9 +12,12 @@ class Cafe:
 
     def visit_cafe(self, visitor: dict) -> str:
         if visitor.get("vaccine") is None:
-            raise NotVaccinatedError("Visitor isn`t vaccinated")
+            raise NotVaccinatedError(f"Visitor {visitor['name']}"
+                                     f" isn`t vaccinated")
         if visitor["vaccine"]["expiration_date"] < date.today():
-            raise OutdatedVaccineError("Visitor have overdue vaccination")
+            raise OutdatedVaccineError(f"Visitor {visitor['name']}"
+                                       f" have overdue vaccination")
         if not visitor["wearing_a_mask"]:
-            raise NotWearingMaskError("Visitor don`t wear mask")
+            raise NotWearingMaskError(f"Visitor {visitor['name']}"
+                                      f" don`t wear mask")
         return f"Welcome to {self.name}"
