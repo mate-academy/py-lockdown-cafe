b423d8ebf81aa4b6fe159116890f3d6e74aca215
Evgen Shepelenko <ShepelenkoEV@gmail.com>
2023-02-17 13:43:15 +0200

    'Solution3'

--- app/cafe.py
+++ app/cafe.py
@@ -12,9 +12,7 @@ class Cafe:
         self.name = name
 
     def visit_cafe(self, visitor: dict) -> str:
-        if not visitor:
-            return
-        elif "vaccine" not in visitor:
+        if "vaccine" not in visitor:
             raise NotVaccinatedError("All friends should be vaccinated")
         elif visitor["vaccine"]["expiration_date"] < datetime.date.today():
             raise OutdatedVaccineError("All friends should be vaccinated")
