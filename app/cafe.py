0e40dacbe127bce88e621f3cef17caa152a6fc4a
Yevhen Kopatko <cs.kopatko.evgen@gmail.com>
2023-02-17 16:15:55 +0200

    'Solution'

---
+++ app/cafe.py
@@ -0,0 +1,23 @@
+import datetime
+from app.errors import (
+    NotVaccinatedError,
+    OutdatedVaccineError,
+    NotWearingMaskError
+)
+
+
+class Cafe:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def visit_cafe(self, visitor: dict) -> str:
+        if "vaccine" not in visitor.keys():
+            raise NotVaccinatedError("NotVaccinatedError")
+
+        if datetime.date.today() > visitor["vaccine"].get("expiration_date"):
+            raise OutdatedVaccineError("OutdatedVaccineError")
+
+        if visitor["wearing_a_mask"] is False:
+            raise NotWearingMaskError("NotWearingMaskError")
+
+        return f"Welcome to {self.name}"
