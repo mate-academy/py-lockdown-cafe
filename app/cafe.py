014d56bc7f4dc87742a38595386a76a7ee3fbba6
Vladimir Titarenko <tytarenkova@gmail.com>
2023-02-19 12:37:57 +0300

    v1 mistakes were fixed

--- app/cafe.py
+++ app/cafe.py
@@ -1,9 +1,10 @@
 import datetime
 from typing import Union
-from app.errors import \
-    NotWearingMaskError,\
-    OutdatedVaccineError,\
+from app.errors import (
+    NotWearingMaskError,
+    OutdatedVaccineError,
     NotVaccinatedError
+)
 
 
 class Cafe:
@@ -11,11 +12,11 @@ class Cafe:
         self.name = name
 
     def visit_cafe(self, visitor: dict) -> Union[str, None]:
-        try:
-            if datetime.date.today() > visitor["vaccine"]["expiration_date"]:
-                raise OutdatedVaccineError("The visitor is not vaccinated")
-        except KeyError:
+        if visitor.get("vaccine", {}).get("expiration_date") is None:
             raise NotVaccinatedError("The visitor is not vaccinated")
+        if datetime.date.today() >\
+                visitor.get("vaccine", {}).get("expiration_date"):
+            raise OutdatedVaccineError("Overdue vaccination")
         if not visitor["wearing_a_mask"]:
             raise NotWearingMaskError("The visitor is not wearing a mask")
         return f"Welcome to {self.name}"
--- app/main.py
+++ app/main.py
@@ -2,12 +2,12 @@ from app.cafe import Cafe
 from app.errors import VaccineError, NotWearingMaskError
 
 
-def go_to_cafe(friends: list, cafe: Cafe) -> str:
+def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
     without_masks = 0
     for visitor in friends:
         try:
             cafe.visit_cafe(visitor)
-        except (KeyError, VaccineError):
+        except VaccineError:
             return "All friends should be vaccinated"
         except NotWearingMaskError:
             without_masks += 1
