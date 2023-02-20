b0466232a6a40d41539f8675b04860b68f08a9d1
Anatolii Pozniak <a13101990a@gmail.com>
2023-02-20 17:32:37 +0200

    fixed cafe type annotation

--- app/cafe.py
+++ app/cafe.py
@@ -10,7 +10,7 @@ class Cafe:
     def __init__(self, name: str) -> None:
         self.name = name
 
-    def visit_cafe(self, visitor: dict) -> [str, Exception]:
+    def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
             raise NotVaccinatedError("Only vaccinated customers allowed")
         if visitor["vaccine"]["expiration_date"] < datetime.date.today():
