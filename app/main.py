9adc7d804149284803d4d0f933d5a1174f5f2e67
Ruslan <noezyboy1@gmail.com>
2023-02-20 09:17:39 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+
+from app.errors import NotVaccinatedError, OutdatedVaccineError, NotWearingMaskError
+
+def go_to_cafe(friends, cafe):
+    try:
+        for friend in friends:
+            cafe.visit_cafe(friend)
+    except NotVaccinatedError:
+        return "All friends should be vaccinated"
+    except OutdatedVaccineError:
+        return "All friends should be vaccinated"
+    except NotWearingMaskError:
+        masks_to_buy = sum(not friend.get("wearing_a_mask", False) for friend in friends)
+        return "Friends should buy {} masks".format(masks_to_buy)
+    return "Friends can go to {}".format(cafe.name)
