4319727269147abd4e3ad42c52eab2d7f0fad30f
Anatolii Pozniak <a13101990a@gmail.com>
2023-02-20 17:03:24 +0200

    return annotation fixed

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from app.errors import (
 )
 
 
-def go_to_cafe(friends: list, cafe: Cafe) -> [str, Exception]:
+def go_to_cafe(friends: list, cafe: Cafe) -> str:
     masks_to_buy = 0
     for friend in friends:
         try:
