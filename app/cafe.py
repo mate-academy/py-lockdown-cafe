6f20a8712a3aec1532a1ea682409810135c19d00
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-02-20 19:11:38 +0200

    Deleted extra variable and args

--- app/cafe.py
+++ app/cafe.py
@@ -10,14 +10,14 @@ class Cafe:
     def __init__(self, name: str) -> None:
         self.name = name
 
-    def visit_cafe(self, visitor: dict, *args) -> str:
+    def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
             raise NotVaccinatedError(f"{visitor.get('name')} has no vaccine")
 
-        current_date = datetime.date.today()
-        expiration_date = visitor.get("vaccine").get("expiration_date")
-
-        if current_date > expiration_date:
+        if (
+                datetime.date.today()
+                > visitor.get("vaccine").get("expiration_date")
+        ):
             raise OutdatedVaccineError(f"{visitor.get('name')}, "
                                        f"your vaccine has been expired")
 
