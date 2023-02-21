20265f83e28eca945c11c3a4b462582a3c81747a
ramsty <ramsty27@gmail.com>
2023-02-21 16:36:02 +0200

    correction

--- app/main.py
+++ app/main.py
@@ -1,34 +1,17 @@
-# from app.cafe import Cafe
-# from app.errors import VaccineError, NotWearingMaskError
-#
-#
-# def go_to_cafe(friends: list, cafe: Cafe) -> str:
-#     count_masks = 0
-#     for friend in friends:
-#         try:
-#             cafe.visit_cafe(friend)
-#         except VaccineError:
-#             return "All friends should be vaccinated"
-#         except NotWearingMaskError:
-#             count_masks += 1
-#
-#     if count_masks:
-#         return f"Friends should buy {count_masks} masks"
-#     return f"Friends can go to {cafe.name}"
 from app.cafe import Cafe
 from app.errors import VaccineError, NotWearingMaskError
 
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
-    masks_to_buy = 0
+    count_masks = 0
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
         except VaccineError:
             return "All friends should be vaccinated"
         except NotWearingMaskError:
-            masks_to_buy += 1
+            count_masks += 1
 
-    if masks_to_buy > 0:
-        return f"Friends should buy {masks_to_buy} masks"
+    if count_masks:
+        return f"Friends should buy {count_masks} masks"
     return f"Friends can go to {cafe.name}"
