f3c6ce629cbc1ff400da0fd443f162ba5de2fc57
Yurii Kuchmenko <kuazakotl1@gmail.com>
2023-02-21 15:50:53 +0200

    remove get() call

--- app/cafe.py
+++ app/cafe.py
@@ -12,7 +12,7 @@ class Cafe:
     def visit_cafe(self, visitor: dict[str]) -> str | Exception:
         if not visitor.get("vaccine"):
             raise NotVaccinatedError("Visitor is not vaccinated.")
-        if visitor["vaccine"].get("expiration_date") < date.today():
+        if visitor["vaccine"]["expiration_date"] < date.today():
             raise OutdatedVaccineError("Vaccine is expired")
         if not visitor.get("wearing_a_mask"):
             raise NotWearingMaskError("Visitor is not wearing a mask")
