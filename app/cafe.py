e0b6aaaaae3db0fbeaa0aff92f403cfb8d05eab4
Roman28101 <r.kovalenko28101@gmail.com>
2023-02-22 14:42:21 +0800

    Solution fixed imports and and selection

--- app/cafe.py
+++ app/cafe.py
@@ -1,8 +1,8 @@
 import datetime
 from app.errors import (
     NotVaccinatedError,
-    OutdatedVaccineError,
-    NotWearingMaskError
+    NotWearingMaskError,
+    OutdatedVaccineError
 )
 
 
@@ -16,6 +16,6 @@ class Cafe:
             raise NotVaccinatedError("Must be vaccinated")
         if visitor["vaccine"]["expiration_date"] < datetime.date.today():
             raise OutdatedVaccineError("Vaccine expired")
-        if not visitor["wearing_a_mask"]:
+        if not visitor.get("wearing_a_mask", False):
             raise NotWearingMaskError("Need to buy mask")
         return f"Welcome to {self.name}"
