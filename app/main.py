821d1d474d8ca1888e1c75a03b0d93b84b6a422f
Vladimir Byshevets <texnadzor.info@gmail.com>
2023-02-27 18:57:05 +0200

    For checking

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+import datetime
+from errors import *
+from cafe import Cafe
+
+
+
+def go_to_cafe(friends: list, cafe: object) -> str:
+	buy = 0
+
+	for x in friends:
+		try:
+			buy += 1
+			if "vaccine" not in x:
+				return "All friends should be vaccinated"
+			if cafe.visit_cafe(x):
+				return f"Friends can go to {cafe.name}"
+		except VaccineError as e:
+			return "All friends should be vaccinated"
+	return f"Friends should buy {buy} masks"
