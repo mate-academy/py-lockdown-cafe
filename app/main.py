8824bb52de8b732592f2ab91b24f9d7f39911b6b
Vladimir Byshevets <texnadzor.info@gmail.com>
2023-02-27 20:43:13 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,19 +1,17 @@
-import datetime
-from errors import *
-from cafe import Cafe
-
+from app.errors import NotWearingMaskError
+from app.errors import VaccineError
 
 
 def go_to_cafe(friends: list, cafe: object) -> str:
-	buy = 0
+    buy = 0
+    for x in friends:
+        try:
+            cafe.visit_cafe(x)
+        except NotWearingMaskError:
+            buy += 1
+        except VaccineError:
+            return "All friends should be vaccinated"
+    if buy:
+        return f"Friends should buy {buy} masks"
 
-	for x in friends:
-		try:
-			buy += 1
-			if "vaccine" not in x:
-				return "All friends should be vaccinated"
-			if cafe.visit_cafe(x):
-				return f"Friends can go to {cafe.name}"
-		except VaccineError as e:
-			return "All friends should be vaccinated"
-	return f"Friends should buy {buy} masks"
+    return f"Friends can go to {cafe.name}"
