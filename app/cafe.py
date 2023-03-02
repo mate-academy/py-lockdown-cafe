fa2a05cf17e48f454abb286fcb3e174b3eaf95c3
Mkrtych Khachatrian <mukuchbrat@gmail.com>
2023-03-02 13:56:17 +0200

    get() instead of parenthesis

--- app/cafe.py
+++ app/cafe.py
@@ -14,8 +14,8 @@ class Cafe:
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
             raise NotVaccinatedError("The visitor is not vaccinated.")
-        if visitor["vaccine"]["expiration_date"] < date.today():
+        if visitor.get("vaccine").get("expiration_date") < date.today():
             raise OutdatedVaccineError("The vaccine is expired")
-        if not visitor["wearing_a_mask"]:
+        if not visitor.get("wearing_a_mask"):
             raise NotWearingMaskError("The visitor is not wearing a mask")
         return f"Welcome to {self.name}"
