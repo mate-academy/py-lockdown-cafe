c1ab9511f535a709e3e15083eb2a91367f298580
Igor Starodub <igorastar@gmail.com>
2023-03-13 22:18:21 +0200

    Solo1

--- app/cafe.py
+++ app/cafe.py
@@ -8,16 +8,12 @@ class Cafe:
         self.name = name
 
     def visit_cafe(self, visitor: dict) -> str:
-        errors = 0
         masks_to_buy = 1
         if "vaccine" not in visitor:
-            errors += 1
             raise NotVaccinatedError("All friends should be vaccinated")
         if visitor["vaccine"]["expiration_date"] < datetime.date.today():
-            errors += 1
             raise OutdatedVaccineError("OutdatedVaccine")
         if visitor["wearing_a_mask"] is False:
-            errors += 1
             raise NotWearingMaskError(
                 f"Friends should buy {masks_to_buy} masks"
             )
