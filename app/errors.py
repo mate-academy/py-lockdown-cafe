0a45818c0627fe8dbabb8793c588f498684e29b9
Volodymyr Borovets <vborovets0@gmail.com>
2023-04-14 23:03:16 +0300

    add docstrings in module errors

--- app/errors.py
+++ app/errors.py
@@ -1,17 +1,20 @@
 class VaccineError(Exception):
-    pass
+    """Parent Class for Vaccine Errors"""
 
 
 class NotVaccinatedError(VaccineError):
+    """The person is not vaccinated"""
     def __str__(self) -> str:
-        return "NotVaccinatedError"
+        return "Not Vaccinated"
 
 
 class OutdatedVaccineError(VaccineError):
+    """Duration of effectiveness of vaccination is expired"""
     def __str__(self) -> str:
-        return "OutdatedVaccineError"
+        return "Expired vaccination duration"
 
 
 class NotWearingMaskError(Exception):
+    """The person without a mask"""
     def __str__(self) -> str:
-        return "NotWearingMaskError"
+        return "No mask - No coffee"
