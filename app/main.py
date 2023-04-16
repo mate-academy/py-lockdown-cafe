2a5557a28b26b5ff52179cb5d28f34112f301888
V Borovets <vborovets0@gmail.com>
2023-04-16 20:37:50 +0300

    refactor code

--- app/main.py
+++ app/main.py
@@ -4,17 +4,13 @@ from app.errors import VaccineError, NotWearingMaskError
 
 def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
     count = 0
-
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
-
         except NotWearingMaskError:
             count += 1
-
         except VaccineError:
             return "All friends should be vaccinated"
-
     if count > 0:
         return f"Friends should buy {count} masks"
 
