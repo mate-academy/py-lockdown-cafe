0644792d7374b85c6d7b67c6a44281c63d372647
Oleksandr Gubarev <alexgubarev885@gmail.com>
2023-05-14 21:16:01 +0300

    Fix Py lockdown cafe

--- app/cafe.py
+++ app/cafe.py
@@ -1,5 +1,7 @@
 from datetime import date
-from app.errors import OutdatedVaccineError, NotWearingMaskError, NotVaccinatedError
+from app.errors import (OutdatedVaccineError,
+                        NotWearingMaskError,
+                        NotVaccinatedError)
 
 
 class Cafe:
--- app/errors.py
+++ app/errors.py
@@ -1,15 +1,17 @@
 class VaccineError(Exception):
-    """Parent class for vaccine-related errors"""
     pass
 
 
 class NotVaccinatedError(VaccineError):
+    """Raised when a visitor is not vaccinated"""
     pass
 
 
 class OutdatedVaccineError(VaccineError):
+    """Raised when a visitor's vaccine is expired"""
     pass
 
 
 class NotWearingMaskError(Exception):
+    """Raised when a visitor is not wearing a mask when visiting the cafe"""
     pass
