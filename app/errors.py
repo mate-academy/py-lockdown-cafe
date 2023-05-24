b788166f241396b97b998a0efbce83395da3ef84
chipa <mezismezis@gmail.com>
2023-05-24 22:49:46 +0300

    hm

--- app/errors.py
+++ app/errors.py
@@ -1,16 +1,21 @@
 class VaccineError(Exception):
+    """This Exception raises when there is some problems with vaccinating"""
     def __init__(self, *args: object) -> None:
         super().__init__(args)
+#   What is "Unnecessary here", I didn't get it.
 
 
 class NotVaccinatedError(VaccineError):
+    """This Exception raises when person didn't vaccinate"""
     pass
 
 
 class OutdatedVaccineError(VaccineError):
+    """This Exception raises when person's vaccination is out of date"""
     pass
 
 
 class NotWearingMaskError(Exception):
+    """This Exception raises when person don't have mask"""
     def __init__(self, *args: object) -> None:
         super().__init__(args)
