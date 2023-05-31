395f8276695b9162366f31b4b7ecb147b52369cf
Glieb <gleblitvinenko3007@gmail.com>
2023-05-31 16:03:10 +0300

    Fixed init, .get

--- app/cafe.py
+++ app/cafe.py
@@ -13,9 +13,10 @@ class Cafe:
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor.keys():
             raise NotVaccinatedError("Visitor should be vaccinated")
-        elif datetime.date.today() > visitor["vaccine"]["expiration_date"]:
+        elif (datetime.date.today() >
+              visitor.get("vaccine").get("expiration_date")):
             raise OutdatedVaccineError("Vaccine is outdated")
-        elif not visitor["wearing_a_mask"]:
+        elif not visitor.get("wearing_a_mask"):
             raise NotWearingMaskError("Visitor must wear the mask")
         else:
             return f"Welcome to {self.name}"
