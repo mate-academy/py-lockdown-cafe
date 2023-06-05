37ed8a73de13c5b01226d500dd49fb73d6478a21
Denys Raboshchuk <kontroler444@gmail.com>
2023-06-05 16:33:58 +0300

    Make changes

--- app/cafe.py
+++ app/cafe.py
@@ -16,7 +16,7 @@ class Cafe:
             raise NotVaccinatedError("Visitors should be vaccinated!")
         elif current_date > visitor["vaccine"]["expiration_date"]:
             raise OutdatedVaccineError("Vaccine shoud be not outdated!")
-        elif visitor.get("wearing_a_mask") is False:
+        elif not visitor.get("wearing_a_mask"):
             raise NotWearingMaskError("Visitors should wear the mask!")
 
         return f"Welcome to {self.name}"
