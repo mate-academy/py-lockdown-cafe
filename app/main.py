7c02b3c92e9b938f5daf6524fddb4570bece4e2d
Yevhenii Iovchu <persie.iovchu@gmail.com>
2023-06-08 15:36:07 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,23 @@
-# write your code here
+from .cafe import Cafe
+from .errors import VaccineError, NotWearingMaskError
+import datetime
+
+
+def go_to_cafe(friends: list, cafe: Cafe) -> str:
+    sorted_friends = sorted(friends, key=lambda friend: (
+        "vaccine" in friend,
+        (friend.get("vaccine", {}).get("expiration_date", datetime.date.max)
+         > datetime.date.today())
+    ))
+
+    masks_to_buy = 0
+    for friend in sorted_friends:
+        try:
+            cafe.visit_cafe(friend)
+        except VaccineError:
+            return "All friends should be vaccinated"
+        except NotWearingMaskError:
+            masks_to_buy += 1
+    if masks_to_buy:
+        return f"Friends should buy {masks_to_buy} masks"
+    return f"Friends can go to {cafe.name}"
