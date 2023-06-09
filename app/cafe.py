773da3302673054013363a700c32d68fbdc721b0
Olha Tryhub <olha.tryhub@nure.ua>
2023-06-09 21:09:58 +0300

    Solution for Lockdown Cafe

---
+++ app/cafe.py
@@ -0,0 +1,19 @@
+import datetime
+
+from app.errors import NotVaccinatedError
+from app.errors import OutdatedVaccineError
+from app.errors import NotWearingMaskError
+
+
+class Cafe:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def visit_cafe(self, visitor: dict) -> str:
+        if "vaccine" not in visitor:
+            raise NotVaccinatedError
+        elif datetime.date.today() > visitor["vaccine"]["expiration_date"]:
+            raise OutdatedVaccineError
+        if not visitor["wearing_a_mask"]:
+            raise NotWearingMaskError
+        return f"Welcome to {self.name}"
