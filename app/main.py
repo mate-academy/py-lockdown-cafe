b1c358679f9089a1b5b2ba628184267397a61aac
YOUR_NAME <YOUR_EMAIL>
2023-06-11 15:56:30 +0300

    Lockdown-Cafe ( fix: 1. return "Friends can go to KFC" 2. del no_vaccine )

--- app/main.py
+++ app/main.py
@@ -6,19 +6,15 @@ from app.errors import VaccineError, NotWearingMaskError
 
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
-    no_vaccine = False
     no_mask = 0
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
         except VaccineError:
-            no_vaccine = True
-            break
+            return "All friends should be vaccinated"
         except NotWearingMaskError:
             no_mask += 1
 
-    if no_vaccine:
-        return "All friends should be vaccinated"
-    elif no_mask:
+    if no_mask:
         return f"Friends should buy {no_mask} masks"
-    return "Friends can go to KFC"
+    return f"Friends can go to {cafe.name}"
