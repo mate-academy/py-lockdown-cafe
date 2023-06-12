7cdba6af2c91a75f3ce8ef44377b7924920b3ada
Vlad <vladislav.zhyla@gmail.com>
2023-06-12 16:12:53 +0300

    Imports fixed. 1 try block removed.Error messages fixed

--- app/cafe.py
+++ app/cafe.py
@@ -1,4 +1,6 @@
 import datetime
+
+
 from app.errors import (
     NotWearingMaskError,
     NotVaccinatedError,
@@ -12,10 +14,10 @@ class Cafe:
 
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
-            raise NotVaccinatedError("NotVaccinatedError")
+            raise NotVaccinatedError("Error. Visitors have to be vaccinated")
         if visitor["vaccine"]["expiration_date"] < datetime.date.today():
-            raise OutdatedVaccineError("OutdatedVaccineError")
+            raise OutdatedVaccineError("Error. Vaccine is outdated")
         if not visitor["wearing_a_mask"]:
-            raise NotWearingMaskError("NotWearingMaskError")
+            raise NotWearingMaskError("Error. Visitors have to be in masks")
 
         return f"Welcome to {self.name}"
