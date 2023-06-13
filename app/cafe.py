4594342f473c6ee38d87ebdcdd8bc28bcdaabd48
Alex Yatsun <starcrafter4444@gmail.com>
2023-06-13 13:30:47 +0300

    Improvement

--- app/cafe.py
+++ app/cafe.py
@@ -14,8 +14,7 @@ class Cafe:
     def visit_cafe(self, visitor: dict) -> str:
         if not visitor.get("vaccine"):
             raise NotVaccinatedError("You should be vaccinated")
-        if ((visitor["vaccine"]["expiration_date"]
-             - datetime.today().date()).days < 0):
+        if visitor["vaccine"]["expiration_date"] < datetime.today().date():
             raise OutdatedVaccineError("Yore vaccine should be valid")
         if not visitor.get("wearing_a_mask"):
             raise NotWearingMaskError("You should have a mask")
