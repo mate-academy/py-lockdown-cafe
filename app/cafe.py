3d12e7ad5e5f7a56142b9bd73a5b8fd4512ac418
stepband <vladshvik@gmail.com>
2023-06-23 08:08:15 +0300

    Problem with mask and vaccine order check

---
+++ app/cafe.py
@@ -0,0 +1,24 @@
+from app.errors import NotWearingMaskError
+from app.errors import OutdatedVaccineError
+from app.errors import NotVaccinatedError
+import datetime
+
+
+class Cafe:
+    def __init__(self, name):
+        self.name = name
+
+    def visit_cafe(self, visitor):
+        if "vaccine" not in visitor:
+            raise NotVaccinatedError("Visitor is not vaccinated")
+
+        expiration_date = visitor["vaccine"].get("expiration_date")
+        if expiration_date < datetime.date.today():
+            raise OutdatedVaccineError("Visitor's vaccine has expired")
+
+        if not visitor.get("wearing_a_mask"):
+            raise NotWearingMaskError("Visitor is not wearing a mask")
+
+        return f"Welcome to {self.name}"
+
+
--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+from app.errors import NotWearingMaskError
+from app.errors import OutdatedVaccineError
+from app.errors import NotVaccinatedError
+
+
+def go_to_cafe(friends, cafe):
+    try:
+        for friend in friends:
+            cafe.visit_cafe(friend)
+    except NotVaccinatedError:
+        return "All friends should be vaccinated"
+    except (OutdatedVaccineError, NotWearingMaskError) as error:
+        if isinstance(error, OutdatedVaccineError):
+            return "All friends should be vaccinated"
+        if isinstance(error, NotWearingMaskError):
+            masks_to_buy = sum(1 for friend in friends if not friend.get("wearing_a_mask"))
+            return f"Friends should buy {masks_to_buy} masks"
+
+    return f"Friends can go to {cafe.name}"
