00fab9ab577e270b2969b541efe22f030b4a2463
Alexander Izmashkin <alexandr.izmashkin@gmail.com>
2023-07-12 11:14:28 +0300

    Solution/Alex/2

--- app/main.py
+++ app/main.py
@@ -18,5 +18,5 @@ def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
         return "All friends should be vaccinated"
     elif masks_to_buy > 0:
         return f"Friends should buy {masks_to_buy} masks"
-    else:
-        return f"Friends can go to {cafe.name}"
+
+    return f"Friends can go to {cafe.name}"
