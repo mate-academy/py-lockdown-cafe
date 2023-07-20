9e93f0f369e46f2c2b9bb3c8da46705670217c73
Vitalii Safian <viisaf.prog.gt@gmail.com>
2023-07-20 22:16:16 +0300

    fixed annotation

--- app/cafe.py
+++ app/cafe.py
@@ -11,7 +11,7 @@ class Cafe:
     def __init__(self, name: str) -> None:
         self.name = name
 
-    def visit_cafe(self, visitor: dict) -> str | Exception:
+    def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
             raise NotVaccinatedError("NotVaccinatedError")
         elif visitor["vaccine"]["expiration_date"] < datetime.date.today():
