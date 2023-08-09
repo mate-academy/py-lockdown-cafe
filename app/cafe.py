79253bccd3417065abfc1b9bbfa1835ea804a5a2
Andrew <67094594+MishkaGAMII@users.noreply.github.com>
2023-08-09 11:57:04 +0300

    cafe_fix.py

--- app/cafe.py
+++ app/cafe.py
@@ -9,7 +9,7 @@ from app.errors import (
 class Cafe:
     def __init__(self, name: str) -> None:
         self.name = name
-    
+
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
             raise NotVaccinatedError
