b6877790d05e85db7ed9e615188e111949d66ba9
Alex Pelevin <ya.pelevin1999@gmail.com>
2023-08-14 10:28:55 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -7,12 +7,10 @@ from app.cafe import Cafe
 
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
-    allow_friend_count = 0
     masks_to_buy = 0
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
-            allow_friend_count += 1
         except NotVaccinatedError as e:
             return str(e)
         except OutdatedVaccineError as e:
@@ -22,7 +20,5 @@ def go_to_cafe(friends: list, cafe: Cafe) -> str:
 
     if masks_to_buy:
         return f"Friends should buy {masks_to_buy} masks"
-    if allow_friend_count == len(friends):
-        return f"Friends can go to {cafe.name}"
 
-    return ""
+    return f"Friends can go to {cafe.name}"
