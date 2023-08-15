c0875ffc215368060b09bf4d57bece082bf6304b
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-08-15 13:25:33 +0300

    Solution 1.1

--- app/errors.py
+++ app/errors.py
@@ -3,15 +3,15 @@ class VaccineError(Exception):
 
 
 class NotVaccinatedError(VaccineError):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__("The Guy seem to be not vaccinated (")
 
 
 class OutdatedVaccineError(VaccineError):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__("The vaccine is expired")
 
 
 class NotWearingMaskError(ValueError):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__("MF is maskless!")
