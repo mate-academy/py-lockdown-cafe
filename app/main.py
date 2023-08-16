12a02a5052d80284d34daf286fbeb5752753e88b
H1kki <vetalkuz102@gmail.com>
2023-08-16 20:24:00 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -13,4 +13,4 @@ def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
             masks_to_buy += 1
     if masks_to_buy > 0:
         return f"Friends should buy {masks_to_buy} masks"
-    return f"Friends can go to {cafe.name}"
+    return f"Friends can go to {cafe.name} "
