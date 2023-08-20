ded0ea6889b52ffb844f8b06f34ea47479abe848
Illia <kononenko2109@gmail.com>
2023-08-20 18:48:43 +0300

    Implemented py-lockdown-cafe

---
+++ app/errors.py
@@ -0,0 +1,20 @@
+class VaccineError(Exception):
+    def __str__(self) -> str:
+        return "All friends should be vaccinated"
+
+
+class NotVaccinatedError(VaccineError):
+    pass
+
+
+class OutdatedVaccineError(VaccineError):
+    pass
+
+
+class NotWearingMaskError(Exception):
+    def __init__(self, masks_to_buy: int) -> None:
+        self.masks_to_buy = masks_to_buy
+        super().__init__()
+
+    def __str__(self) -> str:
+        return f"Friends should buy {self.masks_to_buy} masks"
