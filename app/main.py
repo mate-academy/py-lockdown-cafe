9186da7d9713c5c7fd1d4309f9c80545c256f09c
Marian Kovalyshyn <mariankovalishin12345@gmail.com>
2023-08-21 20:02:43 +0300

    Added blank space between imports and fixed repeated handling of errors

--- app/main.py
+++ app/main.py
@@ -5,22 +5,14 @@ from app.cafe import Cafe
 def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
     masks_to_buy = 0
 
-    try:
-        for friend in friends:
-            try:
-                cafe.visit_cafe(friend)
-            except VaccineError:
-                raise
-            except NotWearingMaskError:
-                masks_to_buy += 1
+    for friend in friends:
+        try:
+            cafe.visit_cafe(friend)
+        except VaccineError as e:
+            return str(e)
+        except NotWearingMaskError:
+            masks_to_buy += 1
 
-        if masks_to_buy:
-            raise NotWearingMaskError(
-                f"Friends should buy {masks_to_buy} masks"
-            )
-    except VaccineError as error:
-        return str(error)
-    except NotWearingMaskError as error:
-        return str(error)
-    else:
-        return f"Friends can go to {cafe.name}"
+    if masks_to_buy:
+        return f"Friends should buy {masks_to_buy} masks"
+    return f"Friends can go to {cafe.name}"
