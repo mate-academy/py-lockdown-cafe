fbd7d1954794b22234fbd83a7daa61b067a74014
Kateryna Zadorozhna <136272476+katryana@users.noreply.github.com>
2023-08-21 20:29:53 +0300

    Update errors.py

--- app/errors.py
+++ app/errors.py
@@ -4,14 +4,14 @@ class VaccineError(Exception):
 
 class NotVaccinatedError(VaccineError):
     def __str__(self) -> str:
-        return "NotVaccinatedError"
+        return "The visitor should be vaccinated."
 
 
 class OutdatedVaccineError(VaccineError):
     def __str__(self) -> str:
-        return "OutdatedVaccineError"
+        return "The vaccination must be up to date."
 
 
 class NotWearingMaskError(Exception):
     def __str__(self) -> str:
-        return "NotWearingMaskError"
+        return "The visitor should wear a mask."
