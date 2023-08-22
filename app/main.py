d27ee9fa4dd25735d9efe9e040b92905fcad3070
Oleksandr Moroz <oleksandrmoroz2003@gmail.com>
2023-08-22 10:56:49 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -3,17 +3,14 @@ from app.errors import VaccineError, NotWearingMaskError
 
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
-    masks_to_buy = sum(
-        1 for friend in friends if not friend.get("wearing_a_mask", False)
-    )
+    masks_to_buy = 0
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
         except VaccineError as e:
             return str(e)
         except NotWearingMaskError:
-            pass
-
+            masks_to_buy += 1
     if masks_to_buy:
         return f"Friends should buy {masks_to_buy} masks"
     return f"Friends can go to {cafe.name}"
