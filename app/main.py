4ec3cb281669d70778cc59c24214588c19c6c7d6
Igor Berezivskyi <igor200305.ber@gmail.com>
2023-08-24 14:38:19 +0300

    solution2

--- app/main.py
+++ app/main.py
@@ -3,20 +3,25 @@ from app.errors import VaccineError, NotWearingMaskError
 from app.cafe import Cafe
 
 
-def go_to_cafe(friends: list, cafe: Cafe) -> str | VaccineError:
-    masks_to_buy = 0
+def get_amount_without_mask(friends: list) -> int:
+    return len([friend for friend in friends if not friend["wearing_a_mask"]])
 
-    for friend in friends:
+
+def go_to_cafe(friends: list, cafe: Cafe) -> str:
+
+    for i, friend in enumerate(friends):
         try:
             cafe.visit_cafe(friend)
+            if i == len(friends) - 1 and get_amount_without_mask(friends) != 0:
+                return (f"Friends should buy "
+                        f"{get_amount_without_mask(friends)} masks")
 
         except VaccineError:
             return "All friends should be vaccinated"
 
         except NotWearingMaskError:
-            masks_to_buy += 1
-
-    if masks_to_buy != 0:
-        return f"Friends should buy {masks_to_buy} masks"
+            if i == len(friends) - 1 and get_amount_without_mask(friends) != 0:
+                return (f"Friends should buy "
+                        f"{get_amount_without_mask(friends)} masks")
 
     return f"Friends can go to {cafe.name}"
