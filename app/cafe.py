9dab580b2271867e530c497f6c545f1d70134768
Oleh Bohatov <obohatov@gmail.com>
2023-08-28 22:21:14 +0200

    Fixed solution

--- app/cafe.py
+++ app/cafe.py
@@ -8,10 +8,14 @@ from app.errors import (
 
 
 class Cafe:
-    def __init__(self, name: str) -> None:
+    def __init__(self,
+                 name: str
+                 ) -> None:
         self.name = name
 
-    def visit_cafe(self, visitor: dict) -> str:
+    def visit_cafe(self,
+                   visitor: dict
+                   ) -> str:
         if "vaccine" not in visitor:
             raise NotVaccinatedError("All friends should be vaccinated")
         if visitor["vaccine"]["expiration_date"] < date.today():
