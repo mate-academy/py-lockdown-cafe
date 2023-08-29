bbf94ebeba0f4a995a561bdea68983cd73779de3
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-08-29 19:41:26 +0200

    Update cafe.py

--- app/cafe.py
+++ app/cafe.py
@@ -1,12 +1,11 @@
 import datetime
 from app.errors import NotVaccinatedError, OutdatedVaccineError, NotWearingMaskError
 
-
 class Cafe:
-    def __init__(self, name):
+    def __init__(self, name: str) -> None:
         self.name = name
 
-    def visit_cafe(self, visitor):
+    def visit_cafe(self, visitor: dict) -> str:
         if 'vaccine' not in visitor:
             raise NotVaccinatedError("Visitor is not vaccinated and cannot enter the cafe.")
 
