2aa9b77ffed6cdbadfe95322dc09897cd9143b26
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-09-04 20:07:55 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,13 +7,11 @@ from app.cafe import Cafe
 
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
-    vaccinated_friends = 0
     masks_to_buy = 0
 
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
-            vaccinated_friends += 1
         except (NotVaccinatedError, OutdatedVaccineError):
             return "All friends should be vaccinated"
         except NotWearingMaskError:
