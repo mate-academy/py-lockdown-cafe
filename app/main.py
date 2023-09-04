78f9eb2650c001a6bee7f54d1bd57e199801723f
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-09-04 20:15:43 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -19,4 +19,5 @@ def go_to_cafe(friends: list, cafe: Cafe) -> str:
 
         if masks_to_buy > 0:
         return f"Friends should buy {masks_to_buy} masks"
+    
     return f"Friends can go to {cafe.name}"
