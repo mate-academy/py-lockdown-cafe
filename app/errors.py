838cf42f81e82d2702c975f16c5cef656d378ad0
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-09-04 20:23:20 +0200

    Update errors.py

--- app/errors.py
+++ app/errors.py
@@ -4,12 +4,12 @@ class VaccineError(Exception):
 
 class NotVaccinatedError(VaccineError):
     def __init__(self, message: str = "Visitor is not vaccinated") -> None:
-    """
-    Initialize the NotVaccinatedError.
+        """
+        Initialize the NotVaccinatedError.
 
-    :param message: The error message (default: "Visitor is not vaccinated")
-    """
-    super().__init__(message)
+        :param message: The error message (default: "Visitor is not vaccinated")
+        """
+        super().__init__(message)
 
 
 class OutdatedVaccineError(VaccineError):
