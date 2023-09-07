2d4c391043c358a6f29b07863451952ff2a198e3
Vlad <hanovskyiv@gmail.com>
2023-09-08 00:51:25 +0300

    'Solution'

--- app/cafe.py
+++ app/cafe.py
@@ -12,11 +12,10 @@ class Cafe:
         self.name = name
 
     def visit_cafe(self, visitor: dict) -> str:
-        try:
-            if datetime.date.today() > visitor["vaccine"]["expiration_date"]:
-                raise OutdatedVaccineError
-            if not visitor["wearing_a_mask"]:
-                raise NotWearingMaskError
-            return f"Welcome to {self.name}"
-        except KeyError:
+        if "vaccine" not in visitor:
             raise NotVaccinatedError
+        if datetime.date.today() > visitor["vaccine"]["expiration_date"]:
+            raise OutdatedVaccineError
+        if not visitor["wearing_a_mask"]:
+            raise NotWearingMaskError
+        return f"Welcome to {self.name}"
--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from app.cafe import Cafe
 from app.errors import VaccineError, NotWearingMaskError
 
 
-def go_to_cafe(friends: list, cafe: Cafe) -> str:
+def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
     not_wearing_masks = 0
     for visitor in friends:
         try:
