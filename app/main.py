06a6d1fc953adbb1f185c172276c28459de797ec
Igor Berezivskyi <igor200305.ber@gmail.com>
2023-09-13 21:32:11 +0300

    Solution6

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
+
 from app.errors import VaccineError, NotWearingMaskError
+
 from app.cafe import Cafe
 
 
@@ -9,19 +11,16 @@ def get_amount_without_mask(friends: list) -> int:
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
     count_without_mask = 0
-    vaccinated = True
 
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
         except VaccineError:
-            vaccinated = False
+            return "All friends should be vaccinated"
         except NotWearingMaskError:
             count_without_mask += 1
 
-    if not vaccinated:
-        return "All friends should be vaccinated"
-    elif count_without_mask > 0:
+    if count_without_mask > 0:
         return f"Friends should buy {count_without_mask} masks"
     else:
         return f"Friends can go to {cafe.name}"
