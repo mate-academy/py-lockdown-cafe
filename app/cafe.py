1c95363597224153dc1dfc2898469295e51f3a29
Serhii Berdianskyi <zeferot94@gmail.com>
2023-09-29 12:55:43 +0300

    Implemented lockdown cafe task

---
+++ app/cafe.py
@@ -0,0 +1,18 @@
+import datetime
+from app.errors import (NotVaccinatedError,
+                        OutdatedVaccineError,
+                        NotWearingMaskError)
+
+
+class Cafe:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def visit_cafe(self, visitor: dict) -> str:
+        if "vaccine" not in visitor:
+            raise NotVaccinatedError("You have to be vaccinated.")
+        if visitor["vaccine"]["expiration_date"] < datetime.date.today():
+            raise OutdatedVaccineError("Your vaccine is expired.")
+        if visitor["wearing_a_mask"] is False:
+            raise NotWearingMaskError("You have to be in mask.")
+        return f"Welcome to {self.name}"
