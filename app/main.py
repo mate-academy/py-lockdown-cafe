20d392bb7865d6606beeae17780c5e8c14bf4fa0
Dmytro Prokhach <dprokhach@gmail.com>
2023-10-03 18:15:34 +0300

    changed e to errors

--- app/main.py
+++ app/main.py
@@ -2,15 +2,15 @@ from app.cafe import Cafe
 from app.errors import NotWearingMaskError, VaccineError
 
 
-def go_to_cafe(friends: list, cafe: Cafe) -> str:
+def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
     masks_to_buy = 0
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
         except NotWearingMaskError:
             masks_to_buy += 1
-        except VaccineError as e:
-            return str(e)
+        except VaccineError as error:
+            return str(error)
     if masks_to_buy > 0:
         return f"Friends should buy {masks_to_buy} masks"
     return f"Friends can go to {cafe.name}"
