1f809c2a92f652d78edf7337c2fd4d43d3929f99
Anton Migunov <migunov1au@gmail.com>
2023-10-13 12:07:39 +0300

    rearranged check blocks in visit_cafe function

--- app/cafe.py
+++ app/cafe.py
@@ -8,12 +8,11 @@ class Cafe:
         self.name = name
 
     def visit_cafe(self, visitor: dict) -> str:
-        if visitor.get("vaccine") is not None:
-            if (visitor["vaccine"].get("expiration_date")
-                    < datetime.date.today()):
-                raise OutdatedVaccineError("Vaccine is expired")
-        else:
+        if visitor.get("vaccine") is None:
             raise NotVaccinatedError("Vaccine is required")
+
+        if visitor["vaccine"].get("expiration_date") < datetime.date.today():
+            raise OutdatedVaccineError("Vaccine is expired")
 
         if visitor.get("wearing_a_mask") in (None, False):
             raise NotWearingMaskError("Mask is required")
