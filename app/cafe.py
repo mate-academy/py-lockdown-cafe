6818b38467b46993dfee4052e9a78a3cfd353062
naiv <nazarivankiv1@gmail.com>
2023-10-16 21:23:23 +0300

    Updated solution

--- app/cafe.py
+++ app/cafe.py
@@ -13,7 +13,7 @@ class Cafe:
     def visit_cafe(self, visitor: dict) -> str:
         if not visitor.get("vaccine"):
             raise NotVaccinatedError("Vaccination certificate missing!")
-        if visitor["vaccine"]["expiration_date"] < date.today():
+        if visitor.get("vaccine").get("expiration_date") < date.today():
             raise OutdatedVaccineError("Vaccination certificate outdated!")
         if not visitor.get("wearing_a_mask"):
             raise NotWearingMaskError("Visitor isn't wearing a mask!")
--- app/main.py
+++ app/main.py
@@ -11,6 +11,6 @@ def go_to_cafe(friends: list, cafe: Cafe) -> str:
             return "All friends should be vaccinated"
         except NotWearingMaskError:
             masks_to_buy += 1
-    return (f"Friends should buy {masks_to_buy} masks"
-            if masks_to_buy
-            else f"Friends can go to {cafe.name}")
+    if masks_to_buy:
+        return f"Friends should buy {masks_to_buy} masks"
+    return f"Friends can go to {cafe.name}"
