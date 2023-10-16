917787de71f2f95c06f1187a483f1476b61cd493
Andrey Britov <abritov82@gmail.com>
2023-10-16 14:03:03 +0300

    Solusion_1

---
+++ app/errors.py
@@ -0,0 +1,18 @@
+class VaccineError(Exception):
+
+    def __str__(self) -> str:
+        return "All friends should be vaccinated"
+
+
+class NotVaccinatedError(VaccineError):
+    pass
+
+
+class OutdatedVaccineError(VaccineError):
+    pass
+
+
+class NotWearingMaskError(Exception):
+
+    def __str__(self) -> str:
+        return f"Friends should buy mask"
--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+from typing import Any
+from app.cafe import Cafe
+from app.errors import (
+     NotVaccinatedError,
+     NotWearingMaskError,
+     OutdatedVaccineError
+)
+
+
+def go_to_cafe(friends: list, cafe: Cafe) -> Any:
+    for one_friend in friends:
+        try:
+            cafe.visit_cafe(one_friend)
+        except NotVaccinatedError or OutdatedVaccineError:
+            raise
+        except NotWearingMaskError:
+            raise
+    return f"Friends can go to {cafe.name}"
