6bc5fd78ebe77302c0a227c569bd502d311b80af
Anatolii Hrechaniuk <genezisium@gmail.com>
2023-11-09 14:47:52 +0200

    changed condition wearing a mask

--- app/cafe.py
+++ app/cafe.py
@@ -13,8 +13,10 @@ class Cafe:
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
             raise NotVaccinatedError
-        if visitor["vaccine"]["expiration_date"] < datetime.date.today():
+        if (visitor.get("vaccine").
+                get("expiration_date") < datetime.date.today()):
             raise OutdatedVaccineError
-        if not visitor["wearing_a_mask"]:
+        if ("wearing_a_mask" not in visitor
+                or not visitor.get("wearing_a_mask")):
             raise NotWearingMaskError
         return f"Welcome to {self.name}"
