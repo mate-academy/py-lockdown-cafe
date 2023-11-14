45728a18ac00ba9b3401259c25b5764d8ee4b97d
Denys Kibalenko <dennieking87@gmail.com>
2023-11-14 14:25:26 +0100

    Solution fix key access

--- app/cafe.py
+++ app/cafe.py
@@ -13,8 +13,9 @@ class Cafe:
     def visit_cafe(self, visitor: dict) -> str:
         if not visitor.get("vaccine"):
             raise NotVaccinatedError("Visitor should be vaccinated.")
-        if not visitor["vaccine"]["expiration_date"] >= datetime.date.today():
+        if not (visitor.get("vaccine").get("expiration_date")
+                >= datetime.date.today()):
             raise OutdatedVaccineError("Visitor should have a valid vaccine.")
-        if not visitor["wearing_a_mask"]:
+        if not visitor.get("wearing_a_mask"):
             raise NotWearingMaskError("Visitor should wear a mask.")
         return f"Welcome to {self.name}"
