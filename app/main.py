0d83b9e2821b67e5ad53b858d66d272d5b6cf668
Oleksandr Matviichuk <oleksandr.matviychuk@gmail.com>
2023-11-21 23:41:51 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
 # write your code here
+
+
+
+import datetime
+
+kfc = Cafe("KFC")
+visitor = {
+    "name": "Paul",
+    "age": 23,
+    "vaccine": {
+        "expiration_date": datetime.date(year=2019, month=2, day=23)
+    }
+}
+kfc.visit_cafe(visitor)     # OutdatedVaccineError
