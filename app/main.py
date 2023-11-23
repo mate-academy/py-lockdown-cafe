29e2d805fcd61126c991302b54cabe0492a1af14
Vlad <mr.darmstadtium@gmail.com>
2023-11-23 22:37:48 +0100

    pass all tests

--- app/main.py
+++ app/main.py
@@ -8,21 +8,24 @@ day_today = datetime.date.today()
 
 def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
 
-    allowed_friends = 0
-
     for friend in friends:
 
         try:
             cafe.visit_cafe(friend)
-            allowed_friends += 1
 
         except VaccineError as v:
             return str(v)
+
+        except NotWearingMaskError:
+            pass
+
+    for friend in friends:
+        try:
+            cafe.visit_cafe(friend)
 
         except NotWearingMaskError as n:
             masks_to_buy = len([friend for friend in friends
                                 if friend.get("wearing_a_mask") is False])
             return str(n) + f"{masks_to_buy} masks"
 
-    if allowed_friends == len(friends):
-        return f"Friends can go to {cafe.name}"
+    return f"Friends can go to {cafe.name}"
