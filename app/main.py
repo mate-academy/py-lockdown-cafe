0c7d95f400b42bad9243ff3ec7503507dc1c17a5
Vladyslav Bazhyn <147983482+VladyslavBazhyn@users.noreply.github.com>
2023-11-24 15:24:03 +0100

    Try one loop 2

--- app/main.py
+++ app/main.py
@@ -7,25 +7,24 @@ day_today = datetime.date.today()
 
 
 def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
+    vaccine_error_encountered = False
+    not_wearing_mask_error = None
 
     for friend in friends:
-
         try:
             cafe.visit_cafe(friend)
-
         except VaccineError as v:
+            vaccine_error_encountered = True
             return str(v)
-
-        except NotWearingMaskError:
-            pass
-
-    for friend in friends:
-        try:
-            cafe.visit_cafe(friend)
-
         except NotWearingMaskError as n:
-            masks_to_buy = len([friend for friend in friends
-                                if friend.get("wearing_a_mask") is False])
-            return str(n) + f"{masks_to_buy} masks"
+            if not vaccine_error_encountered:
+                not_wearing_mask_error = n
+
+    if vaccine_error_encountered:
+        return str(vaccine_error_encountered)
+    elif not_wearing_mask_error:
+        masks_to_buy = len([friend for friend in friends
+                            if not friend.get("wearing_a_mask") is False])
+        return str(not_wearing_mask_error) + f"{masks_to_buy} masks"
 
     return f"Friends can go to {cafe.name}"
