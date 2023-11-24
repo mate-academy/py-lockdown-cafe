07b3ab9549cb06e5a8ef5f5fb50ab6f546b934a3
Liliya <liliyakhu@ukr.net>
2023-11-24 17:00:38 +0200

    use get() method

--- app/cafe.py
+++ app/cafe.py
@@ -15,14 +15,15 @@ class Cafe:
         if "vaccine" not in visitor:
             raise NotVaccinatedError(
                 f"The {self.name} visitor "
-                f"{visitor['name']} doesn't have a vaccine!"
+                f"{visitor.get('name')} doesn't have a vaccine!"
             )
-        if visitor["vaccine"]["expiration_date"] < datetime.date.today():
+        if (visitor.get("vaccine").get("expiration_date")
+                < datetime.date.today()):
             raise OutdatedVaccineError(
-                f"The {visitor['name']}'s vaccine has expired!"
+                f"The {visitor.get('name')}'s vaccine has expired!"
             )
-        if visitor["wearing_a_mask"] is False:
+        if visitor.get("wearing_a_mask") is False:
             raise NotWearingMaskError(
-                f"The visitor {visitor['name']} doesn't wear a mask!"
+                f"The visitor {visitor.get('name')} doesn't wear a mask!"
             )
         return f"Welcome to {self.name}"
