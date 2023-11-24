0bc412063c7701a44798771e0b5569aabe640e47
Vladyslav Bazhyn <147983482+VladyslavBazhyn@users.noreply.github.com>
2023-11-24 15:26:24 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ day_today = datetime.date.today()
 def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
     vaccine_error_encountered = False
     not_wearing_mask_error = None
+    masks_to_buy = 0
 
     for friend in friends:
         try:
@@ -18,13 +19,13 @@ def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
             return str(v)
         except NotWearingMaskError as n:
             if not vaccine_error_encountered:
+                masks_to_buy += 1
                 not_wearing_mask_error = n
 
     if vaccine_error_encountered:
         return str(vaccine_error_encountered)
     elif not_wearing_mask_error:
-        masks_to_buy = len([friend for friend in friends
-                            if not friend.get("wearing_a_mask") is False])
-        return str(not_wearing_mask_error) + f"{masks_to_buy} masks"
-
-    return f"Friends can go to {cafe.name}"
+        return (str(not_wearing_mask_error) + 
+                f"{masks_to_buy} masks")
+    else:
+        return f"Friends can go to {cafe.name}"
