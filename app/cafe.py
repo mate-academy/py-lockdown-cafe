21991c24f5cf81869c5ef741cd5ee45830d88af4
Pavlo-here <pasha.nasonovhtc@gmail.com>
2023-11-30 13:39:47 +0200

    using dict get() method

--- app/cafe.py
+++ app/cafe.py
@@ -18,7 +18,7 @@ class Cafe:
         if visitor["vaccine"]["expiration_date"] < datetime.date.today():
             raise OutdatedVaccineError
 
-        if not visitor["wearing_a_mask"]:
+        if not visitor.get("wearing_a_mask"):
             raise NotWearingMaskError
 
         return f"Welcome to {self.name}"
