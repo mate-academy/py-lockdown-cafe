f787b28ed108c0b183c1c5ac4636c41ed7e2c772
eLQeR <131271125+eLQeR@users.noreply.github.com>
2023-12-01 21:51:42 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+from app.cafe import Cafe
+from app.errors import NotVaccinatedError
+from app.errors import OutdatedVaccineError
+from app.errors import NotWearingMaskError
+
+
+def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
+    has_mask = True
+    without_mask = 0
+    for friend in friends:
+        try:
+            cafe.visit_cafe(friend)
+        except NotVaccinatedError:
+            return "All friends should be vaccinated"
+        except OutdatedVaccineError:
+            return "All friends should be vaccinated"
+        except NotWearingMaskError:
+            has_mask = False
+            without_mask += 1
+    if not has_mask:
+        return f"Friends should buy {without_mask} masks"
+    return f"Friends can go to {cafe.name}"
