6d154a7597e35c82c1fcc8218a7a005b779d4568
eLQeR <131271125+eLQeR@users.noreply.github.com>
2023-12-02 16:52:00 +0200

    Solution fully

--- app/main.py
+++ app/main.py
@@ -5,7 +5,6 @@ from app.errors import NotWearingMaskError
 
 
 def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
-    has_mask = True
     without_mask = 0
     for friend in friends:
         try:
@@ -13,8 +12,7 @@ def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
         except VaccineError:
             return "All friends should be vaccinated"
         except NotWearingMaskError:
-            has_mask = False
             without_mask += 1
-    if not has_mask:
+    if not without_mask > 0:
         return f"Friends should buy {without_mask} masks"
     return f"Friends can go to {cafe.name}"
