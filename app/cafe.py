41ea218f6bd55171dad9fde9e14fe83814cfe388
KwintDimon <121832419+KwintDimon@users.noreply.github.com>
2023-12-23 00:27:02 +0200

    Solution

--- app/cafe.py
+++ app/cafe.py
@@ -1,4 +1,5 @@
 import datetime
+
 from app.errors import (NotVaccinatedError,
                         OutdatedVaccineError,
                         NotWearingMaskError)
@@ -10,13 +11,15 @@ class Cafe:
 
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
-            raise NotVaccinatedError("")
+            raise NotVaccinatedError("Visitor is not vaccinated!")
 
         expiration_date = visitor["vaccine"].get("expiration_date")
         if expiration_date and expiration_date < datetime.date.today():
-            raise OutdatedVaccineError("")
+            raise OutdatedVaccineError(
+                "The vaccine have passed its expiration date"
+            )
 
         if not visitor.get("wearing_a_mask", True):
-            raise NotWearingMaskError("")
+            raise NotWearingMaskError("Visitor is not wearing a mask!")
 
         return f"Welcome to {self.name}"
--- app/main.py
+++ app/main.py
@@ -1,18 +1,13 @@
-from app.errors import (NotVaccinatedError,
-                        OutdatedVaccineError,
-                        VaccineError,
-                        NotWearingMaskError)
+from app.errors import VaccineError, NotWearingMaskError
 from app.cafe import Cafe
 
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
-    vaccinated_friends = 0
     masks_to_buy = 0
 
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
-            vaccinated_friends += 1
         except VaccineError:
             return "All friends should be vaccinated"
         except NotWearingMaskError:
@@ -20,5 +15,4 @@ def go_to_cafe(friends: list, cafe: Cafe) -> str:
 
     if masks_to_buy > 0:
         return f"Friends should buy {masks_to_buy} masks"
-    else:
-        return f"Friends can go to {cafe.name}"
+    return f"Friends can go to {cafe.name}"
