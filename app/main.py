f99a5012ebe5e5856b684125afa6aacbc7c0d0a5
ladmael <don.sosiska339@gmail.com>
2024-01-02 21:50:30 +0200

    fix flake

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from app.cafe import Cafe
-from app.errors import NotVaccinatedError,OutdatedVaccineError, NotWearingMaskError
+from app.errors import (NotVaccinatedError,
+                        OutdatedVaccineError,
+                        NotWearingMaskError)
 
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
@@ -10,7 +12,7 @@ def go_to_cafe(friends: list, cafe: Cafe) -> str:
             result.append(cafe.visit_cafe(friend))
         except (NotVaccinatedError, OutdatedVaccineError):
             return "All friends should be vaccinated"
-        except NotWearingMaskError as e:
+        except NotWearingMaskError:
             masks_needed += 1
     if len(result) == len(friends) and masks_needed == 0:
         return f"Friends can go to {cafe.name}"
