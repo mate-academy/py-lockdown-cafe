023fc95af1c662e05fab3ac55d3a62da22933503
Hermesvlad <hermesvlad@gmail.com>
2024-01-03 15:31:12 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from app.errors import VaccineError, NotWearingMaskError
 
 
-def go_to_cafe(friends: list, cafe: str) -> str:
+def go_to_cafe(friends: list[dict], cafe: str) -> str:
     masks_to_buy = 0
 
     for friend in friends:
