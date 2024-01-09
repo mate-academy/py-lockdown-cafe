8ef991eb22eb6454428610c6777baca70a2f7278
Valentyn <sonik2001www@gmail.com>
2024-01-09 12:47:35 +0200

    finished task with exceptions

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+from app.cafe import Cafe
+from app.errors import VaccineError, NotWearingMaskError
+
+
+def go_to_cafe(friends: list, cafe: Cafe) -> str:
+
+    friends_without_mask = 0
+
+    for visitor in friends:
+        try:
+            cafe.visit_cafe(visitor)
+        except VaccineError:
+            return "All friends should be vaccinated"
+        except NotWearingMaskError:
+            friends_without_mask += 1
+
+    if friends_without_mask:
+        return f"Friends should buy {friends_without_mask} masks"
+
+    return f"Friends can go to {cafe.name}"
