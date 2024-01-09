14250d48caf2ff60c2975a20616ed6bac158b3bb
AlexHCom <alexhcomgrit@gmail.com>
2024-01-09 14:22:09 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -9,16 +9,21 @@ from errors import (
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
     vaccinated_friends = [friend for friend in friends if "vaccine" in friend]
-    masks_to_buy = sum(not friend.get("wearing_a_mask", False) for friend in friends)
+    masks_to_buy = sum(not friend.get("wearing_a_mask", False)
+                       for friend in friends)
 
     try:
         for friend in vaccinated_friends:
             cafe.visit_cafe(friend)
 
         if masks_to_buy > 0:
-            raise NotWearingMaskError(f"Friends should buy {masks_to_buy} masks")
+            raise (NotWearingMaskError
+                   (f"Friends should buy {masks_to_buy} masks"))
 
         return f"Friends can go to {cafe.name}"
 
-    except (NotVaccinatedError, VaccineError, OutdatedVaccineError, NotWearingMaskError) as e:
+    except (NotVaccinatedError,
+            VaccineError,
+            OutdatedVaccineError,
+            NotWearingMaskError) as e:
         return str(e)
