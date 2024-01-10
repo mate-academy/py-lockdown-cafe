300e95184d13ff87b4286147b957870bb8805351
Serhii Hrynyshyn <hrynyshyn.serhii@gmail.com>
2024-01-10 08:46:52 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
-import datetime
 from app.cafe import Cafe
+from app.errors import VaccineError, NotWearingMaskError
 
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
@@ -10,35 +10,15 @@ def go_to_cafe(friends: list, cafe: Cafe) -> str:
         try:
             cafe.visit_cafe(person)
 
-        except Exception as err:
-            noerror = False
-            if str(err) == "OutdatedVaccineError" or str(
-                    err) == "NotVaccinatedError":
-                return "All friends should be vaccinated"
+        except VaccineError:
 
-            if str(err) == "NotWearingMaskError":
-                masks_to_buy += 1
+            return "All friends should be vaccinated"
+
+        except NotWearingMaskError:
+
+            masks_to_buy += 1
 
     if masks_to_buy > 0:
         return f"Friends should buy {masks_to_buy} masks"
     if noerror:
         return f"Friends can go to {cafe.name}"
-
-
-friends = [
-    {
-        "name": "Alisa",
-        "vaccine": {
-            "expiration_date": datetime.date(2100, 12, 5)
-        },
-        "wearing_a_mask": True
-    },
-    {
-        "name": "Bob",
-        "vaccine": {
-            "expiration_date": datetime.date(2100, 12, 5)
-        },
-        "wearing_a_mask": True
-    },
-]
-print(go_to_cafe(friends, Cafe("KFC")))
