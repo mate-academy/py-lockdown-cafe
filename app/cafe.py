425efa046c901fa8d1299d0bb482c5643559f68f
el chibuk <chebukin404@gmail.com>
2024-01-10 12:06:49 +0200

    fixed if statement and errors description

--- app/cafe.py
+++ app/cafe.py
@@ -10,9 +10,9 @@ class Cafe:
 
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
-            raise NotVaccinatedError("All visitors should be vaccinated!")
+            raise NotVaccinatedError("Someone is not vaccinated!")
         elif visitor["vaccine"]["expiration_date"] < datetime.date.today():
             raise OutdatedVaccineError("Vaccine is outdated!")
         elif visitor["wearing_a_mask"] is False:
-            raise NotWearingMaskError("All visitors should wear masks")
+            raise NotWearingMaskError()
         return f"Welcome to {self.name}"
