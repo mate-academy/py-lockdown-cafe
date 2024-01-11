4c3de19a662783328ae1fd7cf9ca26eaeee537fb
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-01-11 17:12:48 +0200

    improve 'if' statements

--- app/cafe.py
+++ app/cafe.py
@@ -13,8 +13,8 @@ class Cafe:
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
             raise NotVaccinatedError("Visitor should be vaccinated")
-        if datetime.date.today() > visitor["vaccine"]["expiration_date"]:
+        elif datetime.date.today() > visitor["vaccine"]["expiration_date"]:
             raise OutdatedVaccineError("Vaccine is outdated!")
-        if not visitor["wearing_a_mask"]:
+        elif not visitor["wearing_a_mask"]:
             raise NotWearingMaskError("Visitor should be wearing a mask!")
         return f"Welcome to {self.name}"
