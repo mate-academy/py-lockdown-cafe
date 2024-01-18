e52f512c55da569690acbd38948b7e076c2098d9
Mariia Movchan <mariia.sergeevna.k@gmail.com>
2024-01-18 20:53:34 +0100

    replace access by key with the .get()

--- app/cafe.py
+++ app/cafe.py
@@ -14,8 +14,8 @@ class Cafe:
     def visit_cafe(self, visitor: dict) -> str | Exception:
         if "vaccine" not in visitor:
             raise NotVaccinatedError("Visitor should be vaccinated.")
-        if visitor["vaccine"]["expiration_date"] < date.today():
+        if visitor["vaccine"].get("expiration_date") < date.today():
             raise OutdatedVaccineError("The vaccine must not be expired.")
-        if visitor["wearing_a_mask"] is False:
+        if not visitor.get("wearing_a_mask"):
             raise NotWearingMaskError("All visitors must wear masks.")
         return f"Welcome to {self.name}"
