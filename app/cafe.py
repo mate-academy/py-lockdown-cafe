6bf94a64a84f4b613b202376ef22d34bbd43aca4
PythonZem <pythonzem@gmail.com>
2024-01-22 17:57:08 +0200

    fix code style

--- app/cafe.py
+++ app/cafe.py
@@ -12,11 +12,17 @@ class Cafe:
 
     def visit_cafe(self, visitors: dict) -> str:
         if not visitors.get("vaccine"):
-            raise NotVaccinatedError("NotVaccinatedError")
+            raise NotVaccinatedError(
+                "NotVaccinatedError: Client without vaccine"
+            )
         if visitors["vaccine"]["expiration_date"] < datetime.date.today():
-            raise OutdatedVaccineError("OutdatedVaccineError")
+            raise OutdatedVaccineError(
+                "OutdatedVaccineError: The client's vaccine was expired"
+            )
         if not visitors.get("wearing_a_mask"):
-            raise NotWearingMaskError("NotWearingMaskError")
+            raise NotWearingMaskError(
+                "NotWearingMaskError: Client don`t has/wear the mask"
+            )
         return (
             f"Welcome to {self.name}"
         )
--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from app.errors import VaccineError, NotWearingMaskError
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
     mask_to_buy = 0
+
     for visitor in friends:
         try:
             cafe.visit_cafe(visitor)
@@ -13,6 +14,7 @@ def go_to_cafe(friends: list, cafe: Cafe) -> str:
             )
         except NotWearingMaskError:
             mask_to_buy += 1
+
     if mask_to_buy != 0:
         return (
             f"Friends should buy {mask_to_buy} masks"
