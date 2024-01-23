61823b332eddf2a2bd2334d11bd1c273baf89637
Yevhen <evgeniy.playid@icloud.com>
2024-01-23 23:00:26 +0200

    'Solution'

--- app/cafe.py
+++ app/cafe.py
@@ -14,10 +14,13 @@ class Cafe:
     def visit_cafe(self, visitor: dict) -> str:
         for _ in visitor:
             if "vaccine" not in visitor:
-                raise NotVaccinatedError("All friends should be vaccinated")
+                raise NotVaccinatedError(f"{visitor["name"]}"
+                                         f" should be vaccinated")
             elif visitor["vaccine"]["expiration_date"] < datetime.date.today():
-                raise OutdatedVaccineError("All friends should be vaccinated")
+                raise OutdatedVaccineError(f"{visitor["name"]}"
+                                           f" should be vaccinated")
             elif not visitor["wearing_a_mask"]:
-                raise NotWearingMaskError("Friends should buy masks")
+                raise NotWearingMaskError(f"{visitor["name"]}"
+                                          f" should buy masks")
             else:
                 return f"Welcome to {self.name}"
--- app/main.py
+++ app/main.py
@@ -1,19 +1,16 @@
 from app.cafe import Cafe
-from app.errors import NotWearingMaskError, VaccineError
+from app.errors import (NotWearingMaskError, VaccineError)
 
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
     masks_to_buy = 0
-    success = 0
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
-            success += 1
         except VaccineError:
             return "All friends should be vaccinated"
         except NotWearingMaskError:
             masks_to_buy += 1
-    if masks_to_buy > 0:
+    if masks_to_buy:
         return f"Friends should buy {masks_to_buy} masks"
-    elif len(friends) == success:
-        return f"Friends can go to {cafe.name}"
+    return f"Friends can go to {cafe.name}"
