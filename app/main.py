d352ff6804b49e066abef9783f93590718134e1d
Vyacheslav Malchyk <k2.mvo98@gmail.com>
2024-01-23 22:04:02 +0200

    Solution 3

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from app.errors import VaccineError, NotWearingMaskError
 from app.cafe import Cafe
 
 
-def go_to_cafe(friends: list, cafe: Cafe) -> str:
+def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
     mask_to_buy = 0
 
     for friend in friends:
@@ -15,5 +15,5 @@ def go_to_cafe(friends: list, cafe: Cafe) -> str:
 
     if mask_to_buy > 0:
         return f"Friends should buy {mask_to_buy} masks"
-    else:
-        return f"Friends can go to {cafe.name}"
+
+    return f"Friends can go to {cafe.name}"
