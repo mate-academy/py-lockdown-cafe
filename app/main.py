64217afe3cf645dc1c497c1e0929bb41bb409694
volry <v.v.ryzhuk@gmail.com>
2024-01-27 16:15:13 +0200

    add structure

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
 class Cafe:
-    def __init__(self) -> None:
+    def __init__(self, name: str) -> None:
         pass
     
     def visit_cafe(visitor: dict):
+        pass
         
+
+#%%
+visitor = {
+    "name": "Paul",
+    "age": 23,
+}
+
+# %%
