b484e997c995e9b4f4f25c66baed9097ae4e6064
Artem Milchenko <pl301078map2@gmail.com>
2024-02-05 16:06:30 +0200

    fix comments

--- app/cafe.py
+++ app/cafe.py
@@ -16,7 +16,7 @@ class Cafe:
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor.keys():
             raise NotVaccinatedError()
-        if visitor["vaccine"]["expiration_date"] < datetime.date.today():
+        if visitor["vaccine"].get("expiration_date") < datetime.date.today():
             raise OutdatedVaccineError()
         if not visitor["wearing_a_mask"]:
             raise NotWearingMaskError()
