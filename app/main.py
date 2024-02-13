8a11e0b65f49226fa6ec49fc3e44c105c7df7cf2
Eugen <zond342@gmail.com>
2024-02-13 12:24:00 +0200

    fixed go_to_cafe function type annotation

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from app.errors import (NotVaccinatedError,
                         OutdatedVaccineError, NotWearingMaskError)
 
 
-def go_to_cafe(friends: list, cafe: Cafe) -> str:
+def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
     count_mask = 0
     for friend in friends:
         try:
