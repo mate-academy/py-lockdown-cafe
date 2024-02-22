34f9dbcf60cea16579137a870e462943ce77a288
CreepyMo <blashchukstas@gmail.com>
2024-02-22 11:30:46 +0700

    Replaced outer double quotes with single quotes

--- app/cafe.py
+++ app/cafe.py
@@ -13,14 +13,14 @@ class Cafe:
 
     def visit_cafe(self, visitor: dict) -> str:
         if visitor.get("vaccine") is None:
-            raise NotVaccinatedError(f"{visitor["name"]} is not vaccinated!")
+            raise NotVaccinatedError(f'{visitor["name"]} is not vaccinated!')
         if visitor["vaccine"]["expiration_date"] < datetime.date.today():
             raise OutdatedVaccineError(
-                f"Vaccine is expired for {visitor["name"]}!"
+                f'Vaccine is expired for {visitor["name"]}!'
             )
         if not visitor["wearing_a_mask"]:
             raise NotWearingMaskError(
-                f"{visitor["name"]} is not wearing a mask!"
+                f'{visitor["name"]} is not wearing a mask!'
             )
 
         return f"Welcome to {self.name}"
