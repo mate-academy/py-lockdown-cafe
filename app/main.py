6198f19f25500fbd070093e09e52e9e2ce1e0f71
Vasyl Rybalka <basilrybalka@gmail.com>
2024-03-03 11:33:01 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+from typing import List, Dict
+from .cafe import Cafe
+from .errors import (NotVaccinatedError,
+                     OutdatedVaccineError,
+                     NotWearingMaskError)
+
+
+def go_to_cafe(friends: List[Dict[str, any]], cafe: Cafe) -> str:
+    masks_needed = 0
+    for friend in friends:
+        try:
+            cafe.visit_cafe(friend)
+        except NotVaccinatedError:
+            return "All friends should be vaccinated"
+        except OutdatedVaccineError:
+            return "All friends should be vaccinated"
+        except NotWearingMaskError:
+            masks_needed += 1
+
+    if masks_needed > 0:
+        return f"Friends should buy {masks_needed} masks"
+    return f"Friends can go to {cafe.name}"
