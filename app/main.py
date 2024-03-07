4233d367c40d4e3a4b397eb311e89988e3f6a124
Mariia Sydoruk <marisydoruk12@gmail.com>
2024-03-08 01:23:49 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,28 +1,23 @@
 from app.cafe import Cafe
 from app.errors import (
-    NotVaccinatedError,
-    OutdatedVaccineError,
+    VaccineError,
     NotWearingMaskError
 )
 
 
-def go_to_cafe(cafe: Cafe, friends: list) -> str:
-    can_return = True
+def go_to_cafe(cafe: Cafe, friends: list[dict]) -> str:
     masks_to_buy = 0
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
         except (
-            NotVaccinatedError,
-            OutdatedVaccineError,
+            VaccineError,
             NotWearingMaskError
         ) as e:
-            can_return = False
-            if isinstance(e, (NotVaccinatedError, OutdatedVaccineError)):
+            if isinstance(e, VaccineError):
                 return "All friends should be vaccinated"
             elif isinstance(e, NotWearingMaskError):
                 masks_to_buy += 1
     if masks_to_buy > 0:
         return f"Friends should buy {masks_to_buy} masks"
-    if can_return:
-        return f"Friends can go to {cafe.name}"
+    return f"Friends can go to {cafe.name}"
