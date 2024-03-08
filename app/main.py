b4c5aed30c82d52000d32c3ae5194b13952165b2
mvsydoruk <150350026+mvsydoruk@users.noreply.github.com>
2024-03-08 21:03:46 +0200

    Update app/main.py
    Co-authored-by: Dmytro Skoryi <76879254+Dimosphen1@users.noreply.github.com>

--- app/main.py
+++ app/main.py
@@ -10,14 +10,10 @@ def go_to_cafe(cafe: Cafe, friends: list[dict]) -> str:
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
-        except (
-            VaccineError,
-            NotWearingMaskError
-        ) as e:
-            if isinstance(e, VaccineError):
-                return "All friends should be vaccinated"
-            elif isinstance(e, NotWearingMaskError):
-                masks_to_buy += 1
+        except VaccineError:
+            ....
+        except NotWearingMaskError:
+            ....
     if masks_to_buy > 0:
         return f"Friends should buy {masks_to_buy} masks"
     return f"Friends can go to {cafe.name}"
