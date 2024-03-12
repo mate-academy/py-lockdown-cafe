100b3bb3533144626d05147a35ec91f578804952
Viesich <v.s.vesich@gmail.com>
2024-03-12 12:41:42 +0200

    removed unnecessary keywords and variables

--- app/main.py
+++ app/main.py
@@ -3,20 +3,15 @@ from app.errors import VaccineError, NotWearingMaskError
 
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
-    vaccinated = True
     mask_not_wear_count = 0
 
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
         except VaccineError:
-            vaccinated = False
+            return "All friends should be vaccinated"
         except NotWearingMaskError:
             mask_not_wear_count += 1
-
-    if not vaccinated:
-        return "All friends should be vaccinated"
-    elif mask_not_wear_count > 0:
+    if mask_not_wear_count > 0:
         return f"Friends should buy {mask_not_wear_count} masks"
-    else:
-        return f"Friends can go to {cafe.name}"
+    return f"Friends can go to {cafe.name}"
