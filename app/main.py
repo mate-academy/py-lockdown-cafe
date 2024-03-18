e6cd5d9bf9e38c651df0eb547f176b4923b1b7a6
bujhm <Bujhm99@gmail.com>
2024-03-18 18:01:29 +0200

    No problems

--- app/main.py
+++ app/main.py
@@ -4,18 +4,15 @@ from app.errors import NotWearingMaskError, VaccineError
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
     no_mask = 0
-    problems = False
     for human in friends:
         try:
             cafe.visit_cafe(human)
         except VaccineError:
             return "All friends should be vaccinated"
         except NotWearingMaskError:
-            problems = True
             no_mask += 1
 
     if no_mask:
         return f"Friends should buy {no_mask} masks"
-
-    if not problems:
+    else:
         return f"Friends can go to {cafe.name}"
