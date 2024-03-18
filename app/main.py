1dabfee01471a09c3ca18d30505fa7f2addd2ef5
Mikita Havryliuk <chinter.games1@gmail.com>
2024-03-18 09:12:19 +0200

    fixes: optimized go_to_cafe

--- app/main.py
+++ app/main.py
@@ -3,15 +3,15 @@ from app.errors import VaccineError, NotWearingMaskError
 
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
-    try:
-        masks_to_buy = 0
-        for friend in friends:
-            try:
-                cafe.visit_cafe(friend)
-            except NotWearingMaskError:
-                masks_to_buy += 1
-        if masks_to_buy:
-            return f"Friends should buy {masks_to_buy} masks"
-        return f"Friends can go to {cafe.name}"
-    except VaccineError:
-        return "All friends should be vaccinated"
+    masks_to_buy = 0
+    for friend in friends:
+        try:
+            cafe.visit_cafe(friend)
+        except NotWearingMaskError:
+            masks_to_buy += 1
+        except VaccineError:
+            return "All friends should be vaccinated"
+
+    if masks_to_buy:
+        return f"Friends should buy {masks_to_buy} masks"
+    return f"Friends can go to {cafe.name}"
