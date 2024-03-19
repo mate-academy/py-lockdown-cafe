6aabc43017563274347d2e6e32a0647dcba0110d
vova-stoliar <vova.stoliar@overdose.digital>
2024-03-19 19:44:15 +0200

    Refactor

--- app/main.py
+++ app/main.py
@@ -5,20 +5,15 @@ from app.errors import NotWearingMaskError, VaccineError
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
     masks_to_buy = 0
 
-    try:
-        for friend in friends:
+    for friend in friends:
+        try:
+            cafe.visit_cafe(friend)
+        except NotWearingMaskError:
+            masks_to_buy += 1
+        except VaccineError:
+            return "All friends should be vaccinated"
 
-            try:
-                cafe.visit_cafe(friend)
-            except NotWearingMaskError:
-                masks_to_buy += 1
-
-        if masks_to_buy:
-            raise NotWearingMaskError()
-
-    except NotWearingMaskError:
+    if masks_to_buy:
         return f"Friends should buy {masks_to_buy} masks"
-    except VaccineError:
-        return "All friends should be vaccinated"
 
     return f"Friends can go to {cafe.name}"
