f90ffd55dbfa07f9f093f0d0ee578e923279b36b
Olha <olgazinevych@gmail.com>
2024-03-20 11:59:35 +0200

    Used get to avoid KeyError

--- app/cafe.py
+++ app/cafe.py
@@ -13,8 +13,8 @@ class Cafe:
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
             raise NotVaccinatedError("The visitor is not vaccinated.")
-        if visitor["vaccine"]["expiration_date"] < datetime.date.today():
+        if visitor["vaccine"].get("expiration_date") < datetime.date.today():
             raise OutdatedVaccineError("The vaccine is expired.")
-        if visitor["wearing_a_mask"] is False:
+        if visitor.get("wearing_a_mask") is False:
             raise NotWearingMaskError("The visitor is not wearing a mask.")
         return f"Welcome to {self.name}"
