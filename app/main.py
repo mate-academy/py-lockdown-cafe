caba0b2d0e74df6600602cda4de1731e8b6f2d4d
Tymur Shmorhun <shmorguntimur@gmail.com>
2024-03-24 12:18:52 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -11,13 +11,7 @@ def go_to_cafe(friends: List[Dict], cafe: Cafe) -> str:
         except VaccineError:
             return "All friends should be vaccinated"
         except NotWearingMaskError:
-            continue
-    for friend in friends:
-        try:
-            cafe.visit_cafe(friend)
-        except NotWearingMaskError:
             mask_to_buy += 1
-            continue
     if mask_to_buy > 0:
         return f"Friends should buy {mask_to_buy} masks"
     else:
