00bc0af772d984e8f1648a0fbaa2326cab1eadf2
Tetyana Pavlyuk <tetyana.pas@gmail.com>
2024-03-24 12:58:37 +0200

    Solution with detail type annotation and without else

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from app.cafe import Cafe
 from app.errors import VaccineError, NotWearingMaskError
 
 
-def go_to_cafe(friends: list, cafe: Cafe) -> str | None:
+def go_to_cafe(friends: list[dict], cafe: Cafe) -> str | None:
     mask_count: int = 0
     for friend in friends:
         try:
@@ -13,5 +13,4 @@ def go_to_cafe(friends: list, cafe: Cafe) -> str | None:
             mask_count += 1
     if mask_count == 0:
         return f"Friends can go to {cafe.name}"
-    else:
-        return f"Friends should buy {mask_count} masks"
+    return f"Friends should buy {mask_count} masks"
