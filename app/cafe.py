271d985938b1244251f1a2c118a398f434795055
Vitalii <warcied1@gmail.com>
2024-03-24 12:12:28 +0200

    solution

---
+++ app/cafe.py
@@ -0,0 +1,21 @@
+import datetime
+
+from app.errors import (NotVaccinatedError,
+                        OutdatedVaccineError, NotWearingMaskError)
+
+
+class Cafe:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def visit_cafe(self, visitor: dict) -> str:
+        if "vaccine" not in visitor.keys():
+            raise NotVaccinatedError
+        expiration_date = visitor["vaccine"].get("expiration_date")
+        if (expiration_date is None
+                or expiration_date < datetime.date.today()):
+            raise OutdatedVaccineError
+        visitor_wears_a_mask = visitor.get("wearing_a_mask")
+        if (visitor_wears_a_mask is None) or (not visitor_wears_a_mask):
+            raise NotWearingMaskError
+        return f"Welcome to {self.name}"
--- app/main.py
+++ app/main.py
@@ -1,1 +1,23 @@
-# write your code here
+from typing import List, Dict
+
+from app.cafe import Cafe
+from app.errors import NotWearingMaskError, VaccineError
+
+
+def go_to_cafe(friends: List[Dict], cafe: Cafe) -> str:
+    not_all_vaccinated = False
+    masks_to_buy = 0
+    for friend in friends:
+        try:
+            cafe.visit_cafe(friend)
+        except VaccineError:
+            not_all_vaccinated = True
+        except NotWearingMaskError:
+            masks_to_buy += 1
+
+    if not_all_vaccinated:
+        return "All friends should be vaccinated"
+    if masks_to_buy:
+        return f"Friends should buy {masks_to_buy} masks"
+
+    return f"Friends can go to {cafe.name}"
