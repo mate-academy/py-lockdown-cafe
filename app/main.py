3b8ed9096a83383dbc3abecca9967a2da5d88e7d
= <=>
2024-03-29 00:10:40 +0200

    fixed problems

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ def go_to_cafe(friends: list, cafe: Cafe) -> str:
         except VaccineError:
             return "All friends should be vaccinated"
         except NotWearingMaskError:
-            masks_to_buy = True
+            masks_to_buy += 1
 
     if masks_to_buy:
         return f"Friends should buy {masks_to_buy} masks"
