334636198b3600e88ad70c1652d7794d71d3fb63
kekW <panicua12@gmail.com>
2024-03-28 19:14:50 +0200

    made conditions in visit_cafe method more universal

--- app/cafe.py
+++ app/cafe.py
@@ -17,11 +17,10 @@ class Cafe:
         if "vaccine" not in visitor:
             raise NotVaccinatedError("Visitor is not vaccinated")
         elif "expiration_date" in visitor["vaccine"]:
-            expiration_date = visitor["vaccine"]["expiration_date"]
-            if expiration_date < datetime.date.today():
+            if visitor["vaccine"]["expiration_date"] < datetime.date.today():
                 raise OutdatedVaccineError("Visitors vaccine is outdated")
 
-        if "wearing_a_mask" in visitor and not visitor["wearing_a_mask"]:
+        if "wearing_a_mask" not in visitor or not visitor["wearing_a_mask"]:
             raise NotWearingMaskError("Visitor is not wearing a mask")
 
         return f"Welcome to {self.name}"
