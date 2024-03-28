bd80b4271cdddb7b3f395030b628351ced3073a5
Vitalina Malynovska <vitalinamalinovskaya557@gmail.com>
2024-03-28 18:25:14 +0100

    added return to the VaccineError in main

--- app/main.py
+++ app/main.py
@@ -3,18 +3,15 @@ from app.errors import VaccineError, NotWearingMaskError
 
 
 def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
-    count_of_non_vaccinated = 0
     do_not_have_mask = 0
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
         except VaccineError:
-            count_of_non_vaccinated += 1
+            return "All friends should be vaccinated"
         except NotWearingMaskError:
             do_not_have_mask += 1
 
-    if count_of_non_vaccinated:
-        return "All friends should be vaccinated"
     if do_not_have_mask:
         return f"Friends should buy {do_not_have_mask} masks"
     return f"Friends can go to {cafe.name}"
