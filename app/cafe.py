604171584180d85cb86cb3a25bb6e1665780a7f7
Shershen Alexandra <hornet240204@gmail.com>
2024-03-29 09:05:17 +0200

    Remove list of errors

--- app/cafe.py
+++ app/cafe.py
@@ -11,11 +11,7 @@ class Cafe:
     def __init__(self, name: str) -> None:
         self.name = name
 
-    def visit_cafe(self, visitor: dict) -> \
-            (NotVaccinatedError
-             | OutdatedVaccineError
-             | NotWearingMaskError
-             | str):
+    def visit_cafe(self, visitor: dict) -> str | None:
         if "vaccine" not in visitor:
             raise NotVaccinatedError("Visitor is not vaccinated")
 
