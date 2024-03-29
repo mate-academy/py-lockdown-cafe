f3297a0e41a778a31ea5bb3e9dd55b2ac0e2e70e
Sofiia <sonagm9@gmail.com>
2024-03-29 20:09:44 +0200

    'Solution'

---
+++ app/cafe.py
@@ -0,0 +1,23 @@
+import datetime
+from app.errors import (NotVaccinatedError,
+                        OutdatedVaccineError,
+                        NotWearingMaskError)
+
+
+class Cafe:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def visit_cafe(self, visitor: dict) -> str:
+        if "vaccine" not in visitor:
+            raise NotVaccinatedError()
+
+        vaccine = visitor["vaccine"]
+        expiration_date = vaccine.get("expiration_date")
+        if not expiration_date or expiration_date < datetime.date.today():
+            raise OutdatedVaccineError()
+
+        if not visitor.get("wearing_a_mask", False):
+            raise NotWearingMaskError()
+
+        return f"Welcome to {self.name}"
--- app/main.py
+++ app/main.py
@@ -1,1 +1,28 @@
-# write your code here
+from app.cafe import Cafe
+from app.errors import (NotVaccinatedError,
+                        OutdatedVaccineError,
+                        NotWearingMaskError)
+
+
+def go_to_cafe(friends: list, cafe: Cafe) -> str:
+    try:
+        vaccinated_friends = 0
+        masks_to_buy = 0
+
+        for friend in friends:
+            try:
+                cafe.visit_cafe(friend)
+                vaccinated_friends += 1
+            except NotVaccinatedError:
+                return "All friends should be vaccinated"
+            except OutdatedVaccineError:
+                return "All friends should be vaccinated"
+            except NotWearingMaskError:
+                masks_to_buy += 1
+
+        if vaccinated_friends == len(friends):
+            return f"Friends can go to {cafe.name}"
+        else:
+            return f"Friends should buy {masks_to_buy} masks"
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
