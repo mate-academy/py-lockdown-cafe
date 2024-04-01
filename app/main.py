512f82f09289e20a796c8840b65d9a9189fb3213
bwaah1 <chykivan@gmail.com>
2024-04-02 01:11:21 +0300

    Silmlify condition in go_to_cafe function

--- app/main.py
+++ app/main.py
@@ -3,19 +3,16 @@ from app.errors import VaccineError, NotWearingMaskError
 
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
-    not_vaccinated = False
     masks_count = 0
 
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
         except VaccineError:
-            not_vaccinated = True
+            return "All friends should be vaccinated"
         except NotWearingMaskError:
             masks_count += 1
 
-    if not_vaccinated:
-        return "All friends should be vaccinated"
-    elif masks_count > 0:
+    if masks_count > 0:
         return f"Friends should buy {masks_count} masks"
     return f"Friends can go to {cafe.name}"
