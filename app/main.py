619b73cbfbfa41994e0077431b90fdff66e25638
Mikola Rozhansky <bymikola@gmail.com>
2024-04-04 22:29:58 +0300

    1

--- app/main.py
+++ app/main.py
@@ -1,14 +1,25 @@
 from cafe import Cafe
 from errors import *
 
+
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
     mask = 0
+    n = 0
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
-            return f"Friends can go to {cafe.name}"
+
         except VaccineError:
             return f"All friends should be vaccinated"
         except NotWearingMaskError:
             mask += 1
+
+    if mask:
+        return f"Friends should by {mask} masks"
+
+    return "Friends can go to {cafe.name}"
+
+
+
+
 
