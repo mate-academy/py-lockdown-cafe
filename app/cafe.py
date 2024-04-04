549ba988132d5598ac9d32ff1e2765dc8696939d
Mikola Rozhansky <bymikola@gmail.com>
2024-04-04 22:13:50 +0300

    1

--- app/cafe.py
+++ app/cafe.py
@@ -5,13 +5,14 @@ class Cafe:
     def __init__(self, name: str) -> None:
         self.name = name
 
-    def visit_cafe(self, visitors: dict) -> None:
+    def visit_cafe(self, visitors: dict) -> str:
         if not ("vaccine" in visitors):
             raise NotVaccinatedError("Not Vaccinated")
         if visitors["vaccine"]["expiration_date"] < datetime.date.today():
             raise OutdatedVaccineError("Outdated Vaccine")
         if not visitors["wearing_a_mask"]:
             raise NotWearingMaskError("Not Wearing mask")
+        return f"Welcome to {self.name}"
 
 
 
