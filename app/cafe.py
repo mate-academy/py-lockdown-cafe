db2ef1dcd82ddcbf869ce594fe4eae5750256922
NULLrider <prilepa.ukr@gmail.com>
2024-04-08 01:46:40 +0200

    Solution

--- app/cafe.py
+++ app/cafe.py
@@ -11,21 +11,9 @@ class Cafe:
 
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
-            raise NotVaccinatedError("something 3")  # TODO: put propper error
-        if visitor["vaccine"]["expiration_date"] < datetime.date.today():
+            raise NotVaccinatedError("something 3")
+        elif visitor["vaccine"]["expiration_date"] < datetime.date.today():
             raise OutdatedVaccineError("something")
-        if not visitor.get("wearing_a_mask", False):
+        elif not visitor.get("wearing_a_mask", False):
             raise NotWearingMaskError("something 2")
         return f"Welcome to {self.name}"
-
-
-kfc = Cafe("KFC")
-visitor = {
-    "name": "Paul",
-    "age": 23,
-    "vaccine": {
-        "expiration_date": datetime.date.today()
-    },
-    "wearing_a_mask": True
-}
-kfc.visit_cafe(visitor)
