c518a6ba25710a270e614167df2174eba323c622
Hanna Holovach <ann.golovach@gmail.com>
2024-04-07 17:22:00 +0200

    delete redundant variables

--- app/main.py
+++ app/main.py
@@ -5,23 +5,18 @@ from app.errors import VaccineError, NotWearingMaskError
 
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
-    someone_not_vaccinated = False
-    someone_not_masked = False
+
     masks_to_buy = 0
 
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
         except VaccineError:
-            someone_not_vaccinated = True
+            return "All friends should be vaccinated"
         except NotWearingMaskError:
-            someone_not_masked = True
-            masks_to_buy = sum(not friend.get("wearing_a_mask", False)
-                               for friend in friends)
+            masks_to_buy += 1
 
-    if someone_not_vaccinated:
-        return "All friends should be vaccinated"
-    elif someone_not_masked:
+    if masks_to_buy != 0:
         return f"Friends should buy {masks_to_buy} masks"
     else:
         return f"Friends can go to {cafe.name}"
