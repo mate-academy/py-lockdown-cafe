5291b40962c81008480372c6f3445a2393574b1d
Eugene <eugenekozhenevskiy@gmail.com>
2024-04-10 22:05:12 +0300

    Push for check

---
+++ app/cafe.py
@@ -0,0 +1,21 @@
+from datetime import date
+from app.errors import (
+    NotVaccinatedError, OutdatedVaccineError, NotWearingMaskError)
+
+
+class Cafe:
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def visit_cafe(self, visitor: dict) -> str:
+        if "vaccine" not in visitor:
+            raise NotVaccinatedError("You have to be vaccinated!")
+
+        elif visitor["vaccine"]["expiration_date"] < date.today():
+            raise OutdatedVaccineError("Your vaccine is broken!")
+
+        elif visitor["wearing_a_mask"] is False:
+            raise NotWearingMaskError("Put on your mask please!")
+
+        return f"Welcome to {self.name}"
