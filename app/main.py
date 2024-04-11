2ab1aa1d9508f84b5ac585a6c0cd30a121ddb17f
semermaks <semermaks@mail.com>
2024-04-11 13:15:29 +0300

    Alfabet imports

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from app.cafe import Cafe
-from app.errors import VaccineError, NotWearingMaskError
+
+from app.errors import NotWearingMaskError, VaccineError
 
 
 def go_to_cafe(friends: list[dict], cafe: Cafe) -> str | None:
