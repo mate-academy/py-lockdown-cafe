95ad3c614e77aeb3e3339ec4977f93227535b268
MRGL <Sonemongoo@gmail.com>
2024-04-12 19:14:55 +0300

    'flake8_Fix'

--- app/cafe.py
+++ app/cafe.py
@@ -13,15 +13,15 @@ class Cafe:
     def visit_cafe(self, visitor: dict) -> str:
         vac = visitor.get("vaccine")
         if vac is None:
-            raise NotVaccinatedError(f"{visitor["name"]}: No vaccine provided")
+            raise NotVaccinatedError(f"{visitor['name']}: No vaccine provided")
 
         current_date = datetime.date.today()
         expiration_date = vac.get("expiration_date")
         if current_date > expiration_date:
-            raise OutdatedVaccineError(f"{visitor["name"]}: Vaccine expired")
+            raise OutdatedVaccineError(f"{visitor['name']}: Vaccine expired")
 
         wearing_mask = visitor.get("wearing_a_mask")
         if wearing_mask is not True:
-            raise NotWearingMaskError(f"{visitor["name"]}: No mask provided")
+            raise NotWearingMaskError(f"{visitor['name']}: No mask provided")
 
         return f"Welcome to {self.name}"
