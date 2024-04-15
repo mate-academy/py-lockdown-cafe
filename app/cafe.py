da0f5815b77d50435e755a52629b87ed643ea2af
Ruxxell <furman06228@gmail.com>
2024-04-16 01:03:43 +0300

    Fixed 2.0.

--- app/cafe.py
+++ app/cafe.py
@@ -1,19 +1,19 @@
 import datetime
-from app.errors import (NotVaccinatedError,
-                        OutdatedVaccineError, NotWearingMaskError)
+
+from app.errors import (OutdatedVaccineError,
+                        NotWearingMaskError,
+                        NotVaccinatedError)
 
 
 class Cafe:
-    name = ""
-
-    def __init__(self, name: str) -> None:
-        self.name = name
+    def __init__(self, cafe_name: str) -> None:
+        self.name = cafe_name
 
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
-            raise NotVaccinatedError("something 3")
+            raise NotVaccinatedError("Visitor is not vaccinated.")
         elif visitor["vaccine"]["expiration_date"] < datetime.date.today():
-            raise OutdatedVaccineError("something")
+            raise OutdatedVaccineError("Visitor's vaccine is outdated.")
         elif not visitor.get("wearing_a_mask", False):
-            raise NotWearingMaskError("something 2")
+            raise NotWearingMaskError("Visitor is not wearing a mask.")
         return f"Welcome to {self.name}"
--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ def go_to_cafe(friends: list, cafe: Cafe) -> str:
         except VaccineError:
             return "All friends should be vaccinated"
         except NotWearingMaskError:
-            masks_required = masks_required + 1
-    if masks_required != 0:
-        return f"Friends should buy {masks_required} masks"
-    return f"Friends can go to {cafe.name}"
+            masks_required += 1
+    if not masks_required:
+        return f"Friends can go to {cafe.name}"
+    return f"Friends should buy {masks_required} masks"
