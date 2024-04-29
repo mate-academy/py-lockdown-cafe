bbaabee59278c66e2d34dfa61afab0fb24d64971
Olena Poplias <poplyasitasony@gmail.com>
2024-04-29 13:16:54 -0400

    "compare with 0 delete"

--- app/main.py
+++ app/main.py
@@ -11,6 +11,6 @@ def go_to_cafe(friends: [dict], cafe: Cafe) -> str:
             return "All friends should be vaccinated"
         except NotWearingMaskError:
             masks_to_buy += 1
-    if masks_to_buy > 0:
+    if masks_to_buy:
         return f"Friends should buy {masks_to_buy} masks"
     return f"Friends can go to {cafe.name}"
