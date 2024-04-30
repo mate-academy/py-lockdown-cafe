d4da72b914f00f7046b153c6e25f677a004d9982
Oleh <musiychuk09@gmail.com>
2024-04-30 19:29:54 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,29 @@
-# write your code here
+from copy import deepcopy
+
+from app.cafe import Cafe
+from app.errors import NotWearingMaskError, VaccineError
+
+
+def masks_to_buy(friends: list[dict]) -> int:
+    return (
+        len(
+            [friend for friend in friends if friend["wearing_a_mask"] is False]
+        )
+    )
+
+
+def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
+    try:
+        copy_friends = deepcopy(friends)
+        for friend in copy_friends:
+            friend["wearing_a_mask"] = True
+            cafe.visit_cafe(friend)
+    except VaccineError:
+        return "All friends should be vaccinated"
+    try:
+        for friend in friends:
+            cafe.visit_cafe(friend)
+    except NotWearingMaskError:
+        return f"Friends should buy {masks_to_buy(friends)} masks"
+    else:
+        return f"Friends can go to {cafe.name}"
