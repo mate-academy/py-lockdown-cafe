7fd6328f61771501c2cf84485972da830b71ff40
Oleh <musiychuk09@gmail.com>
2024-05-01 13:14:30 +0300

    Only one loop is used in the go_to_cafe function

--- app/cafe.py
+++ app/cafe.py
@@ -14,8 +14,8 @@ class Cafe:
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
             raise NotVaccinatedError("Please get vaccinated. ")
-        if visitor["vaccine"]["expiration_date"] < datetime.date.today():
+        if visitor["vaccine"].get("expiration_date") < datetime.date.today():
             raise OutdatedVaccineError("The previous vaccine has expired. ")
-        if visitor["wearing_a_mask"] is False:
+        if visitor.get("wearing_a_mask") is False:
             raise NotWearingMaskError("Please buy a mask.")
         return f"Welcome to {self.name}"
