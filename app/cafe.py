4bff35d274831af0b442164896e1616a7b798218
Oleh <musiychuk09@gmail.com>
2024-05-01 14:28:51 +0300

    Was deleted one Try-Except

--- app/cafe.py
+++ app/cafe.py
@@ -16,6 +16,6 @@ class Cafe:
             raise NotVaccinatedError("Please get vaccinated. ")
         if visitor["vaccine"].get("expiration_date") < datetime.date.today():
             raise OutdatedVaccineError("The previous vaccine has expired. ")
-        if visitor.get("wearing_a_mask") is False:
+        if not visitor.get("wearing_a_mask"):
             raise NotWearingMaskError("Please buy a mask.")
         return f"Welcome to {self.name}"
--- app/main.py
+++ app/main.py
@@ -4,17 +4,14 @@ from app.errors import NotWearingMaskError, VaccineError
 
 def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
     masks_to_buy = 0
-    try:
-        for friend in friends:
-            try:
-                cafe.visit_cafe(friend)
-            except VaccineError:
-                return "All friends should be vaccinated"
-            except NotWearingMaskError:
-                masks_to_buy += 1
-                pass
-        if masks_to_buy:
-            raise NotWearingMaskError
-    except NotWearingMaskError:
+    for friend in friends:
+        try:
+            cafe.visit_cafe(friend)
+        except VaccineError:
+            return "All friends should be vaccinated"
+        except NotWearingMaskError:
+            masks_to_buy += 1
+            pass
+    if masks_to_buy:
         return f"Friends should buy {masks_to_buy} masks"
     return f"Friends can go to {cafe.name}"
