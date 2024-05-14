37fd47c1799150c33525b54c42f455942ca2216a
Olena Zaritska <zaritskaos@gmail.com>
2024-05-14 13:28:55 +0300

    change import formatting and type annotation

--- app/cafe.py
+++ app/cafe.py
@@ -1,17 +1,17 @@
 import datetime
-from app.errors import (NotVaccinatedError,
-                        OutdatedVaccineError,
-                        NotWearingMaskError)
+
+from app.errors import (
+    NotVaccinatedError,
+    OutdatedVaccineError,
+    NotWearingMaskError
+)
 
 
 class Cafe:
     def __init__(self, name: str) -> None:
         self.name = name
 
-    def visit_cafe(
-            self,
-            visitor: dict
-    ) -> str | NotVaccinatedError | OutdatedVaccineError | NotWearingMaskError:
+    def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
             raise NotVaccinatedError(
                 "visitor dictionary has no key 'vaccine'"
