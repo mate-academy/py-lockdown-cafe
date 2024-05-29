3ee973ebd9df742185630b018181902f753f9450
Bohdan Zinchenko <bogdan.zinchenko.2019@gmail.com>
2024-05-29 12:19:50 +0300

    py-lockdown-cafe v2

--- app/main.py
+++ app/main.py
@@ -11,6 +11,5 @@ def go_to_cafe(friends: list, cafe: Cafe) -> str:
             return "All friends should be vaccinated"
         except NotWearingMaskError:
             masks_to_buy += 1
-    if masks_to_buy:
-        return f"Friends should buy {masks_to_buy} masks"
-    return f"Friends can go to {cafe.name}"
+    return f"Friends should buy {masks_to_buy} masks" if masks_to_buy \
+        else f"Friends can go to {cafe.name}"
