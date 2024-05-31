8f51622a920d6f956cd5912137730db6ee064370
Artem <mikhailetc.artem@gmail.com>
2024-05-31 18:28:53 +0300

    test problem

--- app/main.py
+++ app/main.py
@@ -1,1 +1,37 @@
-# write your code here
+import datetime
+from cafe import Cafe
+from errors import (NotWearingMaskError,
+                    VaccineError)
+
+
+def go_to_cafe(friends: list, cafe: Cafe) -> str:
+    masks_to_buy = 0
+    for friend in friends:
+        try:
+            cafe.visit_cafe(friend)
+        except VaccineError:
+            return "All friends should be vaccinated"
+        except NotWearingMaskError:
+            masks_to_buy += 1
+    if masks_to_buy > 0:
+        return f"Friends should buy {masks_to_buy} masks"
+    return f"Friends can go to {cafe.name}"
+
+
+friends = [
+    {
+        "name": "Alisa",
+        "vaccine": {
+            "expiration_date": datetime.date.today()
+        },
+        "wearing_a_mask": True
+    },
+    {
+        "name": "Bob",
+        "vaccine": {
+            "expiration_date": datetime.date.today()
+        },
+        "wearing_a_mask": True
+    },
+]
+print(go_to_cafe(friends, Cafe("KFC")))
