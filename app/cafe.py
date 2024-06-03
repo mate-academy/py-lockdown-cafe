9db3366c92588bcfa0b02f02a7607efc53c39ded
unlie9 <unlie.devops9@gmail.com>
2024-06-03 21:30:11 +0300

    sol2

--- app/cafe.py
+++ app/cafe.py
@@ -10,9 +10,12 @@ class Cafe:
 
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
-            raise NotVaccinatedError("NotVaccinatedError")
+            raise NotVaccinatedError(f"NotVaccinatedError"
+                                     f" {visitor["name"]}")
         if visitor["vaccine"].get("expiration_date") < datetime.date.today():
-            raise OutdatedVaccineError("OutdatedVaccineError")
+            raise OutdatedVaccineError(f"OutdatedVaccineError"
+                                       f" {visitor["name"]}")
         if visitor["wearing_a_mask"] is False:
-            raise NotWearingMaskError("NotWearingMaskError")
+            raise NotWearingMaskError(f"NotWearingMaskError"
+                                      f" {visitor["name"]}")
         return f"Welcome to {self.name}"
--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from app.errors import (NotVaccinatedError,
                         OutdatedVaccineError, NotWearingMaskError)
+from app.cafe import Cafe
 
 
-def go_to_cafe(friends: list, cafe: str) -> str:
+def go_to_cafe(friends: list, cafe: Cafe) -> str:
     result = f"Friends can go to {cafe.name}"
     masks_to_buy = 0
     for friend in friends:
