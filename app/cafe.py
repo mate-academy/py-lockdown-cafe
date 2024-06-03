a958adeef7b284d477394c236be65e565c13f89b
Oleh Deshko <kochaminny@gmail.com>
2024-06-03 21:56:34 +0300

    Added_description_for_user!!!

--- app/cafe.py
+++ app/cafe.py
@@ -13,12 +13,12 @@ class Cafe:
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
             raise NotVaccinatedError(
-                f"{visitor.get('name')} You are not vaccinated"
+                f"{visitor.get('name')}, you are not vaccinated"
             )
         elif visitor["vaccine"]["expiration_date"] < date.today():
             raise OutdatedVaccineError("OutdatedVaccineError")
         elif not visitor["wearing_a_mask"]:
             raise NotWearingMaskError(
-                f"{visitor.get('name')} need to put on a mask"
+                f"{visitor.get('name')}, you need to put on a mask"
             )
         return f"Welcome to {self.name}"
