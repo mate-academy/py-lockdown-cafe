3ee133276fb3c165301c9c6dc27a4fc965c9f091
Maksym Protsak <maksym.protsak@gmail.com>
2024-06-06 20:51:27 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,26 @@
-# write your code here
+class NotVaccinatedError(Exception):
+    pass
+
+
+class OutdatedVaccineError (Exception):
+    pass
+
+
+class Cafe:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def visit_cafe(self, visitor: dict) -> None:
+        try:
+            visitor["vaccine"]
+        except KeyError:
+            raise NotVaccinatedError
+
+
+if __name__ == "__main__":
+    kfc = Cafe("KFC")
+    visitor = {
+        "name": "Paul",
+        "age": 23,
+    }
+    kfc.visit_cafe(visitor)
