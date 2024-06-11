6beceae0519739cb60d8c5080890aeec82ea6fed
Ira <iramladanovych@gmail.com>
2024-06-11 15:36:22 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from app.cafe import Cafe
-from app.errors import VaccineError, NotVaccinatedError, OutdatedVaccineError, NotWearingMaskError
+from app.errors import NotVaccinatedError, OutdatedVaccineError, NotWearingMaskError
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
     masks_to_buy = 0
