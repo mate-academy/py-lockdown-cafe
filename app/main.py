2f95f66bf65435ad88dc1721005d0a9807bfcb4b
Danylo Minkov <minkov215@gmail.com>
2024-06-15 12:37:29 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
-        except (VaccineError):
+        except VaccineError:
             return "All friends should be vaccinated"
         except NotWearingMaskError:
             masks_to_buy += 1
