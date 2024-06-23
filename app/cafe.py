cab5fab4ea17390dd292ac29fe59b3d5f8273417
Максим Данилів <maksym.danyliv.23@pnu.edu.ua>
2024-06-23 22:36:10 +0300

    Solution

--- app/cafe.py
+++ app/cafe.py
@@ -7,22 +7,26 @@ from app.errors import (
 
 
 class Cafe:
-    def __init__(self, name: str):
+    def __init__(self, name: str) -> None:
         self.name = name
 
     def visit_cafe(self, visitor: dict) -> str:
         # Check if vaccinated
         if "vaccine" not in visitor:
-            raise NotVaccinatedError(f"{visitor['name']} is not vaccinated.")
+            raise NotVaccinatedError(f"{visitor["name"]} is not vaccinated.")
 
         # Check if vaccine is not outdated
         expiration_date = visitor["vaccine"].get("expiration_date")
         if expiration_date < datetime.date.today():
-            raise OutdatedVaccineError(f"{visitor['name']}'s vaccine is outdated.")
+            raise OutdatedVaccineError(
+                f"{visitor["name"]}'s vaccine is outdated."
+            )
 
         # Check if wearing a mask
         if not visitor.get("wearing_a_mask", False):
-            raise NotWearingMaskError(f"{visitor['name']} is not wearing a mask.")
+            raise NotWearingMaskError(
+                f"{visitor["name"]} is not wearing a mask."
+            )
 
         # All checks passed
         return f"Welcome to {self.name}"
