2495f2add32ef75370fecacd9e647b6a1807370a
Andriy Zhuk <andri195595@gmail.com>
2024-06-24 12:59:31 +0300

    +

--- app/cafe.py
+++ app/cafe.py
@@ -1,7 +1,8 @@
-from app.errors import NotVaccinatedError
-from app.errors import OutdatedVaccineError
-from app.errors import NotWearingMaskError
-
+from app.errors import (
+    NotVaccinatedError,
+    OutdatedVaccineError,
+    NotWearingMaskError
+)
 
 import datetime
 
