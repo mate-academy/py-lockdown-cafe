38b93c66321c75200d972c5502106247559ac010
hhorhe <horhehorda@gmail.com>
2024-06-24 19:49:51 +0200

    'Solution_2'

--- app/errors.py
+++ app/errors.py
@@ -1,22 +1,15 @@
 class VaccineError(Exception):
     """Base class for vaccine-related errors."""
 
-    pass
-
 
 class NotVaccinatedError(VaccineError):
     """Raised when the visitor is not vaccinated."""
-
-    pass
 
 
 class OutdatedVaccineError(VaccineError):
     """Raised when the vaccine is outdated."""
 
-    pass
-
 
 class NotWearingMaskError(Exception):
     """Raised when the visitor is not wearing a mask."""
 
-    pass
