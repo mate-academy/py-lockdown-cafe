fbce0609ed13a83b071d315233a76f4f54d28032
Maksim <golevmaksim52@gmail.com>
2024-07-15 00:58:32 +0300

    create a vaccination control for a cafe

--- app/cafe.py
+++ app/cafe.py
@@ -1,5 +1,6 @@
 import datetime
-from .errors import NotVaccinatedError, OutdatedVaccineError, NotWearingMaskError
+from .errors import (
+    NotVaccinatedError, OutdatedVaccineError, NotWearingMaskError)
 
 
 class Cafe:
