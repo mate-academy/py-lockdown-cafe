7f399b5436293a47c20b95ca52c63b2c2e6c89df
maksym_korniev <maksymkorniev88@gmail.com>
2024-07-16 00:09:53 +0300

    refactor too long lines

--- app/cafe.py
+++ app/cafe.py
@@ -1,6 +1,7 @@
 import datetime
 
-from app.errors import NotVaccinatedError, OutdatedVaccineError, NotWearingMaskError
+from app.errors import (NotVaccinatedError, OutdatedVaccineError,
+                        NotWearingMaskError)
 
 
 class Cafe:
@@ -9,13 +10,15 @@ class Cafe:
 
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
-            raise NotVaccinatedError("To visit the cafe you need to be vaccinated!")
+            raise NotVaccinatedError(
+                "To visit the cafe you need to be vaccinated!")
 
         if visitor["vaccine"]["expiration_date"] < datetime.date.today():
             raise OutdatedVaccineError(
                 "To visit the cafe your vaccine shouldn't be expired!")
 
         if not visitor["wearing_a_mask"]:
-            raise NotWearingMaskError("Wear the masks before visiting the cafe!")
+            raise NotWearingMaskError(
+                "Wear the masks before visiting the cafe!")
 
         return f"Welcome to {self.name}"
