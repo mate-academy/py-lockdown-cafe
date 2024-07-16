5337c8c904e0504a211cdfdbd4a3b266d45d88fc
limbxbx <ilya.msd2003@gmail.com>
2024-07-16 14:55:19 +0300

    Implemented Solution

---
+++ app/errors.py
@@ -0,0 +1,18 @@
+class VaccineError(Exception):
+    def __init__(self, message: str = "All friends should be vaccinated") \
+            -> None:
+        super().__init__(message)
+
+
+class NotVaccinatedError(VaccineError):
+    pass
+
+
+class OutdatedVaccineError(VaccineError):
+    pass
+
+
+class NotWearingMaskError(Exception):
+    def __init__(self, message: str = "All friends should wear a mask") \
+            -> None:
+        super().__init__(message)
