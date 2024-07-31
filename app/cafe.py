a3ec3de22eb1b2439875d689a893380f373b6fd6
Halyna Rohatska <rohatskahr@gmail.com>
2024-07-31 16:14:13 +0300

    added needed changes

--- app/cafe.py
+++ app/cafe.py
@@ -10,31 +10,12 @@ class Cafe:
         self.name = name
 
     def visit_cafe(self, visitor: dict) -> str:
-        all_check = True
 
         if not visitor.get("vaccine"):
-            all_check = False
             raise NotVaccinatedError
         if date.today() > visitor["vaccine"]["expiration_date"]:
-            all_check = False
             raise OutdatedVaccineError
-        if not visitor["wearing_a_mask"]:
-            all_check = False
+        if not visitor.get("wearing_a_mask"):
             raise NotWearingMaskError
 
-        if all_check:
-            return f"Welcome to {self.name}"
-
-
-if __name__ == "__main__":
-    vs = Cafe("lux")
-    visitor = {
-        "name": "Paul",
-        "age": 23,
-        "vaccine": {
-            "expiration_date": date(year=2025, month=2, day=23)
-        },
-        "wearing_a_mask": False
-    }
-
-    print(vs.visit_cafe(visitor))
+        return f"Welcome to {self.name}"
