1e047b2d589165b467ba833f1dae175e1c187d63
Illia Losiev <elijahlosiev7@gmail.com>
2024-08-03 15:14:15 +0300

    'fixed-flake8'

--- app/main.py
+++ app/main.py
@@ -1,18 +1,24 @@
 from datetime import date
 from app.cafe import Cafe
-from app.errors import VaccineError, NotVaccinatedError, OutdatedVaccineError, NotWearingMaskError
+from app.errors import (VaccineError,
+                        NotVaccinatedError,
+                        OutdatedVaccineError,
+                        NotWearingMaskError)
 
 
 def go_to_cafe(friends, cafe: Cafe) -> str:
     masks_to_buy = 0
     try:
-
         for friend in friends:
             vaccine = friend.get("vaccine")
             if not vaccine:
-                raise NotVaccinatedError(f"{friend['name']} is not vaccinated.")
+                raise NotVaccinatedError(
+                    f"{friend['name']} is not vaccinated."
+                )
             if vaccine["expiration_date"] < date.today():
-                raise OutdatedVaccineError(f"{friend['name']}'s vaccine is expired.")
+                raise OutdatedVaccineError(
+                    f"{friend['name']}'s vaccine is expired."
+                )
             if not friend.get("wearing_a_mask", True):
                 masks_to_buy += 1
 
@@ -21,5 +27,11 @@ def go_to_cafe(friends, cafe: Cafe) -> str:
 
         return f"Friends can go to {cafe.name}"
 
+    except NotVaccinatedError:
+        return "All friends should be vaccinated"
+    except OutdatedVaccineError:
+        return "All friends should be vaccinated"
+    except NotWearingMaskError:
+        return f"Friends should buy {masks_to_buy} masks"
     except VaccineError:
         return "All friends should be vaccinated"
