efb5cc415ca3885b5d31a61b3541e12852f079c7
Egor Derepovskiy <derepovskiy98@gmail.com>
2024-08-04 22:40:13 +0300

    create cafe with check mask adn vaccine

---
+++ app/cafe.py
@@ -0,0 +1,22 @@
+import datetime
+from app.errors import NotVaccinatedError
+from app.errors import OutdatedVaccineError
+from app.errors import NotWearingMaskError
+
+
+class Cafe:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def visit_cafe(self, visitor: dict) -> str:
+        if "vaccine" not in visitor:
+            raise NotVaccinatedError("The visitor doesn't have a vaccine!")
+
+        expiration_date = visitor["vaccine"]["expiration_date"]
+        if expiration_date < datetime.date.today():
+            raise OutdatedVaccineError("The visitor's vaccine is outdated!")
+
+        if not visitor.get("wearing_a_mask", False):
+            raise NotWearingMaskError("The visitor doesn't have a mask!")
+
+        return f"Welcome to {self.name}"
--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+from app.cafe import Cafe
+from app.errors import NotVaccinatedError
+from app.errors import OutdatedVaccineError
+from app.errors import NotWearingMaskError
+
+
+def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
+    mask_to_buy = 0
+
+    for friend in friends:
+        try:
+            cafe.visit_cafe(friend)
+        except (NotVaccinatedError, OutdatedVaccineError):
+            return "All friends should be vaccinated"
+        except NotWearingMaskError:
+            mask_to_buy += 1
+
+    if mask_to_buy == 0:
+        return f"Friends can go to {cafe.name}"
+    else:
+        return f"Friends should buy {mask_to_buy} masks"
