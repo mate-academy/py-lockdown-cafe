f38bcc8cf593d01bd5e029d3fe547295c242693c
Egor Derepovskiy <derepovskiy98@gmail.com>
2024-08-06 14:48:12 +0300

    create cafe with check mask adn vaccine

--- app/main.py
+++ app/main.py
@@ -16,7 +16,6 @@ def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
         except NotWearingMaskError:
             mask_to_buy += 1
 
-    if mask_to_buy == 0:
-        return f"Friends can go to {cafe.name}"
-
-    return f"Friends should buy {mask_to_buy} masks"
+    if mask_to_buy > 0:
+        return f"Friends should buy {mask_to_buy} masks"
+    return f"Friends can go to {cafe.name}"
