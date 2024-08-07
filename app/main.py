9921a61ce8e8391102303ce5a928b7ab08cd8265
Illia Dubina <armahanz.music@gmail.com>
2024-08-07 19:49:47 +0300

    fixing nesting try except blocks

--- app/main.py
+++ app/main.py
@@ -4,17 +4,13 @@ from app.errors import VaccineError, NotWearingMaskError
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
     masks_to_buy = 0
-    try:
-        for friend in friends:
-            try:
-                cafe.visit_cafe(friend)
-            except NotWearingMaskError:
-                masks_to_buy += 1
-        if masks_to_buy == 1:
-            return f"Friends should buy {masks_to_buy} mask"
-        elif masks_to_buy > 1:
-            return f"Friends should buy {masks_to_buy} masks"
-    except VaccineError:
-        return "All friends should be vaccinated"
-    else:
-        return f"Friends can go to {cafe.name}"
+    for friend in friends:
+        try:
+            cafe.visit_cafe(friend)
+        except NotWearingMaskError:
+            masks_to_buy += 1
+        except VaccineError:
+            return "All friends should be vaccinated"
+    if masks_to_buy > 0:
+        return f"Friends should buy {masks_to_buy} masks"
+    return f"Friends can go to {cafe.name}"
