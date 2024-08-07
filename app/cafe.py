afe4349cc2a1c41ab557489521f89a9a439cb905
Myroslava Nezbrytska <miroslavanezbricka@gmail.com>
2024-08-07 08:32:50 +0300

    fixed

--- app/cafe.py
+++ app/cafe.py
@@ -13,16 +13,11 @@ class Cafe:
         self.name = name
 
     def visit_cafe(self, visitor: dict) -> str:
-        all_rule_done = True
         if not visitor.get("vaccine"):
             raise NotVaccinatedError("Visitor is not vaccinated.")
-            all_rule_done = False
         if (visitor.get("vaccine").get("expiration_date")
                 < datetime.date.today()):
             raise OutdatedVaccineError("Visitor's vaccine is expired.")
-            all_rule_done = False
         if not visitor.get("wearing_a_mask"):
             raise NotWearingMaskError("Visitor is not wearing a mask.")
-            all_rule_done = False
-        if all_rule_done:
-            return (f"Welcome to {self.name}")
+        return f"Welcome to {self.name}"
