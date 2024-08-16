73cb97442edb17f6774c9f3f5474b84989f886aa
Alex <54539kalashnikov@gmail.com>
2024-08-16 10:39:40 +0300

    dell else and optimize the try

--- app/main.py
+++ app/main.py
@@ -3,18 +3,14 @@ from app.errors import VaccineError, NotWearingMaskError
 
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
-    unvaccinated = False
     without_a_mask = 0
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
         except VaccineError:
-            unvaccinated = True
+            return "All friends should be vaccinated"
         except NotWearingMaskError:
             without_a_mask += 1
-    if unvaccinated:
-        return "All friends should be vaccinated"
-    elif without_a_mask > 0:
+    if without_a_mask > 0:
         return f"Friends should buy {without_a_mask} masks"
-    else:
-        return f"Friends can go to {cafe.name}"
+    return f"Friends can go to {cafe.name}"
