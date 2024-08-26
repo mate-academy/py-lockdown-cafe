406540383755166b9e8728d4ed1c8a439862be3b
Anastasiia Karpenko <karpenko.anastasiya@gmail.com>
2024-08-26 11:26:33 +0300

    add go_to_cafe function

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+from app.errors import (VaccineError,
+                        NotWearingMaskError)
+from app.cafe import Cafe
+
+
+def go_to_cafe(friends: list, cafe: Cafe) -> str:
+    masks_to_buy = 0
+    for friend in friends:
+        try:
+            cafe.visit_cafe(friend)
+        except VaccineError:
+            print("All friends should be vaccinated")
+        except NotWearingMaskError:
+            masks_to_buy += 1
+    if masks_to_buy:
+        print(f"Friends should buy {masks_to_buy} masks")
