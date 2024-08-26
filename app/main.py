47b5228003bc2570ce05b229df6e837864cbc7da
Bogdan <bogdan.rylov422@gmail.com>
2024-08-26 23:14:42 +0300

    Reformat code according to mentor's requirements

--- app/main.py
+++ app/main.py
@@ -8,12 +8,12 @@ def go_to_cafe(friends: list, cafe: Cafe) -> str:
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
-        except NotWearingMaskError:
-            lacking_masks_count += 1
         except VaccineError:
             return "All friends should be vaccinated"
+        except NotWearingMaskError:
+            lacking_masks_count += 1
 
-    if lacking_masks_count != 0:
+    if lacking_masks_count:
         return f"Friends should buy {lacking_masks_count} masks"
 
     return f"Friends can go to {cafe.name}"
