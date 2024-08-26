9d95091d8f9172cceeb27f9e5bd14f39fc9fb003
Yaroslav Ivanov <165368790+nrjss67@users.noreply.github.com>
2024-08-26 12:15:58 +0300

    Update cafe.py

--- app/cafe.py
+++ app/cafe.py
@@ -11,12 +11,12 @@ class Cafe:
 
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
-            raise NotVaccinatedError(f"{visitor["name"]} "
+            raise NotVaccinatedError(f"{visitor['name']} "
                                      f"doesn't have vaccine.")
         if visitor.get("vaccine")["expiration_date"] < date.today():
-            raise OutdatedVaccineError(f"{visitor["name"]}'s "
+            raise OutdatedVaccineError(f"{visitor['name']}'s "
                                        f"vaccine is expired.")
         if not visitor.get("wearing_a_mask"):
-            raise NotWearingMaskError(f"{visitor["name"]} "
+            raise NotWearingMaskError(f"{visitor['name']} "
                                       f"not wearing mask.")
         return f"Welcome to {self.name}"
