599ef7bf78f84d9b25d2a4c946888a0ea2bedfa8
Anastasiia Karpenko <karpenko.anastasiya@gmail.com>
2024-08-26 13:34:18 +0300

    code refactor

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from app.errors import (VaccineError,
 from app.cafe import Cafe
 
 
-def go_to_cafe(friends: list, cafe: Cafe) -> str:
+def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
     masks_to_buy = 0
     for friend in friends:
         try:
