782569ce4b14441ab31950994577f1ffd5ebca52
Ruslan Toister <rtoister.cv@gmail.com>
2024-08-29 16:30:41 +0300

    'Solution2'

--- app/cafe.py
+++ app/cafe.py
@@ -16,15 +16,15 @@ class Cafe:
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
             raise NotVaccinatedError(
-                f"{visitor["name"]} is not vaccinated. Access to "
-                f"{self.name} denied."
+                f'{visitor["name"]} is not vaccinated. Access to '
+                f'{self.name} denied.'
             )
 
         expiration_date = visitor["vaccine"].get("expiration_date")
         if expiration_date < datetime.date.today():
             raise OutdatedVaccineError(
-                f"{visitor["name"]} has an outdated vaccine. "
-                f"Access to {self.name} denied."
+                f'{visitor["name"]} has an outdated vaccine. '
+                f'Access to {self.name} denied.'
             )
 
         if not visitor.get("wearing_a_mask", False):
