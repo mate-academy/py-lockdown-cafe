4265e2dbd5dc455ff896595019b8106033e601ea
Denys Podobrii <denys.podobrii@gmail.com>
2024-09-08 00:43:50 +0300

    add doc strings to app.errors module

--- app/errors.py
+++ app/errors.py
@@ -1,14 +1,18 @@
 class NotWearingMaskError(Exception):
+    """Exception raised when a visitor is not wearing a mask."""
     pass
 
 
 class VaccineError(Exception):
+    """Base exception for vaccine-related errors."""
     pass
 
 
 class NotVaccinatedError(VaccineError):
+    """Exception raised when a visitor does not have a vaccine."""
     pass
 
 
 class OutdatedVaccineError(VaccineError):
+    """Exception raised when a visitor's vaccine has expired."""
     pass
