608caae8cba413835a86386027e1dff83e9ddecd
Denys Podobrii <denys.podobrii@gmail.com>
2024-09-07 23:43:17 +0300

    add visit_cafe method to cafe class

--- app/cafe.py
+++ app/cafe.py
@@ -1,3 +1,22 @@
+from datetime import date
+
+from app.errors import NotVaccinatedError
+from app.errors import NotWearingMaskError
+from app.errors import OutdatedVaccineError
+
+
 class Cafe:
     def __init__(self, name: str) -> None:
         self.name = name
+
+    def visit_cafe(self, visitor: dict) -> str:
+        if "vaccine" not in visitor:
+            raise NotVaccinatedError
+
+        if visitor["vaccine"]["expiration_date"] < date.today():
+            raise OutdatedVaccineError
+
+        if not visitor["wearing_a_mask"]:
+            raise NotWearingMaskError
+
+        return "Welcome to {self.name}"
