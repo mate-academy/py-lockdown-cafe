e6db2dbf8ba2a57eb989c2766670d0b61c75aab9
Ivan Hutsuliak <ivan.guculyak@gmail.com>
2024-10-02 00:17:20 +0300

    solution

---
+++ app/errors.py
@@ -0,0 +1,16 @@
+class VaccineError(Exception):
+    def __str__(self) -> str:
+        return "All friends should be vaccinated"
+
+
+class NotVaccinatedError(VaccineError):
+    pass
+
+
+class OutdatedVaccineError(VaccineError):
+    pass
+
+
+class NotWearingMaskError(Exception):
+    def __str__(self) -> str:
+        return "All friends should buy masks"
