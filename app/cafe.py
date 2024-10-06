7d82c219f10600697c65d7406ab118ac6d02fad2
yuliiapyrih <yuliia.pyrih.pm.2018@lpnu.ua>
2024-10-06 16:22:45 +0300

    implement py-lockdown-cafe

---
+++ app/cafe.py
@@ -0,0 +1,25 @@
+from datetime import date
+
+from app.errors import (
+    NotVaccinatedError,
+    OutdatedVaccineError,
+    NotWearingMaskError
+)
+
+
+class Cafe:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def visit_cafe(self, visitor: dict) -> None | str:
+        if "vaccine" not in visitor:
+            raise NotVaccinatedError(f"{visitor['name']} is not vaccinated!")
+
+        expiration_date = visitor["vaccine"].get("expiration_date")
+        if expiration_date < date.today():
+            raise OutdatedVaccineError(f"{visitor['name']}'s vaccine is outdated!")
+
+        if not visitor["wearing_a_mask"]:
+            raise NotWearingMaskError(f"{visitor['name']} is not wearing a mask!")
+
+        return f"Welcome to {self.name}"
