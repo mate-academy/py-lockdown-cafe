5f981af4ceb6b0bc193d7cabd24720ba0c40f232
yuliiapyrih <yuliia.pyrih.pm.2018@lpnu.ua>
2024-10-06 16:28:11 +0300

    fixed syntax errors py-lockdown-cafe

--- app/cafe.py
+++ app/cafe.py
@@ -13,13 +13,19 @@ class Cafe:
 
     def visit_cafe(self, visitor: dict) -> None | str:
         if "vaccine" not in visitor:
-            raise NotVaccinatedError(f"{visitor['name']} is not vaccinated!")
+            raise NotVaccinatedError(
+                f'{visitor["name"]} is not vaccinated!'
+            )
 
         expiration_date = visitor["vaccine"].get("expiration_date")
         if expiration_date < date.today():
-            raise OutdatedVaccineError(f"{visitor['name']}'s vaccine is outdated!")
+            raise OutdatedVaccineError(
+                f'{visitor["name"]}\'s vaccine is outdated!'
+            )
 
         if not visitor["wearing_a_mask"]:
-            raise NotWearingMaskError(f"{visitor['name']} is not wearing a mask!")
+            raise NotWearingMaskError(
+                f'{visitor["name"]} is not wearing a mask!'
+            )
 
         return f"Welcome to {self.name}"
