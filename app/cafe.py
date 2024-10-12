09c27f0e60ee22d5decdc698ad0ccc5d5f53cd4f
Serhiy Dema <sergeydema@gmail.com>
2024-10-12 21:39:23 +0300

    Solution 03

--- app/cafe.py
+++ app/cafe.py
@@ -12,18 +12,18 @@ class Cafe:
 
     def visit_cafe(self, visitor: dict) -> str:
         if not isinstance(visitor, dict):
-            raise NotVaccinatedError("Visitor is incorrect and not vaccinated")
+            raise NotVaccinatedError("Visitor's data is incorrect")
         visitor_name = visitor.get("name")
         visitor_vaccine_dict = visitor.get("vaccine")
 
         if not visitor_vaccine_dict:
             raise NotVaccinatedError(f"Visitor {visitor_name} "
-                                     f"is not vaccinated")
+                                     f"has no vaccination record")
 
         vaccine_date = visitor_vaccine_dict.get("expiration_date")
         if (not isinstance(vaccine_date, datetime.date)
                 or vaccine_date < datetime.date.today()):
-            raise OutdatedVaccineError(f"The vaccine of {visitor_name} "
+            raise OutdatedVaccineError(f"The vaccination of {visitor_name} "
                                        f"is expired")
 
         if not visitor.get("wearing_a_mask"):
