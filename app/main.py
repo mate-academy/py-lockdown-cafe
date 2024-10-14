aa39f20b286bd41f151e031236cfdb74545c4b0c
Alex <delete.rap.stupid@gmail.com>
2024-10-14 19:40:23 +0200

    fix annotation

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from app.errors import (
 from app.cafe import Cafe
 
 
-def go_to_cafe(friends: list, cafe: Cafe) -> str:
+def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
     count_of_mask = 0
     for friend in friends:
         try:
@@ -14,6 +14,6 @@ def go_to_cafe(friends: list, cafe: Cafe) -> str:
             return "All friends should be vaccinated"
         except NotWearingMaskError:
             count_of_mask += 1
-    if count_of_mask != 0:
+    if count_of_mask:
         return f"Friends should buy {count_of_mask} masks"
     return f"Friends can go to {cafe.name}"
