b7fe0819bdd81017b89d5bfdc677d20a8c3feb6f
Matvii Safonov <matveysafonov2@gmail.com>
2024-10-14 12:09:59 +0300

    Solution

---
+++ app/cafe.py
@@ -0,0 +1,24 @@
+import datetime
+from app.errors import (NotVaccinatedError,
+                        OutdatedVaccineError,
+                        NotWearingMaskError)
+
+
+class Cafe:
+    def __init__(self, name: str) -> str:
+        self.name = name
+
+    def visit_cafe(self, visitor: dict) -> str:
+        if "vaccine" not in visitor:
+            raise NotVaccinatedError()
+
+        vaccine_info = visitor["vaccine"]
+        expiration_date = vaccine_info["expiration_date"]
+
+        if expiration_date < datetime.date.today():
+            raise OutdatedVaccineError()
+
+        if not visitor.get("wearing_a_mask", False):
+            raise NotWearingMaskError()
+
+        return f"Welcome to {self.name}"
--- app/main.py
+++ app/main.py
@@ -1,1 +1,82 @@
-# write your code here
+import datetime
+from typing import List, Dict, Any
+from app.cafe import Cafe
+
+
+def go_to_cafe(friends: List[Dict[str, Any]], cafe: Cafe) -> str:
+    for friend in friends:
+        if "vaccine" not in friend:
+            return "All friends should be vaccinated"
+
+    not_vaccinated_count = sum(
+        1 for friend in friends
+        if friend["vaccine"]["expiration_date"] < datetime.date.today())
+    mask_needed_count = sum(
+        1
+        for friend in friends
+        if not friend.get("wearing_a_mask", True))
+
+    if not_vaccinated_count > 0:
+        return "All friends should be vaccinated"
+
+    if mask_needed_count > 0:
+        if mask_needed_count == 1:
+            return f"Friends should buy {mask_needed_count} mask"
+        else:
+            return f"Friends should buy {mask_needed_count} masks"
+
+    return f"Friends can go to {cafe.name}"
+
+
+if __name__ == "__main__":
+    kfc = Cafe("KFC")
+
+    friends = [
+        {
+            "name": "Alisa",
+            "vaccine": {
+                "expiration_date": datetime.date.today()
+            },
+            "wearing_a_mask": True
+        },
+        {
+            "name": "Bob",
+            "vaccine": {
+                "expiration_date": datetime.date.today()
+            },
+            "wearing_a_mask": True
+        },
+    ]
+    print(go_to_cafe(friends, kfc))
+    friends = [
+        {
+            "name": "Alisa",
+            "vaccine": {
+                "expiration_date": datetime.date.today()
+            },
+            "wearing_a_mask": False
+        },
+        {
+            "name": "Bob",
+            "vaccine": {
+                "expiration_date": datetime.date.today()
+            },
+            "wearing_a_mask": False
+        },
+    ]
+    print(go_to_cafe(friends, kfc))
+
+    friends = [
+        {
+            "name": "Alisa",
+            "wearing_a_mask": True
+        },
+        {
+            "name": "Bob",
+            "vaccine": {
+                "expiration_date": datetime.date.today()
+            },
+            "wearing_a_mask": True
+        },
+    ]
+    print(go_to_cafe(friends, kfc))
