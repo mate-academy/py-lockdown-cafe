277100fa5d30584778b2386812ae9d1c2772f6c1
Anastasiia Mylnikova <87014295+amylnikova@users.noreply.github.com>
2024-10-15 01:30:40 +0300

    change imports back

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
-from errors import NotWearingMaskError, VaccineError
-from cafe import Cafe
+from app.errors import NotWearingMaskError, VaccineError
+from app.cafe import Cafe
 
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
