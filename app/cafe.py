3351519e9c02b9b9e63c7028f84ff1235f647074
Oleksandr Zaiets <mykola.oleksandr.zaiets@gmail.com>
2024-10-14 21:07:46 +0300

    Solution

--- app/cafe.py
+++ app/cafe.py
@@ -1,19 +1,21 @@
 import datetime
+from typing import Any, Optional
 
-from app.errors import NotVaccinatedError
-from app.errors import OutdatedVaccineError
-from app.errors import NotWearingMaskError
-
+from app.errors import (
+NotVaccinatedError,
+OutdatedVaccineError,
+NotWearingMaskError
+)
 
 class Cafe:
     def __init__(self, name: str) -> None:
         self.name = name
 
-    def visit_cafe(self, visitor: dict) -> str:
+    def visit_cafe(self, visitor: dict[str, Any]) -> Optional[str]:
         if "vaccine" not in visitor:
-            raise NotVaccinatedError("Visitor not vaccinated")
-        if visitor["vaccine"]["expiration_date"] < datetime.date.today():
+            raise NotVaccinatedError("The visitor is not vaccinated.")
+        elif visitor["vaccine"]["expiration_date"] < datetime.date.today():
             raise OutdatedVaccineError("Visitors vaccination is outdated")
-        if visitor["wearing_a_mask"] is False:
+        elif not visitor["wearing_a_mask"]:
             raise NotWearingMaskError("Visitor not wearing a mask")
         return f"Welcome to {self.name}"
--- app/errors.py
+++ app/errors.py
@@ -1,14 +1,18 @@
 class VaccineError(Exception):
-    pass
+    def __init__(self, message="Vaccine error occurred"):
+        super().__init__(message)
 
 
 class NotVaccinatedError(VaccineError):
-    pass
+    def __init__(self, message="The visitor is not vaccinated"):
+        super().__init__(message)
 
 
 class OutdatedVaccineError(VaccineError):
-    pass
+    def __init__(self, message="Visitors vaccination is outdated"):
+        super().__init__(message)
 
 
 class NotWearingMaskError(Exception):
-    pass
+    def __init__(self, message="Visitor not wearing a mask"):
+        super().__init__(message)
