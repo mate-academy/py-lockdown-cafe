b40976719f3fbcb4cb79e30434046308b04b6161
Alex <delete.rap.stupid@gmail.com>
2024-10-15 14:09:19 +0200

    fix "elif" construction

--- app/cafe.py
+++ app/cafe.py
@@ -13,8 +13,8 @@ class Cafe:
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
             raise NotVaccinatedError("You need to be vaccinated!")
-        elif visitor["vaccine"]["expiration_date"] < date.today():
+        if visitor["vaccine"]["expiration_date"] < date.today():
             raise OutdatedVaccineError("Your date of vaccine is expired!")
-        elif not visitor["wearing_a_mask"]:
+        if not visitor["wearing_a_mask"]:
             raise NotWearingMaskError("You don't have mask!")
         return f"Welcome to {self.name}"
