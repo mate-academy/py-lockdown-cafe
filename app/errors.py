4871851e6d4e401e4c5611ef6bd7ff2a54bc0b75
Oleksii Ivanov <oleks4335@gmail.com>
2024-10-20 20:11:17 +0200

    Add Lockdown cafe solution

---
+++ app/errors.py
@@ -0,0 +1,27 @@
+class VaccineError(Exception):
+    pass
+
+
+class NotVaccinatedError(VaccineError):
+
+    def __init__(self, message: str = "Visitor is not vaccinated.") -> None:
+        self.message = message
+        super().__init__(self.message)
+
+
+class OutdatedVaccineError(VaccineError):
+
+    def __init__(
+            self, message: str = "Visitor's vaccine is outdated."
+    ) -> None:
+        self.message = message
+        super().__init__(self.message)
+
+
+class NotWearingMaskError(Exception):
+
+    def __init__(
+            self, message: str = "Visitor is not wearing a mask."
+    ) -> None:
+        self.message = message
+        super().__init__(self.message)
