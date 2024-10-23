926b9617610b342f872f5a31349cab9ac29f4d47
Dmytro <dimontus02140@gmail.com>
2024-10-23 20:30:25 +0300

    fix problem

--- app/cafe.py
+++ app/cafe.py
@@ -7,12 +7,14 @@ from app.errors import (
 
 
 class Cafe:
-    def __init__(self, name: str) -> str:
+    def __init__(self, name: str) -> None:
         self.name = name
 
-    def visit_cafe(self, visitor: dict) -> None:
+    def visit_cafe(self, visitor: dict) -> str:
         if not ("vaccine" in visitor):
             raise NotVaccinatedError()
+        elif "expiration_date" not in visitor["vaccine"]:
+            raise OutdatedVaccineError()
         elif datetime.date.today() > visitor["vaccine"]["expiration_date"]:
             raise OutdatedVaccineError()
         elif not visitor["wearing_a_mask"]:
