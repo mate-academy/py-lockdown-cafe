02dc5be53c4419805ceaed3bffd1f0caa79d13c7
srx <srx92@protonmail.com>
2024-10-23 11:22:02 +0300

    Solution simple fixes

--- app/errors.py
+++ app/errors.py
@@ -6,13 +6,13 @@ class VaccineError(Exception):
 class NotVaccinatedError(VaccineError):
     """Raises if visitor don't have a 'vaccine' key in 'visitor' dict"""
     def __str__(self) -> str:
-        return "The visitor not vaccinated"
+        return "The visitor is not vaccinated"
 
 
 class OutdatedVaccineError(VaccineError):
     """Raises if visitor's vaccine date has expired"""
     def __str__(self) -> str:
-        return "The visitor has vaccine but with expired date"
+        return "The visitor vaccine is expired"
 
 
 class NotWearingMaskError(Exception):
